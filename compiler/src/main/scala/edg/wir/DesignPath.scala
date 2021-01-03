fc75b67b6f2fed510fab1662e92670b57ea0fdcd
Richard Lin <elpato25@gmail.com>
2021-01-02 22:42:36 -0800

    wip paths, omg paths

--- compiler/src/main/scala/edg/wir/DesignPath.scala
+++ compiler/src/main/scala/edg/wir/DesignPath.scala
@@ -4,14 +4,45 @@ import edg.elem.elem
 import edg.schema.schema
 import edg.ref.ref
 
+/**
+  * Absolute path (from the design root) to some element, including indirect elements like CONNECTED_LINK and link-side
+  * ports.
+  *
+  * Case classes will automatically generate an equality method that does the right thing,
+  * including field and type checks.
+  *
+  * TODO: would be much improved with union types... which are not supported in Scala =(
+  */
+trait IndirectDesignPath
+
+trait IndirectLinkParentable extends IndirectDesignPath
+trait IndirectLinkPortParentable extends IndirectDesignPath
+trait IndirectLinkParamParentable extends IndirectDesignPath
+
+case class ConnectedLink(parent: PortPath) extends IndirectDesignPath
+  with IndirectLinkPortParentable with IndirectLinkParamParentable
+case class LinkPort(eltName: String, parent: LinkPath) extends IndirectDesignPath
+  with IndirectLinkPortParentable with IndirectLinkParamParentable
+case class LinkPortParam(eltName: String, parent: IndirectLinkParamParentable) extends IndirectDesignPath
 
 /**
-  * Absolute path (from the design root) to some element
+  * Absolute path (from the design root) to some element.
+  * Excludes link ports, since the block side port is treated as authoritative.
   */
-trait DesignPath {
+trait DesignPath extends IndirectDesignPath
 
-}
+trait LinkParentable extends DesignPath with IndirectLinkParentable
+trait PortParentable extends DesignPath
+trait ParamParentable extends DesignPath
 
+case class BlockPath(eltName: String, parent: Option[BlockPath]) extends DesignPath
+  with LinkParentable with PortParentable with ParamParentable
+
+case class LinkPath(eltName: String, parent: LinkParentable) extends LinkParentable with ParamParentable
+
+case class PortPath(eltName: String, parent: PortParentable) extends PortParentable with ParamParentable
+
+case class ParamPath(eltName: String, parent: ParamParentable) extends DesignPath
 
 
 // TODO refactor to its own class file?
