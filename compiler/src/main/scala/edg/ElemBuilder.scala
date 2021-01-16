d98afdf599fa3ccc78898f1b568fc7c7017dd01c
Richard Lin <elpato25@gmail.com>
2021-01-16 15:12:06 -0800

    elembuilder, untested

--- compiler/src/main/scala/edg/ElemBuilder.scala
+++ compiler/src/main/scala/edg/ElemBuilder.scala
@@ -42,11 +42,72 @@ object ElemBuilder {
               constraints: Map[String, expr.ValueExpr] = Map(),
               superclasses: Seq[String] = Seq()
              ): elem.BlockLike = elem.BlockLike(`type`=elem.BlockLike.Type.Hierarchy(elem.HierarchyBlock(
-      params=params,
-      ports=ports, blocks=blocks, links=links,
+      params=params, ports=ports, blocks=blocks, links=links,
       constraints=constraints,
       superclasses=superclasses.map { LibraryPath(_) }
     )))
+  }
+
+  object Link {
+    def Library(path: ref.LibraryPath): elem.LinkLike = elem.LinkLike(`type`=elem.LinkLike.Type.LibElem(
+      value=path
+    ))
+    def Library(name: String): elem.LinkLike = elem.LinkLike(`type`=elem.LinkLike.Type.LibElem(
+      value=LibraryPath(name)
+    ))
+
+    def Link(params: Map[String, init.ValInit],
+             ports: Map[String, elem.PortLike],
+             links: Map[String, elem.LinkLike],
+             constraints: Map[String, expr.ValueExpr] = Map(),
+             superclasses: Seq[String] = Seq()
+            ): elem.LinkLike = elem.LinkLike(`type`=elem.LinkLike.Type.Link(elem.Link(
+      params=params, ports=ports, links=links,
+      constraints=constraints,
+      superclasses=superclasses.map { LibraryPath(_) }
+    )))
+  }
+
+  object Port {
+    def Library(path: ref.LibraryPath): elem.PortLike = elem.PortLike(`is`=elem.PortLike.Is.LibElem(
+      value=path
+    ))
+    def Library(name: String): elem.PortLike = elem.PortLike(`is`=elem.PortLike.Is.LibElem(
+      value=LibraryPath(name)
+    ))
+
+    def Port(params: Map[String, init.ValInit],
+             constraints: Map[String, expr.ValueExpr] = Map(),
+             superclasses: Seq[String] = Seq()
+            ): elem.PortLike = elem.PortLike(`is`=elem.PortLike.Is.Port(elem.Port(
+      params=params,
+      constraints=constraints,
+      superclasses=superclasses.map { LibraryPath(_) }
+    )))
+
+    def Bundle(params: Map[String, init.ValInit],
+               ports: Map[String, elem.PortLike],
+               constraints: Map[String, expr.ValueExpr] = Map(),
+               superclasses: Seq[String] = Seq()
+              ): elem.PortLike = elem.PortLike(`is`=elem.PortLike.Is.Bundle(elem.Bundle(
+      params=params, ports=ports,
+      constraints=constraints,
+      superclasses=superclasses.map { LibraryPath(_) }
+    )))
+
+    // Unelaborated (unknown length) PortArray, containing just a superclass reference
+    def Array(superclass: String): elem.PortLike = elem.PortLike(`is`=elem.PortLike.Is.Array(elem.PortArray(
+      superclasses=Seq(LibraryPath(superclass))
+    )))
+
+    // Fully elaborated (known length) PortArray
+    def Array(superclass: String, count: Int, port: elem.PortLike): elem.PortLike =
+      elem.PortLike(`is`=elem.PortLike.Is.Array(elem.PortArray(
+        superclasses=Seq(LibraryPath(superclass)),
+        ports=(0 until count).map { i =>
+          i.toString -> port
+        }.toMap
+      )))
   }
 
   def LibraryPath(name: String): ref.LibraryPath = ref.LibraryPath(target=Some(
