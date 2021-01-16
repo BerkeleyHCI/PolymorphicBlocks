100bc6b2b2a78d5d39ab3ddb0626b687de826470
Richard Lin <elpato25@gmail.com>
2021-01-16 00:38:47 -0800

    compiler algo sketching

--- compiler/src/main/scala/edg/compiler/Compiler.scala
+++ compiler/src/main/scala/edg/compiler/Compiler.scala
@@ -1,6 +1,9 @@
 package edg.compiler
 
+import scala.collection.mutable
 import edg.schema.schema
+import edg.wir.DesignPath
+import edg.wir
 
 
 /** Compiler for a particular design, with an associated library to elaborate references from.
@@ -9,6 +12,36 @@ import edg.schema.schema
   * During the compilation process, internal data structures are mutated.
   */
 class Compiler(inputDesignPb: schema.Design, library: edg.wir.Library) {
+  private val pending = mutable.Set[DesignPath]()  // block-likes pending elaboration
+  private val constProp = new ConstProp()
+
+  // Seed compilation with the root
+  //
+  private val root = wir.Block(inputDesignPb.contents.get)
+  for (rootBlockName <- root.pb.blocks.keys) {
+    pending += DesignPath.root + rootBlockName
+  }
+  for (rootLinkName <- root.pb.links.keys) {
+    pending += DesignPath.root + rootLinkName
+  }
+
+
+  /** Elaborate the unelaborated block at path (but where the parent has been elaborated and is reachable from root),
+    * and adds it to the parent and replaces the lib_elem proto entry with a placeholder unknown.
+    * Adds children to the pending queue, and adds constraints to constProp.
+    */
+  protected def elaborateBlock(path: DesignPath): Unit = {
+
+  }
+
+  /** Elaborate the unelaborated link at path (but where the parent has been elaborated and is reachable from root),
+    * and adds it to the parent and replaces the lib_elem proto entry with a placeholder unknown.
+    * Adds children to the pending queue, and adds constraints to constProp.
+    */
+  protected def elaborateBlock(path: DesignPath): Unit = {
+
+  }
+
   /** Performs full compilation and returns the resulting design. Might take a while.
     */
   def compile(): schema.Design = {
