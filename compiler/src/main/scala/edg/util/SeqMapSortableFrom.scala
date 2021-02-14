6e7c2f13c7bac928e9e0ba869ebe09682d3f6c89
Richard Lin <elpato25@gmail.com>
2021-02-13 18:14:40 -0800

    simpler and not as great sort

--- compiler/src/main/scala/edg/util/SeqMapSortableFrom.scala
+++ compiler/src/main/scala/edg/util/SeqMapSortableFrom.scala
@@ -6,12 +6,8 @@ import scala.collection.SeqMap
 object SeqMapSortableFrom {
   implicit class MapSortableFrom[K, V](map: Map[K, V]) {
     def sortKeysFrom(order: Seq[K]): SeqMap[K, V] = {
-      val sortedSeq = map.toSeq.sortWith { case ((k1, v1), (k2, v2)) => // true means _1 strictly before _2
-        (order.indexOf(k1), order.indexOf(k2)) match {
-          case (-1, _) | (_, -1) => false
-          case (k1Index, k2Index) => k1Index < k2Index
-        }
-      }
+      val sortedSeq = order.flatMap(key => map.get(key).map(key -> _)) ++
+          map.toSeq.filter { case (key, value) => !order.contains(key) }
       SeqMap(sortedSeq:_*)
     }
   }
