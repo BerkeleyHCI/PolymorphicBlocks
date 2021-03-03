cef1c5c05d500cf9efe56fefbcbaf98c262e4b66
josephinekoe <koejosephine@gmail.com>
2021-03-02 17:58:36 -0800

    adding mppt

--- electronics_lib/test_LT3652.py
+++ electronics_lib/test_LT3652.py
@@ -6,22 +6,22 @@ from electronics_lib.Microcontroller_nRF52840 import Adafruit_ItsyBitsy_BLE
 from examples.ExampleTestUtils import run_test
 
 
-class TestnRF52840Basic(BoardTop):
+class TestLT3652Basic(BoardTop):
   def contents(self):
     super().contents()
-    self.mcu = self.Block(Adafruit_ItsyBitsy_BLE())
+    self.mppt = self.Block(ChargeTracker_LT3652())
 
-    self.led = self.Block(IndicatorLed())
-    self.connect(self.mcu.gnd, self.led.gnd)
-    self.connect(self.mcu.new_io(DigitalBidir), self.led.signal)
+    # self.led = self.Block(IndicatorLed())
+    # self.connect(self.mppt.gnd, self.led.gnd)
+    # self.connect(self.mcu.new_io(DigitalBidir), self.led.signal)
 
   def refinements(self) -> Refinements:
     return super().refinements() + Refinements(
       instance_values=[
-        (['mcu', 'pin_assigns'], "")
+        (['mppt', 'pin_assigns'], "")
       ]
     )
 
-class nRF52840TestCase(unittest.TestCase):
+class LT3652TestCase(unittest.TestCase):
   def test_design_basic(self) -> None:
-    run_test(TestnRF52840Basic)
+    run_test(TestLT3652Basic)
