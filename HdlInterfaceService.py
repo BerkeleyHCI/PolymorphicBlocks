cbac5a35f22af7619e1b105de3393a62063ce214
Richard Lin <elpato25@gmail.com>
2021-09-20 15:18:47 -0700

    Update HdlInterfaceService.py

--- HdlInterfaceService.py
+++ HdlInterfaceService.py
@@ -6,65 +6,58 @@ from edg_core import BufferDeserializer, BufferSerializer
 
 
 if __name__ == '__main__':
-  verbose = True
+  verbose = False
+
+  def eprint(*args, **kwargs):
+    # Print to stderr, to avoid messing up the stdout communication channel
+    if verbose:
+      print(*args, **kwargs, file=sys.stderr, flush=True)
 
   server = HdlInterface(rollback=RollbackImporter())
   stdin_deserializer = BufferDeserializer(edgrpc.HdlRequest, sys.stdin.buffer)
   stdout_serializer = BufferSerializer[edgrpc.HdlResponse](sys.stdout.buffer)
 
-  print(f"HDL Interface Server started",
-        flush=True, file=sys.stderr)
+  eprint(f"HDL Interface Server started")
 
   while True:
     request = stdin_deserializer.read()
     response = edgrpc.HdlResponse()
     if request.HasField('reload_module'):
-      if verbose:
-        print(f"ReloadModule({request.reload_module.name}) -> ", end='',
-              flush=True, file=sys.stderr)
+      eprint(f"ReloadModule({request.reload_module.name}) -> ", end='')
 
       indexed = []
       try:
         indexed = server.ReloadModule(request.reload_module)
       except BaseException as e:
         # TODO pipe full error into response
-        print(f"Error {e}", flush=True, file=sys.stderr)
+        eprint(f"Error {e}")
 
-      if verbose:
-        print(f"(Indexed {len(indexed)})",
-              flush=True, file=sys.stderr)
+      eprint(f"(Indexed {len(indexed)})")
       response.reload_module.indexed.extend(indexed)
     elif request.HasField('get_library_element'):
       if verbose:
-        print(f"GetLibraryElement({request.get_library_element.element.target.name}) -> ", end='',
-              flush=True, file=sys.stderr)
+        eprint(f"GetLibraryElement({request.get_library_element.element.target.name}) -> ", end='')
+
       lib = server.GetLibraryElement(request.get_library_element)
-      if verbose:
-        if lib.HasField('error'):
-          print(f"Error {lib.error}",
-                flush=True, file=sys.stderr)
-        elif lib.HasField('refinements'):
-          print(f"(elt, w/ refinements)",
-                flush=True, file=sys.stderr)
-        else:
-          print(f"(elt)",
-                flush=True, file=sys.stderr)
+
+      if lib.HasField('error'):
+        eprint(f"Error {lib.error}")
+      elif lib.HasField('refinements'):
+        eprint(f"(elt, w/ refinements)")
+      else:
+        eprint(f"(elt)")
       response.get_library_element.CopyFrom(lib)
     elif request.HasField('elaborate_generator'):
-      if verbose:
-        print(f"ElaborateGenerator({request.elaborate_generator.element.target.name}) -> ", end='',
-              flush=True, file=sys.stderr)
+      eprint(f"ElaborateGenerator({request.elaborate_generator.element.target.name}) -> ", end='')
+
       gen = server.ElaborateGenerator(request.elaborate_generator)
-      if verbose:
-        if gen.HasField('error'):
-          print(f"Error {gen.error}",
-                flush=True, file=sys.stderr)
-        else:
-          print(f"(generated)",
-                flush=True, file=sys.stderr)
+
+      if gen.HasField('error'):
+        eprint(f"Error {gen.error}")
+      else:
+        eprint(f"(generated)")
       response.elaborate_generator.CopyFrom(gen)
     else:
-      print(f"Unknown request {request}",
-            flush=True, file=sys.stderr)
+      eprint(f"Unknown request {request}")
 
     stdout_serializer.write(response)
