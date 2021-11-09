0b1264eef90178e44ba659707a1313785923f223
Tengis Dashmunkh <tdashmunkh@berkeley.edu>
2021-11-08 16:44:56 -0800

    WIP

--- electronics_lib/JLC_Resistor.py
+++ electronics_lib/JLC_Resistor.py
@@ -1,3 +1,7 @@
+from electronics_abstract_parts import *
+from .ProductTableUtils import *
+from .PartsTable import *
+
 class JLCPCB_ResistorTable(ESeriesResistor):
   RESISTANCE = PartsTableColumn(Range)  
   POWER_DISSIPATION = PartsTableColumn(Range)  
@@ -7,45 +11,43 @@ class JLCPCB_ResistorTable(ESeriesResistor):
   @classmethod
   def _generate_table(cls) -> PartsTable:
     RESISTOR_MATCHES = {
-    'resistance': "\s?(\d+(\.\d*)?[GMkmunp]?)?[\u03A9]\s",
-    'tolerance': "\s?([\u00B1]\d+(\.\d*)?%)\s",
-    'power_dissipation': "\s?([\u00B1]\d+(\.\d*)?ppm)/℃\s"
+      'resistance': "\s?(\d+(\.\d*)?[GMkmunp]?)?[\u03A9]\s",
+      'tolerance': "\s?([\u00B1]\d+(\.\d*)?%)\s",
+      'power_dissipation': "\s?(\d+(\.\d*)?[GMkmunp]?)?W\s",
     }
 
     def parse_row(row: PartsTableRow) -> Optional[Dict[PartsTableColumn, Any]]:
-      if (row['Library Type'] != 'Basic'):
-          return None            
-      if (row['First Category'] != 'Resistors'):
+      if (row['Library Type'] != 'Basic' and row['First Category'] != 'Resistors'):
+        new_rows: Dict[PartsTableColumn, Any] = {}
+        try:
+          # handle the footprint first since this is the most likely to filter
+          footprint = (cls.PACKAGE_FOOTPRINT_MAP.get(row['Package / Case'], None) or
+                       cls.SERIES_FOOTPRINT_MAP.get(row['Series'], None) or
+                       cls.MPN_NR_FOOTPRINT_REGEX.apply(row['MFR.Part']))
+          new_rows[cls.FOOTPRINT] = footprint
+      
+          extracted_values = parse(row['Description'], RESISTOR_MATCHES)
+      
+          new_rows[cls.RESISTANCE] = Range.from_tolerance(
+              PartsTableUtil.parse_value(extracted_values['resistance'][0], 'Ohm'),
+              PartsTableUtil.parse_tolerance(extracted_values['tolerance'][0])
+              )
+          new_rows[cls.POWER_DISSIPATION] = PartsTableUtil.parse_value(extracted_values['power_dissipation'][0], 'W')
+          new_rows[cls.PRICE] = float(max(re.findall(":(\d+\.\d*),", row['Price'])))
+      
+          new_rows[cls.PRICE] = float(row['Price'])
+      
+          return new_rows
+        except (KeyError, PartsTableUtil.ParseError):
           return None
 
-      new_rows: Dict[PartsTableColumn, Any] = {}
-      try:
-      # handle the footprint first since this is the most likely to filter
-      footprint = ""
-      new_rows[cls.FOOTPRINT] = footprint
-    
-      extracted_values = parse(row['Description'], RESISTOR_MATCHES)
-    
-      new_rows[cls.RESISTANCE] = Range.from_tolerance(
-          PartsTableUtil.parse_value(extracted_values['resistance'][0], 'Ohm'),
-          PartsTableUtil.parse_tolerance(extracted_values['tolerance'][0])
-      )
-      new_rows[cls.POWER_DISSIPATION] = PartsTableUtil.parse_value(extracted_values['power_dissipation'][0])
-      new_rows[cls.PRICE] = float(max(re.findall(":(\d+\.\d*),", row['Price'])))
-    
-      new_rows[cls.PRICE] = float(row['Price'])
-    
-      return new_rows
-      except (KeyError, PartsTableUtil.ParseError):
-      return None
-
-      raw_table = PartsTable.from_csv_files(PartsTableUtil.with_source_dir([
+    raw_table = PartsTable.from_csv_files(PartsTableUtil.with_source_dir([
       'JLCPCB_SMT_Parts_Library.csv'
       ], 'resources'), encoding='utf-8-sig')
-      return raw_table.map_new_columns(parse_row)
+    return raw_table.map_new_columns(parse_row)
     
  
- class JLCPCB_Resistor(Resistor, FootprintBlock, GeneratorBlock):
+class JLCPCB_Resistor(Resistor, FootprintBlock, GeneratorBlock):
    @init_in_parent
    def __init__(self, **kwargs):
      super().__init__(**kwargs)
@@ -61,16 +63,16 @@ class JLCPCB_ResistorTable(ESeriesResistor):
    def select_inductor(self, resistance: Range, power_dissipation: Range,
                        part_spec: str, footprint_spec: str) -> None:
      compatible_parts = JLCPCB_ResistorTable.table().filter(lambda row: (
-         (not part_spec or part_spec == row['Manufacturer Part Number']) and
+         (not part_spec or part_spec == row['MFR.Part']) and
          (not footprint_spec or footprint_spec == row[InductorTable.FOOTPRINT]) and
          row[JLCPCB_ResistorTable.RESISTANCE].fuzzy_in(resistance) and
-         row[JLCPCB_ResistorTable.POWER_DISSIPATION].fuzzy_in(power_dissipation) and
+         row[JLCPCB_ResistorTable.POWER_DISSIPATION].fuzzy_in(power_dissipation)
      ))
      part = compatible_parts.sort_by(
        lambda row: row[JLCPCB_ResistorTable.FOOTPRINT]
      ).sort_by(
        lambda row: row[JLCPCB_ResistorTable.PRICE]
-     ).first(f"no resistors in {resistance} Ohm, {power_dissipation} ppm, {[row.value['Manufacturer Part Number'] for row in JLCPCB_ResistorTable.table().rows]}")
+     ).first(f"no resistors in {resistance} Ohm, {power_dissipation} W, {[row.value['MFR.Part'] for row in JLCPCB_ResistorTable.table().rows]}")
 
      self.assign(self.selected_resistance, part[JLCPCB_ResistorTable.RESISTANCE])
      self.assign(self.selected_power_dissipation, part[JLCPCB_ResistorTable.POWER_DISSIPATION])
@@ -81,7 +83,7 @@ class JLCPCB_ResistorTable(ESeriesResistor):
          '1': self.a,
          '2': self.b,
        },
-       mfr=part['Manufacturer'], part=part['Manufacturer Part Number'],
+       mfr=part['Manufacturer'], part=part['MFR.Part'],
        value=f"{part['Resistance']}, {part['Power_dissipation']}",
        datasheet=part['Datasheets']
      )
