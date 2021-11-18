cec9f20b524b7c5250b7fcfc6a947062732cd389
Tengis Dashmunkh <tdashmunkh@berkeley.edu>
2021-11-18 00:10:01 -0800

    WIP

--- electronics_lib/JLcCapacitor.py
+++ electronics_lib/JLcCapacitor.py
@@ -1,77 +1,77 @@
 from typing import Optional
-from .parse import parse
 from electronics_abstract_parts import *
 from .JLcTable import *
 from .TableDeratingCapacitor import TableDeratingCapacitor
 from .PassiveCapacitor import *
 
 class JLcCapacitorTable(JLcTable):
-    CAPACITANCE = PartsTableColumn(Range)
-    NOMINAL_CAPACITANCE = PartsTableColumn(float)
-    VOLTAGE_RATING = PartsTableColumn(Range)
-    FOOTPRINT = PartsTableColumn(str)
+  CAPACITANCE = PartsTableColumn(Range)
+  NOMINAL_CAPACITANCE = PartsTableColumn(float)
+  VOLTAGE_RATING = PartsTableColumn(Range)
+  FOOTPRINT = PartsTableColumn(str)
 
-    PACKAGE_FOOTPRINT_MAP = {
-        '0603': 'Capacitor_SMD:C_0603_1608Metric',
-        '0805': 'Capacitor_SMD:C_0805_2012Metric',
-        '1206': 'Capacitor_SMD:C_1206_3216Metric',
+  PACKAGE_FOOTPRINT_MAP = {
+    '0603': 'Capacitor_SMD:C_0603_1608Metric',
+    '0805': 'Capacitor_SMD:C_0805_2012Metric',
+    '1206': 'Capacitor_SMD:C_1206_3216Metric',
+  }
+
+  @classmethod
+  def _generate_table(cls) -> PartsTable:
+    CAPACITOR_MATCHES = {
+        'nominal_capacitance': "\s?(\d+(?:\.\d*)?[GMkmunp]?F)\s",
+        'tolerance': "\s?([\u00B1]\d+(?:\.\d*)?%)\s",
+        'voltage': "\s?(\d+(?:\.\d*)?V)\s",
     }
 
-    @classmethod
-    def _generate_table(cls) -> PartsTable:
-        CAPACITOR_MATCHES = {
-            'nominal_capacitance': "\s?(\d+(?:\.\d*)?[GMkmunp]?F)\s",
-            'tolerance': "\s?([\u00B1]\d+(?:\.\d*)?%)\s",
-            'voltage': "\s?(\d+(?:\.\d*)?V)\s",
-        }
+    def parse_row(row: PartsTableRow) -> Optional[Dict[PartsTableColumn, Any]]:
+      if (row['Library Type'] == 'Basic' and row['First Category'] == 'Capacitors'):
+        new_cols: Dict[PartsTableColumn, Any] = {}
+        try:
+          # handle the footprint first since this is the most likely to filter
+          footprint = cls.PACKAGE_FOOTPRINT_MAP[row['Package']]
+          new_cols[cls.FOOTPRINT] = footprint
 
-        def parse_row(row: PartsTableRow) -> Optional[Dict[PartsTableColumn, Any]]:
-            if (row['Library Type'] == 'Basic' and row['First Category'] == 'Capacitors'):
-                new_cols: Dict[PartsTableColumn, Any] = {}
-                try:
-                    # handle the footprint first since this is the most likely to filter
-                    footprint = cls.PACKAGE_FOOTPRINT_MAP[row['Package']]
-                    new_cols[cls.FOOTPRINT] = footprint
+          extracted_values = JLcTable.parse(row[JLcTable.DESCRIPTION], CAPACITOR_MATCHES)
 
-                    extracted_values = parse(row['Description'], CAPACITOR_MATCHES)
-                    if (bool(extracted_values) == False):
-                        return None
+          nominal_capacitance = PartsTableUtil.parse_value(extracted_values['nominal_capacitance'], 'F')
 
-                    nominal_capacitance = PartsTableUtil.parse_value(extracted_values['nominal_capacitance'], 'F')
+          # enforce minimum packages, note the cutoffs are exclusive
+          if nominal_capacitance > 10e-6 and footprint not in [
+            'Capacitor_SMD:C_1206_3216Metric',
+          ]:
+            return None
+          elif nominal_capacitance > 1e-6 and footprint not in [
+            'Capacitor_SMD:C_0805_2012Metric',
+            'Capacitor_SMD:C_1206_3216Metric',
+          ]:
+            return None
 
-                    # enforce minimum packages, note the cutoffs are exclusive
-                    if nominal_capacitance > 10e-6 and footprint not in [
-                        'Capacitor_SMD:C_1206_3216Metric',
-                    ]:
-                        return None
-                    elif nominal_capacitance > 1e-6 and footprint not in [
-                        'Capacitor_SMD:C_0805_2012Metric',
-                        'Capacitor_SMD:C_1206_3216Metric',
-                    ]:
-                        return None
+          new_cols['Capacitance'] = extracted_values['nominal_capacitance']
+          new_cols['Voltage - Rated'] = extracted_values['voltage']
 
-                    new_cols[cls.FOOTPRINT] = footprint
-                    new_cols[cls.CAPACITANCE] = Range.from_tolerance(
-                        nominal_capacitance,
-                        PartsTableUtil.parse_tolerance(extracted_values['tolerance'])
-                    )
-                    new_cols[cls.NOMINAL_CAPACITANCE] = nominal_capacitance
-                    new_cols[cls.VOLTAGE_RATING] = Range.zero_to_upper(
-                        PartsTableUtil.parse_value(extracted_values['voltage'], 'V')
-                    )
+          new_cols[cls.FOOTPRINT] = footprint
+          new_cols[cls.CAPACITANCE] = Range.from_tolerance(
+            nominal_capacitance,
+            PartsTableUtil.parse_tolerance(extracted_values['tolerance'])
+          )
+          new_cols[cls.NOMINAL_CAPACITANCE] = nominal_capacitance
+          new_cols[cls.VOLTAGE_RATING] = Range.zero_to_upper(
+            PartsTableUtil.parse_value(extracted_values['voltage'], 'V')
+          )
 
-                    new_cols.update(cls._parse_jlcpcb_common(row))
+          new_cols.update(cls._parse_jlcpcb_common(row))
 
-                    return new_cols
-                except (KeyError, PartsTableUtil.ParseError):
-                    return None
+          return new_cols
+        except (KeyError, PartsTableUtil.ParseError):
+          return None
 
-        raw_table = PartsTable.from_csv_files(PartsTableUtil.with_source_dir([
-            'JLCPCB_SMT_Parts_Library.csv'
-        ], 'resources'), encoding='gb2312')
-        return raw_table.map_new_columns(parse_row).sort_by(
-            lambda row: [row[cls.FOOTPRINT], row[cls.COST]]
-        )
+    raw_table = PartsTable.from_csv_files(PartsTableUtil.with_source_dir([
+      'JLCPCB_SMT_Parts_Library.csv'
+    ], 'resources'), encoding='gb2312')
+    return raw_table.map_new_columns(parse_row).sort_by(
+      lambda row: [row[cls.FOOTPRINT], row[cls.COST]]
+    )
 
 class JLcCapacitor(TableDeratingCapacitor, FootprintBlock, GeneratorBlock):
 
--- electronics_lib/TableDeratingCapacitor.py
+++ electronics_lib/TableDeratingCapacitor.py
@@ -3,39 +3,97 @@ from .PartsTable import *
 import math
 
 class TableDeratingCapacitor(Capacitor):
-    DERATE_VOLTCO = {
-        'Capacitor_SMD:C_0805_2012Metric': 0.08,
-        'Capacitor_SMD:C_1206_3216Metric': 0.04,
-    }
-    DERATE_MIN_VOLTAGE = 3.6  # voltage at which derating is zero
-    DERATE_MIN_CAPACITANCE = 1.0e-6
-    DERATED_CAPACITANCE = PartsTableColumn(Range)
+  DERATE_VOLTCO = {  # in terms of %capacitance / V over 3.6
+    #  'Capacitor_SMD:C_0603_1608Metric'  # not supported, should not generate below 1uF
+    'Capacitor_SMD:C_0805_2012Metric': 0.08,
+    'Capacitor_SMD:C_1206_3216Metric': 0.04,
+  }
+  DERATE_MIN_VOLTAGE = 3.6  # voltage at which derating is zero
+  DERATE_MIN_CAPACITANCE = 1.0e-6
+  DERATED_CAPACITANCE = PartsTableColumn(Range)
 
-    PARALLEL_COUNT = PartsTableColumn(int)
-    PARALLEL_CAPACITANCE = PartsTableColumn(Range)
-    PARALLEL_DERATED_CAPACITANCE = PartsTableColumn(Range)
-    PARALLEL_COST = PartsTableColumn(float)
+  PARALLEL_COUNT = PartsTableColumn(int)
+  PARALLEL_CAPACITANCE = PartsTableColumn(Range)
+  PARALLEL_DERATED_CAPACITANCE = PartsTableColumn(Range)
+  PARALLEL_COST = PartsTableColumn(float)
 
-    def derate_row(self, row: PartsTableRow, voltage: Range, Table: Type) -> Optional[Dict[PartsTableColumn, Any]]:
-        if voltage.upper < self.DERATE_MIN_VOLTAGE:  # zero derating at low voltages
-            derated = row[Table.CAPACITANCE]
-        elif row[Table.NOMINAL_CAPACITANCE] <= self.DERATE_MIN_CAPACITANCE:  # don't derate below 1uF
-            derated = row[Table.CAPACITANCE]
-        elif row[Table.FOOTPRINT] not in self.DERATE_VOLTCO:  # should be rare, small capacitors should hit the above
-            return None
-        else:  # actually derate
-            voltco = self.DERATE_VOLTCO[row[Table.FOOTPRINT]]
-            factor = 1 - voltco * (Table.upper - 3.6)
-            derated = row[Table.CAPACITANCE] * Range(factor, 1)
+  @init_in_parent
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
 
-        return {self.DERATED_CAPACITANCE: derated}
+    self.part_spec = self.Parameter(StringExpr(""))
+    self.footprint_spec = self.Parameter(StringExpr(""))
 
-    def parallel_row(self, row: PartsTableRow, capacitance: Range, Table: Type) -> Optional[Dict[PartsTableColumn, Any]]:
+    # Default that can be overridden
+    self.single_nominal_capacitance = self.Parameter(RangeExpr((0, 22)*uFarad))  # maximum capacitance in a single part
+
+    self.generator(self.select_capacitor, self.capacitance, self.voltage, self.single_nominal_capacitance,
+                   self.part_spec, self.footprint_spec)
+
+    # Output values
+    self.selected_capacitance = self.Parameter(RangeExpr())
+    self.selected_derated_capacitance = self.Parameter(RangeExpr())
+    self.selected_voltage_rating = self.Parameter(RangeExpr())
+
+  def select_capacitor(self, capacitance: Range, voltage: Range,
+                       single_nominal_capacitance: Range,
+                       part_spec: str, footprint_spec: str, Table: Type) -> None:
+    # Pre-filter out by the static parameters
+    # Note that we can't filter out capacitance before derating
+    prefiltered_parts = Table.table().filter(lambda row: (
+      (not part_spec or part_spec == row[Table.PART_NUMBER]) and
+      (not footprint_spec or footprint_spec == row[Table.FOOTPRINT]) and
+      voltage.fuzzy_in(row[Table.VOLTAGE_RATING]) and
+      Range.exact(row[Table.NOMINAL_CAPACITANCE]).fuzzy_in(single_nominal_capacitance)
+    ))
+
+    def derate_row(row: PartsTableRow) -> Optional[Dict[PartsTableColumn, Any]]:
+      if voltage.upper < self.DERATE_MIN_VOLTAGE:  # zero derating at low voltages
+        derated = row[Table.CAPACITANCE]
+      elif row[Table.NOMINAL_CAPACITANCE] <= self.DERATE_MIN_CAPACITANCE:  # don't derate below 1uF
+        derated = row[Table.CAPACITANCE]
+      elif row[Table.FOOTPRINT] not in self.DERATE_VOLTCO:  # should be rare, small capacitors should hit the above
+        return None
+      else:  # actually derate
+        voltco = self.DERATE_VOLTCO[row[Table.FOOTPRINT]]
+        factor = 1 - voltco * (voltage.upper - 3.6)
+        derated = row[Table.CAPACITANCE] * Range(factor, 1)
+      return {self.DERATED_CAPACITANCE: derated}
+
+    # If the min required capacitance is above the highest post-derating minimum capacitance, use the parts table.
+    # An empty parts table handles the case where it's below the minimum or does not match within a series.
+    derated_parts = prefiltered_parts.map_new_columns(
+      derate_row
+    )
+    derated_max_min_capacitance = max(derated_parts.map(lambda row: row[self.DERATED_CAPACITANCE].lower))
+
+    if capacitance.lower <= derated_max_min_capacitance:
+      part = derated_parts.filter(lambda row: (
+        row[self.DERATED_CAPACITANCE] in capacitance
+      )).first(f"no single capacitor in {capacitance} F, {voltage} V")
+
+    self.assign(self.selected_voltage_rating, part[Table.VOLTAGE_RATING])
+    self.assign(self.selected_capacitance, part[Table.CAPACITANCE])
+    self.assign(self.selected_derated_capacitance, part[self.DERATED_CAPACITANCE])
+
+    self.footprint(
+      'C', part[Table.FOOTPRINT],
+      {
+        '1': self.pos,
+        '2': self.neg,
+      },
+      mfr=part[Table.MANUFACTURER], part=part[Table.PART_NUMBER],
+      value=f"{part['Capacitance']}, {part['Voltage - Rated']}",
+      datasheet=part[Table.DATASHEETS]
+      )
+    else:  # Otherwise, generate multiple capacitors
+      # Additionally annotate the table by total cost and count, sort by lower count then total cost
+      def parallel_row(row: PartsTableRow) -> Optional[Dict[PartsTableColumn, Any]]:
         new_cols: Dict[PartsTableColumn, Any] = {}
         count = math.ceil(capacitance.lower / row[self.DERATED_CAPACITANCE].lower)
         derated_parallel_capacitance = row[self.DERATED_CAPACITANCE] * count
         if not derated_parallel_capacitance.fuzzy_in(capacitance):  # not satisfying spec
-            return None
+          return None
 
         new_cols[self.PARALLEL_COUNT] = count
         new_cols[self.PARALLEL_DERATED_CAPACITANCE] = derated_parallel_capacitance
@@ -43,3 +101,24 @@ class TableDeratingCapacitor(Capacitor):
         new_cols[self.PARALLEL_COST] = row[Table.COST] * count
 
         return new_cols
+
+        part = derated_parts.map_new_columns(
+          parallel_row
+        ).sort_by(lambda row:
+            (row[self.PARALLEL_COUNT], row[self.PARALLEL_COST])
+          ).first(f"no parallel capacitor in {capacitance} F, {voltage} V")
+
+        self.assign(self.selected_voltage_rating, part[Table.VOLTAGE_RATING])
+        self.assign(self.selected_capacitance, part[self.PARALLEL_CAPACITANCE])
+        self.assign(self.selected_derated_capacitance, part[self.PARALLEL_DERATED_CAPACITANCE])
+
+        cap_model = DummyCapacitor(capacitance=part[Table.NOMINAL_CAPACITANCE],
+                                   voltage=self.voltage,
+                                   footprint=part[Table.FOOTPRINT],
+                                   manufacturer=part[Table.MANUFACTURER], part_number=part[Table.PART_NUMBER],
+                                   value=f"{part['Capacitance']}, {part['Voltage - Rated']}")
+        self.c = ElementDict[DummyCapacitor]()
+        for i in range(part[self.PARALLEL_COUNT]):
+          self.c[i] = self.Block(cap_model)
+          self.connect(self.c[i].pos, self.pos)
+          self.connect(self.c[i].neg, self.neg)
