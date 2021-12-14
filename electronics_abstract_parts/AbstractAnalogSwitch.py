9454aaf2877d61e1cf69ea7a7b39363f0c9fe8c3
Richard Lin <elpato25@gmail.com>
2021-12-13 22:31:33 -0800

    analog switch, passes typing at least

--- electronics_abstract_parts/AbstractAnalogSwitch.py
+++ electronics_abstract_parts/AbstractAnalogSwitch.py
@@ -5,7 +5,7 @@ from .AbstractPassives import Resistor
 
 @abstract_block
 class AnalogSwitch(Block):
-  """Base class for a SPDT analog switch.
+  """Base class for a SPDT analog switch with passive-typed ports.
   """
   def __init__(self) -> None:
     super().__init__()
@@ -21,9 +21,72 @@ class AnalogSwitch(Block):
 
     # TODO: this is a different way of modeling parts - parameters in the part itself
     # instead of on the ports (because this doesn't have typed ports)
-    self.led_forward_voltage = self.Parameter(RangeExpr())
-    self.led_current_limit = self.Parameter(RangeExpr())
-    self.led_current_recommendation = self.Parameter(RangeExpr())
-    self.load_voltage_limit = self.Parameter(RangeExpr())
-    self.load_current_limit = self.Parameter(RangeExpr())
-    self.load_resistance = self.Parameter(RangeExpr())
+    self.analog_voltage_limits = self.Parameter(RangeExpr())  # for all analog ports
+    self.analog_current_limits = self.Parameter(RangeExpr())  # for all analog ports
+    self.analog_on_resistance = self.Parameter(RangeExpr())
+
+
+class AnalogMuxer(Block):
+  """Wrapper around AnalogSwitch that provides muxing functionality - multiple sink ports, one source port.
+  """
+  def __init__(self) -> None:
+    super().__init__()
+    self.device = self.Block(AnalogSwitch())
+    self.pwr = self.Export(self.device.pwr)
+    self.gnd = self.Export(self.device.gnd)
+    self.control = self.Export(self.device.control)
+
+    # TODO: ideally this would be in0/in1, but just in (for AnalogDemuxer) is a reserved Python keyword
+    self.input0 = self.Export(self.device.nc.as_analog_sink(
+      voltage_limits=self.device.analog_voltage_limits,
+      current_draw=RangeExpr(),  # forward-declared
+      impedance=RangeExpr(),  # forward-declared
+    ))
+    self.input1 = self.Export(self.device.no.as_analog_sink(
+      voltage_limits=self.device.analog_voltage_limits,
+      current_draw=RangeExpr(),  # forward-declared
+      impedance=RangeExpr(),  # forward-declared
+    ))
+
+    self.out = self.Export(self.device.com.as_analog_source(
+      voltage_out=self.input0.link().voltage.hull(self.input1.link().voltage),
+      current_limits=self.device.analog_current_limits,
+      impedance=self.input0.link().source_impedance.hull(self.input1.link().source_impedance) +
+                self.device.analog_on_resistance
+    ))
+    self.assign(self.input0.current_draw, self.out.link().current_drawn)
+    self.assign(self.input1.current_draw, self.out.link().current_drawn)
+    self.assign(self.input0.impedance, self.out.link().sink_impedance + self.device.analog_on_resistance)
+    self.assign(self.input1.impedance, self.out.link().sink_impedance + self.device.analog_on_resistance)
+
+
+class AnalogDemuxer(Block):
+  """Wrapper around AnalogSwitch that provides demuxing functionality - multiple source ports, one sink port.
+  """
+  def __init__(self) -> None:
+    super().__init__()
+    self.device = self.Block(AnalogSwitch())
+    self.pwr = self.Export(self.device.pwr)
+    self.gnd = self.Export(self.device.gnd)
+    self.control = self.Export(self.device.control)
+
+    self.input = self.Export(self.device.com.as_analog_sink(
+      voltage_limits=self.device.analog_voltage_limits,
+      current_draw=RangeExpr(),  # forward-declared
+      impedance=RangeExpr(),  # forward-declared
+    ))
+    self.out0 = self.Export(self.device.nc.as_analog_source(
+      voltage_out=self.input.link().voltage,
+      current_limits=self.device.analog_current_limits,
+      impedance=self.input.link().source_impedance + self.device.analog_on_resistance
+    ))
+    self.out1 = self.Export(self.device.no.as_analog_source(
+      voltage_out=self.input.link().voltage,
+      current_limits=self.device.analog_current_limits,
+      impedance=self.input.link().source_impedance + self.device.analog_on_resistance
+    ))
+    self.assign(self.input.current_draw,
+                self.out0.link().current_drawn.hull(self.out1.link().current_drawn))
+    self.assign(self.input.impedance,
+                self.out0.link().sink_impedance.hull(self.out1.link().sink_impedance) +
+                self.device.analog_on_resistance)
--- electronics_lib/AnalogSwitch_Nlas4157.py
+++ electronics_lib/AnalogSwitch_Nlas4157.py
@@ -5,27 +5,28 @@ class Nlas4157_Device(FootprintBlock):
   def __init__(self):
     super().__init__()
 
-    self.pwr = self.Port(VoltageSink(
+    self.vcc = self.Port(VoltageSink(
       voltage_limits=(1.65, 5.5)*Volt,
       current_draw=(0.5, 1.0)*uAmp,  # Icc, at 5.5v, typ to max
     ))
     self.gnd = self.Port(Ground())
 
-    self.control = self.Port(DigitalSink(
+    self.s = self.Port(DigitalSink(
       voltage_limits=(-0.5, 6)*Volt,
       current_draw=(-1, 1)*uAmp,  # input leakage current
       input_thresholds=(0.6, 2.4)*Volt,  # strictest of Vdd=2.7, 4.5 V
     ))
 
+    self.analog_voltage_limits = self.Parameter(RangeExpr((
+      self.gnd.link().voltage.upper() - 0.5,
+      self.vcc.link().voltage.lower() + 0.5
+    )))
+    self.analog_current_limits = self.Parameter(RangeExpr((-300, 300)*mAmp))
+    self.analog_on_resistance = self.Parameter((0.3, 4.3)*Ohm)
 
-    # TBD analog parameters:
-    # max analog voltage -0.5 to Vcc+0.5
-    # max analog current limit 300mA continuous
-    # switch on-resistance 0.3-4.3 ohm (typ and max, for all Vcc)
-
-    self.com = self.Port(Passive())
-    self.no = self.Port(Passive())
-    self.nc = self.Port(Passive())
+    self.a = self.Port(Passive())
+    self.b1 = self.Port(Passive())
+    self.b0 = self.Port(Passive())
 
   def contents(self):
     super().contents()
@@ -33,12 +34,12 @@ class Nlas4157_Device(FootprintBlock):
     self.footprint(
       'U', 'Package_TO_SOT:SOT-363_SC-70-6',
       {
-        '1': self.no,
+        '1': self.b1,
         '2': self.gnd,
-        '3': self.nc,
-        '4': self.com,
-        '5': self.pwr,
-        '6': self.control,
+        '3': self.b0,
+        '4': self.a,
+        '5': self.vcc,
+        '6': self.s,
       },
       mfr='ON Semiconductor', part='NLAS4157',
       datasheet='https://www.onsemi.com/pdf/datasheet/nlas4157-d.pdf'
@@ -46,5 +47,22 @@ class Nlas4157_Device(FootprintBlock):
 
 
 class Nlas4157(AnalogSwitch):
-  def __init__(self):
-    super().__init__()
+  def contents(self):
+    super().contents()
+
+    self.ic = self.Block(Nlas4157_Device())
+    self.connect(self.pwr, self.ic.vcc)
+    self.connect(self.gnd, self.ic.gnd)
+    self.connect(self.com, self.ic.a)
+    self.connect(self.no, self.ic.b1)
+    self.connect(self.nc, self.ic.b0)
+    self.connect(self.control, self.ic.s)
+
+    # surprisingly, the datasheet doesn't actually specify any decoupling capacitors, but here's one anyways
+    with self.implicit_connect(
+        ImplicitConnect(self.pwr, [Power]),
+        ImplicitConnect(self.gnd, [Common])
+    ) as imp:
+      self.vdd_cap = imp.Block(DecouplingCapacitor(
+        capacitance=0.1*uFarad(tol=0.2),
+      ))
