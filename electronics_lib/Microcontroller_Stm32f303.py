b0f18e043bf3027319a884709baa03d6750be518
Richard Lin <elpato25@gmail.com>
2022-04-03 22:18:26 -0700

    wip

--- electronics_lib/Microcontroller_Stm32f303.py
+++ electronics_lib/Microcontroller_Stm32f303.py
@@ -20,6 +20,13 @@ class Stm32f303Base_Device():
       input_threshold_factor=input_threshold_factor,
       pullup_capable=True, pulldown_capable=True
     )
+    dio_tc_switch_model = DigitalBidir.from_supply(
+      gnd, vdd,
+      voltage_limit_abs=(-0.3, 0.3) * Volt,  # Table 19
+      current_draw=(0, 0)*Amp, current_limits=(-3, 0),  # Table 13, note 1, can sink 3 mA and should not source current
+      input_threshold_factor=input_threshold_factor,
+      pullup_capable=True, pulldown_capable=True
+    )
     dio_tt_model = DigitalBidir.from_supply(
       gnd, vdd,
       voltage_limit_abs=(-0.3, 3.6) * Volt,  # Table 19
@@ -65,7 +72,7 @@ class Stm32f303Base_Device():
     spi_model = SpiMaster(DigitalBidir.empty())
     i2c_model = I2cMaster(DigitalBidir.empty())
 
-    return PinMapUtil([  # Table 5, partial table for 48-pin only
+    return PinMapUtil([  # Table 13, partial table for 48-pin only
       PinResource('PC13', {'PC13': dio_tc_model}),
       PinResource('PC14', {'PC14': dio_tc_model}),  # TODO remappable to OSC32_IN
       PinResource('PC15', {'PC15': dio_tc_model}),  # TODO remappable to OSC32_OUT
@@ -79,148 +86,148 @@ class Stm32f303Base_Device():
       PinResource('PA4', {'PA4': dio_tta_model, 'ADC2_IN1': adc_model, 'DAC1_OUT1': dac_model}),
       PinResource('PA5', {'PA5': dio_tta_model, 'ADC2_IN2': adc_model, 'DAC1_OUT2': dac_model}),
 
+      PinResource('PA6', {'PA6': dio_tta_model, 'ADC2_IN3': adc_model, 'DAC2_OUT1': dac_model}),
+      PinResource('PA7', {'PA7': dio_tta_model, 'ADC2_IN4': adc_model}),
+      PinResource('PB0', {'PB0': dio_tta_model, 'ADC1_IN11': adc_model}),
+      PinResource('PB1', {'PB1': dio_tta_model, 'ADC1_IN12': adc_model}),
+      PinResource('PB2', {'PB2': dio_tta_model, 'ADC2_IN12': adc_model}),
+      PinResource('PB10', {'PB10': dio_tt_model}),
+      PinResource('PB11', {'PB11': dio_tta_model}),
+      PinResource('PB12', {'PB12': dio_tta_model, 'ADC2_IN13': adc_model}),
+      PinResource('PB13', {'PB13': dio_tta_model, 'ADC1_IN13': adc_model}),
+      PinResource('PB14', {'PB14': dio_tta_model, 'ADC2_IN14': adc_model}),
 
-
-
-
-
-
-      PinResource('PC13', {'PC13': dio_pc_13_14_15_model}),
-      PinResource('PC14', {'PC14': dio_pc_13_14_15_model, 'OSC32_IN': Passive()}),
-      PinResource('PC15', {'PC15': dio_pc_13_14_15_model, 'OSC32_OUT': Passive()}),
-
-      PinResource('PA0', {'PA0': dio_std_model, 'ADC12_IN0': adc_model}),
-      PinResource('PA1', {'PA1': dio_std_model, 'ADC12_IN1': adc_model}),
-      PinResource('PA2', {'PA2': dio_std_model, 'ADC12_IN2': adc_model}),
-      PinResource('PA3', {'PA3': dio_std_model, 'ADC12_IN3': adc_model}),
-      PinResource('PA4', {'PA4': dio_std_model, 'ADC12_IN4': adc_model}),
-      PinResource('PA5', {'PA5': dio_std_model, 'ADC12_IN5': adc_model}),
-      PinResource('PA6', {'PA6': dio_std_model, 'ADC12_IN6': adc_model}),
-      PinResource('PA7', {'PA7': dio_std_model, 'ADC12_IN7': adc_model}),
-      PinResource('PB0', {'PB0': dio_std_model, 'ADC12_IN8': adc_model}),
-      PinResource('PB1', {'PB1': dio_std_model, 'ADC12_IN9': adc_model}),
-
-      PinResource('PB2', {'PB2': dio_ft_model}),  # BOOT1
-      PinResource('PB10', {'PB10': dio_ft_model}),
-      PinResource('PB11', {'PB11': dio_ft_model}),
-      PinResource('PB12', {'PB12': dio_ft_model}),
-      PinResource('PB13', {'PB13': dio_ft_model}),
-      PinResource('PB14', {'PB14': dio_ft_model}),
-      PinResource('PB15', {'PB15': dio_ft_model}),
-
+      PinResource('PB15', {'PB15': dio_tta_model, 'ADC2_IN15': adc_model}),
       PinResource('PA8', {'PA8': dio_ft_model}),
       PinResource('PA9', {'PA9': dio_ft_model}),
       PinResource('PA10', {'PA10': dio_ft_model}),
       PinResource('PA11', {'PA11': dio_ft_model}),
       PinResource('PA12', {'PA12': dio_ft_model}),
-      # PinResource('PA13', {'PA13': dio_ft_model}),  # forced SWDIO default is JTMS/SWDIO
+      PinResource('PA13', {'PA13': dio_ft_model}),  # also JTMS/SWDAT
 
-      # PinResource('PA14', {'PA14': dio_ft_model}),  # forced SWCLK, default is JTCK/SWCLK
-      PinResource('PA15', {'PA15': dio_ft_model}),  # default is JTDI
-      # PinResource('PB3', {'PB3': dio_ft_model}),  # forced SWO, default is JTDO
-      PinResource('PB4', {'PB4': dio_ft_model}),  # default is JNTRST
-      PinResource('PB5', {'PB5': dio_std_model}),
-      PinResource('PB6', {'PB6': dio_ft_model}),
-      PinResource('PB7', {'PB7': dio_ft_model}),
-      PinResource('PB8', {'PB8': dio_ft_model}),
-      PinResource('PB9', {'PB9': dio_ft_model}),
+      PinResource('PA14', {'PA14': dio_ftf_model}),  # also JTCK/SWCLK
+      PinResource('PA15', {'PA15': dio_ftf_model}),  # also JTDI
+      PinResource('PB3', {'PB3': dio_ft_model}),  # also JTDO/TRACESWO
+      PinResource('PB4', {'PB4': dio_ft_model}),
+      PinResource('PB5', {'PB5': dio_ft_model}),
+      PinResource('PB6', {'PB6': dio_ftf_model}),
+      PinResource('PB7', {'PB7': dio_ftf_model}),
 
-      # PinResource('NRST', {'NRST': dio_std_model}),  # non-mappable to IO!
+      PinResource('PB8', {'PB8': dio_ftf_model}),
+      PinResource('PB9', {'PB9': dio_ftf_model}),
 
       PeripheralFixedResource('USART2', uart_model, {
-        'tx': ['PA2', 'PD5'], 'rx': ['PA3', 'PD6']
+        'tx': ['PA2', 'PA14', 'PB3'], 'rx': ['PA3', 'PA15', 'PB4']
       }),
       PeripheralFixedResource('SPI1', spi_model, {
         'sck': ['PA5', 'PB3'], 'miso': ['PA6', 'PB4'], 'mosi': ['PA7', 'PB5']
       }),
-      PeripheralFixedResource('USART3', uart_model, {
-        'tx': ['PB10', 'PD8', 'PC10'], 'rx': ['PB11', 'PD9', 'PC11']
-      }),
-      PeripheralFixedResource('I2C2', i2c_model, {
-        'scl': ['PB10'], 'sda': ['PB11']
-      }),
-      PeripheralFixedResource('SPI2', spi_model, {
-        'sck': ['PB13'], 'miso': ['PB14'], 'mosi': ['PB15']
+      PeripheralFixedResource('USART3', uart_model, {  # 1/3 check
+        'tx': ['PB10', 'PC10', 'PB9'], 'rx': ['PB11', 'PC11', 'PB8']
       }),
       PeripheralFixedResource('USART1', uart_model, {
         'tx': ['PA9', 'PB6'], 'rx': ['PA10', 'PB7']
       }),
       PeripheralFixedResource('CAN', CanControllerPort(DigitalBidir.empty()), {
-        'tx': ['PA12', 'PD1', 'PB9'], 'rx': ['PA11', 'PD0', 'PB8']
-      }),
-      PeripheralFixedResource('USB', UsbDevicePort(DigitalBidir.empty()), {
-        'dm': ['PA11'], 'dp': ['PA12']
-      }),
-      PeripheralFixedPin('SWD', SwdTargetPort(dio_std_model), {  # TODO most are FT pins
-        'swdio': ['PA13'], 'swclk': ['PA14'], 'reset': ['NRST'], 'swo': ['PB3'],
+        'tx': ['PA12', 'PB9'], 'rx': ['PA11', 'PB8']
       }),
       PeripheralFixedResource('I2C1', i2c_model, {
-        'scl': ['PB6', 'PB8'], 'sda': ['PB7', 'PB9']
+        'scl': ['PA15', 'PB6', 'PB8'], 'sda': ['PA14', 'PB7', 'PB9']
+      }),
+
+      PeripheralFixedPin('SWD', SwdTargetPort(dio_ft_model), {  # TODO some are FTf pins
+        'swdio': ['PA13'], 'swclk': ['PA14'], 'reset': ['NRST'], 'swo': ['PB3'],
       }),
     ])
 
 
-class Nucleo_F303k8(Microcontroller, FootprintBlock, AssignablePinBlock):  # TODO refactor with _Device
+class Nucleo_F303k8_new(PinMappable, GeneratorBlock, FootprintBlock):
+  """Nucleo32 F303K8 configured as power source from USB.
+  TODO: this should be IoController, but this has a power output (instead of input) pin"""
   RESOURCE_PIN_REMAP = {
-    'PC13': '2',
-    'PC14': '3',
-    'PC15': '4',
-    'NRST': '7',
+    'PA9': '1',  # CN3.1, D1
+    'PA10': '2',  # CN3.2, D0
+    # 'NRST': '3'  # CN3.3, RESET
+    'PA12': '5',  # CN3.5, D2
+    'PB0': '6',  # CN3.6, D3
+    'PB7': '7',  # CN3.7, D4
+    'PB6': '8',  # CN3.8, D5
+    'PB1': '9',  # CN3.9, D6
+    'PF0': '10',  # CN3.10, D7
+    'PF1': '11',  # CN3.11, D8
+    'PA8': '12',  # CN3.12, D9
+    'PA11': '13',  # CN3.13, D10
+    'PB5': '14',  # CN3.14, D11
+    'PB4': '15',  # CN3.15, D12
 
-    'PA0': '10',
-    'PA1': '11',
-    'PA2': '12',
-    'PA3': '13',
-    'PA4': '14',
-    'PA5': '15',
-    'PA6': '16',
-    'PA7': '17',
-    'PB0': '18',
-    'PB1': '19',
-
-    'PB2': '20',
-    'PB10': '21',
-    'PB11': '22',
-    'PB12': '25',
-    'PB13': '26',
-    'PB14': '27',
-    'PB15': '28',
-
-    'PA8': '29',
-    'PA9': '30',
-    'PA10': '31',
-    'PA11': '32',
-    'PA12': '33',
-    'PA13': '34',
-
-    'PA14': '37',
-    'PA15': '38',
-    'PB3': '39',
-    'PB4': '40',
-    'PB5': '41',
-    'PB6': '42',
-    'PB7': '43',
-
-    'PB8': '45',
-    'PB9': '46',
+    # 'NRST': '18'  # CN4.3, RESET
+    'PA2': '20',  # CN4.5, A7
+    'PA7': '21',  # CN4.6, A6
+    'PA6': '22',  # CN4.7, A5
+    'PA5': '23',  # CN4.8, A4
+    'PA4': '24',  # CN4.9, A3
+    'PA3': '25',  # CN4.10, A2
+    'PA1': '26',  # CN4.11, A1
+    'PA0': '27',  # CN4.12, A0
+    'PB3': '30',  # CN4.15, D13
   }
-  PACKAGE = 'Package_QFP:LQFP-48_7x7mm_P0.5mm'
-  PART = 'STM32F103xxT6'
 
-
-class Nucleo_F303k8_new(PinMappable, IoController, GeneratorBlock, FootprintBlock):
-  """Nucleo32 F303K8 configured as power source from USB."""
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
-    self.usb.defined()  # no usb support
+
+    self.pwr_in = self.Port(VoltageSink(
+      voltage_limits=(5.1 + 1.2 + 0.58, 15) * Volt,  # lower from 5v out + LD1117S50TR dropout + BAT60JFILM diode
+      # TODO can be lower if don't need 5.0v out
+      current_draw=(0, 0) * Amp # TODO current draw specs, the part doesn't really have a datasheet
+    ), optional=True)
+    self.pwr_5v = self.Port(VoltageSource(
+      voltage_out=(4.75 - 0.58, 5.1) * Volt,  # 4.75V USB - 0.58v BAT60JFILM drop to 5.1 from LD1117S50TR, ignoring ST890CDR
+      current_limits=(0, 0.5) * Amp  # max USB draw  # TODO higher from external power
+    ), optional=True)
+    self.pwr_3v3 = self.Port(VoltageSource(
+      voltage_out=3.3 * Volt(tol=0.03),  # LD39050PU33R worst-case Vout accuracy
+      current_limits=(0, 0.5) * Amp  # max USB current draw, LDO also guarantees 500mA output current
+    ), optional=True)
+    self.gnd = self.Port(GroundSource(), optional=True)
+
+    self.gpio = self.Port(Vector(DigitalBidir.empty()), optional=True)
+    self.adc = self.Port(Vector(AnalogSink.empty()), optional=True)
+    self.dac = self.Port(Vector(AnalogSource.empty()), optional=True)
+
+    self.spi = self.Port(Vector(SpiMaster.empty()), optional=True)
+    self.i2c = self.Port(Vector(I2cMaster.empty()), optional=True)
+    self.uart = self.Port(Vector(UartPort.empty()), optional=True)
+    self.can = self.Port(Vector(CanControllerPort.empty()), optional=True)
+
+    self.generator(self.generate, self.pin_assigns,
+                   self.gpio.allocated(), self.adc.allocated(), self.dac.allocated(),
+                   self.spi.allocated(), self.i2c.allocated(), self.uart.allocated(),
+                   self.can.allocated())
 
   def generate(self, assignments: str,
                gpio_allocates: List[str], adc_allocates: List[str], dac_allocates: List[str],
                spi_allocates: List[str], i2c_allocates: List[str], uart_allocates: List[str],
                can_allocates: List[str]) -> None:
+    system_pins = {
+      '16': self.pwr_in,
+      '19': self.pwr_5v,
+      '29': self.pwr_3v3,
+      '4': self.gnd,
+      '17': self.gnd,
+    }
+
+    allocated = self.abstract_pinmaps.remap_pins(self.RESOURCE_PIN_REMAP).allocate([
+      (SpiMaster, spi_allocates), (I2cMaster, i2c_allocates),
+      (UartPort, uart_allocates), (CanControllerPort, can_allocates),
+      (AnalogSink, adc_allocates), (AnalogSource, dac_allocates), (DigitalBidir, gpio_allocates),
+    ], assignments)
+
+    io_pins = self._instantiate_from([self.gpio, self.adc, self.dac, self.spi, self.i2c, self.uart,
+                                      self.usb, self.can, self.swd],
+                                     allocated)
     self.footprint(
       'U', 'edg:Nucleo32',
-      all_pins,
+      dict(chain(system_pins.items(), io_pins.items())),
       mfr='STMicroelectronics', part='NUCLEO-F303K8',
       datasheet='https://www.st.com/resource/en/user_manual/dm00231744.pdf',
     )
