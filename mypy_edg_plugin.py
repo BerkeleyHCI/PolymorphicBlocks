bbd170744bac204305ee12318c2930a4f129a7a0
Richard Lin <elpato25@gmail.com>
2022-04-16 16:52:16 -0700

    Update mypy_edg_plugin.py

--- mypy_edg_plugin.py
+++ mypy_edg_plugin.py
@@ -12,25 +12,39 @@ class CustomPlugin(Plugin):
         return base.args
     return None
 
-  def get_function_hook(self, fullname: str
-                        ) -> Optional[Callable[[mypy.plugin.FunctionContext], Type]]:
-    def inner(fn: mypy.plugin.FunctionContext) -> mypy.types.Type:
-      assert isinstance(fn.default_return_type, mypy.types.CallableType)
-      print(fn.default_return_type.arg_types)
-      for (i, arg_type) in enumerate(fn.default_return_type.arg_types):
-        if isinstance(arg_type, mypy.types.Instance):
-          assignable_type = self._type_args_of_base(arg_type.type, 'ConstraintAssignable')
-          if assignable_type is not None:
-            print(assignable_type)
+  # def get_function_hook(self, fullname: str
+  #                       ) -> Optional[Callable[[mypy.plugin.FunctionContext], Type]]:
+  #   def inner(fn: mypy.plugin.FunctionContext) -> mypy.types.Type:
+  #     assert isinstance(fn.default_return_type, mypy.types.CallableType)
+  #     print(fn.default_return_type.arg_types)
+  #     for (i, arg_type) in enumerate(fn.default_return_type.arg_types):
+  #       if isinstance(arg_type, mypy.types.Instance):
+  #         assignable_type = self._type_args_of_base(arg_type.type, 'ConstraintAssignable')
+  #         if assignable_type is not None:
+  #           print(assignable_type)
+  #
+  #           fn.default_return_type.arg_types[i] = assignable_type[0]  # first type argument is the assignable types
+  #
+  #     print(fn.default_return_type.arg_types)
+  #
+  #     return fn.default_return_type
+  #
+  #   if 'init_in_parent' in fullname:
+  #
+  #     return inner
+  #   return None
 
-            fn.default_return_type.arg_types[i] = assignable_type[0]  # first type argument is the assignable types
+  def get_method_signature_hook(self, fullname: str) -> Optional[Callable[[mypy.plugin.MethodSigContext], Type]]:
+    def inner(method: mypy.plugin.MethodSigContext) -> mypy.types.Type:
+      print(method.default_signature)
+      for arg in method.args:
+        print(arg)
+        # assert len(arg_type) == 1
+        # print(f"{arg_type}: {type(arg_type)}")
 
-      print(fn.default_return_type.arg_types)
+      return method.default_signature
 
-      return fn.default_return_type
-
-    if 'init_in_parent' in fullname:
-
+    if fullname.split('.')[-1] == 'generator':
       return inner
     return None
 
