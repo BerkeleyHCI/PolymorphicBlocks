9e70d05b58d45ddd98657dbab617032e2fbe9904
Richard Lin <elpato25@gmail.com>
2022-04-24 01:21:23 -0700

    add JLC pruning script and output

--- electronics_lib/resources/README.md
+++ electronics_lib/resources/README.md
@@ -4,6 +4,7 @@ This folder contains parts tables used by some components.
 The full table (274MB) was downloaded on April 24, 2022, and is [available on the Internet Archive here](https://web.archive.org/web/20220424072635/https://jlcpcb.com/componentSearch/uploadComponentInfo).
 
 Only a pruned version is included in this repository, to reduce cloning and compilation time.
+See jlcprune.py for the script used to produce the pruned file.
 
 ## DigiKey Parts Library (deprecated)
 Some older example designs used parts tables from DigiKey using their CSV export.
---
+++ electronics_lib/resources/jlcprune.py
@@ -0,0 +1,61 @@
+KEEP_CATEGORIES = [
+  'Chip Resistor - Surface Mount',
+  'Current Sense Resistors/Shunt Resistors',
+  'Low Resistors & Current Sense Resistors - Surface Mount',
+  'Resistor Networks & Arrays',
+  'Through Hole Resistors',
+
+
+  'Aluminum Electrolytic Capacitors - Leaded',
+  'Aluminum Electrolytic Capacitors - SMD',
+  'Multilayer Ceramic Capacitors MLCC - Leaded',
+  'Multilayer Ceramic Capacitors MLCC - SMD/SMT',
+  'Tantalum Capacitors',
+
+  'Fuse Holders',
+  'Fuses',
+  'Resettable Fuses',
+
+  'Crystals',
+
+  'Diodes - Fast Recovery Rectifiers',
+  'Diodes - General Purpose',
+  'Schottky Barrier Diodes (SBD)',
+  'Switching Diode',
+
+  'Zener Diodes',
+
+  'Inductors (SMD)',
+  'Power Inductors',
+  'Through Hole Inductors',
+
+  'Infrared (IR) LEDs',
+  'Light Emitting Diodes (LED)',
+  'Ultra Violet LEDs',
+
+  'Bipolar Transistors - BJT',
+  'MOSFETs',
+]
+
+MIN_STOCK = 100  # arbitrary cutoff to prune rare parts
+
+FILE = 'JLCPCB SMT Parts Library(20220419).csv'
+
+if __name__ == '__main__':
+  # Not using a standard CSV parser to preserve the original formatting as much as possible
+  with open(f'{FILE}', 'rb') as full_file, open(f'Pruned_{FILE}', 'wb') as out_file:
+    header = full_file.readline()
+    out_file.writelines([header])
+    rows = str(header.decode('gb2312').strip()).split(',')
+    CATEGORY_INDEX = rows.index('Second Category')
+    STOCK_INDEX = -2  # hard-coded because str.split doesn't account for quoted elements
+
+    wrote_lines = 0
+    for i, line in enumerate(full_file.readlines()):
+      values = str(line.decode('gb2312').strip()).split(',')
+      if values[CATEGORY_INDEX] in KEEP_CATEGORIES and \
+          values[STOCK_INDEX] and int(values[STOCK_INDEX]) >= MIN_STOCK:
+        out_file.writelines([line])
+        wrote_lines += 1
+
+  print(f"read {i} lines, wrote {wrote_lines} lines")
