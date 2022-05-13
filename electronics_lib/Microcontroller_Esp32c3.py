1833091f6636b67e8cd1ba447f580c12bcba0a8b
Richard Lin <elpato25@gmail.com>
2022-05-13 01:59:53 -0700

    wip esp32c3

---
+++ electronics_lib/Microcontroller_Esp32c3.py
@@ -0,0 +1,132 @@
+from itertools import chain
+from typing import *
+
+from electronics_abstract_parts import *
+
+
+@abstract_block
+class Esp32c3_Device(PinMappable, IoController, DiscreteChip, GeneratorBlock, FootprintBlock):
+  """Base class for ESP32-C3 series devices, with RISC-V core, 2.4GHz WiF,i, BLE5, and USB.
+  PlatformIO: use board ID esp32-c3-devkitm-1
+
+  Chip datasheet: https://espressif.com/sites/default/files/documentation/esp32-c3_datasheet_en.pdf
+  """
+  def __init__(self, **kwargs) -> None:
+    super().__init__(**kwargs)
+
+    self.pwr.init_from(VoltageSink(
+      voltage_limits=(3.0, 3.6)*Volt,  # section 4.2
+      current_draw=(0.001, 335) * mAmp  # section 4.6, from power off to RF active
+      # TODO propagate current consumption from IO ports
+    ))
+    self.gnd.init_from(Ground())
+
+    self.dio_model = dio_model = DigitalBidir.from_supply(  # table 4.4
+      self.gnd, self.pwr,
+      voltage_limit_tolerance=(-0.3, 0.3) * Volt,
+      current_limits=(-28, 40)*mAmp,
+      current_draw=(0, 0)*Amp,
+      input_threshold_factor=(0.25, 0.75),
+      pullup_capable=True, pulldown_capable=True,
+    )
+
+    # section 2.4: strapping IOs that need a fixed value to boot, and currently can't be allocated as GPIO
+    self.en = self.Port(dio_model)  # needs external pullup
+    self.io2 = self.Port(dio_model)  # needs external pullup
+    self.io8 = self.Port(dio_model)  # needs external pullup, may control prints
+    self.io9 = self.Port(dio_model)  # internally pulled up for SPI boot, connect to GND for download
+
+    # similarly, the programming UART is fixed and allocated separately
+    self.uart0 = self.Port(UartPort(dio_model))
+
+    # TODO add JTAG support
+
+    self.generator(self.generate, self.pin_assigns,
+                   self.gpio.allocated(), self.adc.allocated(), self.dac.allocated(),
+                   self.spi.allocated(), self.i2c.allocated(), self.uart.allocated(),
+                   self.usb.allocated(), self.swd.is_connected())
+
+  @staticmethod
+  def mappable_ios(dio_model: DigitalBidir) -> PinMapUtil:
+    adc_model = AnalogSink(
+      voltage_limits=(0, 2.5) * Volt,  # table 15, effective ADC range
+      current_draw=(0, 0) * Amp,
+      # TODO: impedance / leakage - not specified by datasheet
+    )
+
+    uart_model = UartPort(DigitalBidir.empty())
+    spi_model = SpiMaster(DigitalBidir.empty(), (0, 60)*MHertz)  # section 3.4.2, max block in GP master mode
+    i2c_model = I2cMaster(DigitalBidir.empty())  # section 3.4.4, supporting 100/400 and up to 800 kbit/s
+
+    return PinMapUtil([  # section 2.2
+      PinResource('GPIO0', {'GPIO0': dio_model, 'ADC1_CH0': adc_model}),  # also XTAL_32K_P
+      PinResource('GPIO1', {'GPIO1': dio_model, 'ADC1_CH1': adc_model}),  # also XTAL_32K_N
+      PinResource('GPIO2', {'GPIO2': dio_model, 'ADC1_CH2': adc_model}),
+      PinResource('GPIO3', {'GPIO3': dio_model, 'ADC1_CH3': adc_model}),
+      PinResource('MTMS', {'GPIO4': dio_model, 'ADC1_CH4': adc_model}),
+      PinResource('MTDI', {'GPIO5': dio_model, 'ADC2_CH0': adc_model}),
+      PinResource('MTCK', {'GPIO6': dio_model}),
+      PinResource('MTDO', {'GPIO7': dio_model}),
+      PinResource('GPIO8', {'GPIO8': dio_model}),
+      PinResource('GPIO9', {'GPIO9': dio_model}),
+      PinResource('GPIO10', {'GPIO10': dio_model}),
+      PinResource('VDD_SPI', {'GPIO11': dio_model}),
+      # SPI pins skipped - internal to the modules supported so far
+      PinResource('GPIO18', {'GPIO18': dio_model}),
+      PinResource('GPIO19', {'GPIO19': dio_model}),
+      PinResource('GPIO20', {'GPIO20': dio_model}),
+      PinResource('GPIO21', {'GPIO21': dio_model}),
+
+      # peripherals in section 3.11
+      PeripheralFixedResource('U0', uart_model, {
+        'txd': ['GPIO21'], 'rxd': ['GPIO20']
+      }),
+      PeripheralAnyResource('U1', uart_model),
+      PeripheralAnyResource('I2C', i2c_model),
+      PeripheralAnyResource('SPI2', spi_model),
+      PeripheralFixedResource('USB', UsbDevicePort(), {
+        'dp': ['GPIO19'], 'dm': ['DPIO18']
+      }),
+    ])
+
+
+class Esp32c3_Wroom02_Device(Esp32c3_Device, FootprintBlock):
+  """ESP32C module
+
+  Module datasheet: https://www.espressif.com/sites/default/files/documentation/esp32-c3-wroom-02_datasheet_en.pdf
+  """
+
+
+class Esp32c3_Wroom02(PinMappable, Microcontroller, IoController, Block):
+  """Wrapper around Esp32c3_Wroom02 with external capacitors and UART programming header."""
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self.ic = self.Block(Esp32c3_Wroom02_Device(pin_assigns=self.pin_assigns))
+
+    self.generator(self.generate, self.usb.allocated())
+
+  def contents(self, usb_allocated: List[str]) -> None:
+    super().contents()
+    self.connect(self.pwr, self.ic.pwr)
+    self.connect(self.gnd, self.ic.gnd)
+    self._export_ios_from(self.ic, excludes=[self.usb])
+
+    with self.implicit_connect(
+        ImplicitConnect(self.pwr, [Power]),
+        ImplicitConnect(self.gnd, [Common])
+    ) as imp:
+      self.vcc_cap0 = imp.Block(DecouplingCapacitor(10 * uFarad(tol=0.2)))  # C1
+      self.vcc_cap1 = imp.Block(DecouplingCapacitor(0.1 * uFarad(tol=0.2)))  # C2
+
+      # Note: IO4/5/6/7, 10/0/1/3 free
+      # IO18/19 used for USB (optional) - total 8-10 IOs that aren't strapping pins
+
+
+      # TBD R8 PUR to IO8
+      # TBD Boot option jumper JP2 to IO9
+      # TBD UART programming header JP4
+      # TBD R0 PUR to IO2
+      # TBD PUR to EN, optional RC circuit R=10k, C=1uF, recommended RC delay
+      # Note strapping pins (section 3.3) IO2, 8, 9; IO9 is internally pulled up
+      # IO9 is 1 for SPI boot and 0 for download boot
+      # IO2 must be 1 for both SPI and download boot, while IO8 must be 1 for download boot
