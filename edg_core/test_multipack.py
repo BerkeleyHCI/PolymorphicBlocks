42ed54da4c059bb66152691337f235a62d916162
Richard Lin <elpato25@gmail.com>
2022-05-28 19:14:01 -0700

    use sinks instead

--- edg_core/test_multipack.py
+++ edg_core/test_multipack.py
@@ -2,39 +2,41 @@ import unittest
 
 import edgir
 from . import *
-from .test_elaboration_common import TestPortSource, TestBlockSink, TestBlockSource
+from .test_elaboration_common import TestPortSink, TestBlockSink, TestBlockSource
 
 
-class MultipackBlockSource(MultipackBlock):
+class MultipackBlockSink(MultipackBlock):
   """Unlike a real multipack block, this is simplified and has no implementation."""
   def __init__(self):
     super().__init__()
-    self.source_port1 = self.Port(TestPortSource())
-    self.source_port2 = self.Port(TestPortSource())
-    self.source1 = self.PackedPart(TestBlockSource())
-    self.source2 = self.PackedPart(TestBlockSource())
-    self.packed_connect(self.source_port1, self.source1.source)
-    self.packed_connect(self.source_port2, self.source2.source)
+    self.sink_port1 = self.Port(TestPortSink())
+    self.sink_port2 = self.Port(TestPortSink())
+    self.sink1 = self.PackedPart(TestBlockSink())
+    self.sink2 = self.PackedPart(TestBlockSink())
+    self.packed_connect(self.sink_port1, self.sink1.sink)
+    self.packed_connect(self.sink_port2, self.sink2.sink)
 
 
-class TestBlockContainerSource(Block):
+class TestBlockContainerSink(Block):
   def __init__(self) -> None:
     super().__init__()
-    self.inner = self.Block(TestBlockSource())
-    self.source = self.Export(self.inner.source)
+    self.inner = self.Block(TestBlockSink())
+    self.sink = self.Export(self.inner.sink)
 
 
 class TopMultipackDesign(DesignTop):
   def contents(self):
     super().contents()
+    self.source = self.Block(TestBlockSource())
+    self.sink1 = self.Block(TestBlockSink())
+    self.sink2 = self.Block(TestBlockContainerSink())
 
-    self.source1 = self.Block(TestBlockSource())
-    self.source2 = self.Block(TestBlockContainerSource())
+    self.connect(self.source.source, self.sink1.sink, self.sink2.sink)
 
   def multipack(self):
-    self.packed = self.Block(MultipackBlockSource())
-    self.pack(self.packed.source1, ['source1'])
-    self.pack(self.packed.source2, ['source2', 'inner'])
+    self.packed = self.Block(MultipackBlockSink())
+    self.pack(self.packed.sink1, ['sink1'])
+    self.pack(self.packed.sink2, ['sink2', 'inner'])
 
 
 class TopMultipackDesignTestCase(unittest.TestCase):
