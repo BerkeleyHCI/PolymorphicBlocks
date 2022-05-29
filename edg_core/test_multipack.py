73514889ee068aa54deb2f2346892b94e0e31e43
Richard Lin <elpato25@gmail.com>
2022-05-29 01:21:09 -0700

    add parameter to test case

--- edg_core/test_multipack.py
+++ edg_core/test_multipack.py
@@ -5,30 +5,43 @@ from . import *
 from .test_elaboration_common import TestPortSink, TestBlockSink, TestBlockSource
 
 
+class PartSink(Block):
+  def __init__(self) -> None:
+    super().__init__()
+    self.param = self.Parameter(FloatExpr())
+    self.sink = self.Port(TestPortSink(), optional=True)
+
+
 class MultipackBlockSink(MultipackBlock):
   """Unlike a real multipack block, this is simplified and has no implementation."""
   def __init__(self):
     super().__init__()
     self.sink_port1 = self.Port(TestPortSink())
     self.sink_port2 = self.Port(TestPortSink())
-    self.sink1 = self.PackedPart(TestBlockSink())
-    self.sink2 = self.PackedPart(TestBlockSink())
+    self.param1 = self.Parameter(FloatExpr())
+    self.param2 = self.Parameter(FloatExpr())
+    self.sink1 = self.PackedPart(PartSink())
+    self.sink2 = self.PackedPart(PartSink())
     self.packed_connect(self.sink_port1, self.sink1.sink)
     self.packed_connect(self.sink_port2, self.sink2.sink)
+    self.packed_assign(self.param1, self.sink1.param)
+    self.packed_assign(self.param2, self.sink2.param)
 
 
 class TestBlockContainerSink(Block):
   def __init__(self) -> None:
     super().__init__()
-    self.inner = self.Block(TestBlockSink())
+    self.inner = self.Block(PartSink())
+    self.param = self.Parameter(FloatExpr())
     self.sink = self.Export(self.inner.sink)
+    self.assign(self.inner.param, self.param)
 
 
 class TopMultipackDesign(DesignTop):
   def contents(self):
     super().contents()
     self.source = self.Block(TestBlockSource())
-    self.sink1 = self.Block(TestBlockSink())
+    self.sink1 = self.Block(PartSink())
     self.sink2 = self.Block(TestBlockContainerSink())
 
     self.connect(self.source.source, self.sink1.sink, self.sink2.sink)
