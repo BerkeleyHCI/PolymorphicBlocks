c32f51058a59f0f66dd766210f856a176b6b0bc5
Richard Lin <elpato25@gmail.com>
2022-06-10 22:22:59 -0700

    chairlieplexing + ignore .pio

--- examples/test_ledmatrix/firmware/main.cpp
+++ examples/test_ledmatrix/firmware/main.cpp
@@ -1,29 +1,68 @@
 #include <Arduino.h>
 
-const auto led0 = 4;  // GPIO4
-const auto led1 = 5;  // GPIO5
-const auto led2 = 6;  // GPIO6
-const auto led3 = 7;  // GPIO7
-const auto led4 = 1;  // GPIO1
-const auto led5 = 3;  // GPIO3
-const auto led6 = 10;  // GPIO10
-const auto sw1 = 0;  // GPIO0
+const uint8_t kNumLedDrivers = 7;
+const uint8_t kNumCols = 5;  // X size, each element goes through a resistor so only one may be lit
+const uint8_t kNumRows = 6;  // Y size
+const int LedDrivers[kNumLedDrivers] = {4, 5, 6, 7, 1, 3, 10};  // GPIO pin numbers
+const int sw1 = 0;  // GPIO0
+
+void charlieplex(const uint8_t arr[kNumCols * kNumRows]) {
+  for (size_t col=0; col<kNumCols; col++) {
+    digitalWrite(LedDrivers[col], 0);  // drive the cathode low
+    pinMode(LedDrivers[col], OUTPUT);
+
+    for (size_t driver=0; driver<kNumLedDrivers; driver++) {
+      if (driver != col) {
+        bool driveMode = false;
+        if (driver >= col) {
+          driveMode = arr[(driver - 1) * kNumCols + col];
+        } else {
+          driveMode = arr[driver * kNumCols + col];
+        }
+        
+        if (driveMode) {
+          digitalWrite(LedDrivers[driver], 1);
+          pinMode(LedDrivers[driver], OUTPUT);
+        } else {
+          pinMode(LedDrivers[driver], INPUT);
+        }
+      }
+    }
+    delay(2);
+
+    for (size_t driver=0; driver<kNumLedDrivers; driver++) {  // blank before driving the next column
+      pinMode(LedDrivers[driver], INPUT);
+    }
+  }
+}
 
 void setup() {
-  pinMode(led0, OUTPUT);
-  digitalWrite(led0, 0);
-  pinMode(led1, OUTPUT);
-  digitalWrite(led1, 1);
-  pinMode(led2, OUTPUT);
-  digitalWrite(led2, 1);
 }
   
 void loop() {
-  uint8_t lastMode = 0;
+  const uint8_t smile[kNumCols * kNumRows] = {
+    0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0,
+    1, 0, 0, 0, 1,
+    0, 1, 1, 1, 0,
+  };
+  const uint8_t frown[kNumCols * kNumRows] = {
+    0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    1, 0, 0, 0, 1,
+  };
+
+  uint64_t lastMillis = millis();
   while (1) {
-    pinMode(led3, OUTPUT);
-    digitalWrite(led3, lastMode);    
-    lastMode = !lastMode;
-    delay(100);
+    if (millis() % 2000 < 1000) {
+      charlieplex(smile);
+    } else {
+      charlieplex(frown);
+    }
   }
 }
