23a64541788b713f9d68ee08021ebf1293767757
Richard Lin <elpato25@gmail.com>
2022-08-10 12:27:24 -0700

    cleanup

--- electronics_model/footprint.py
+++ electronics_model/footprint.py
@@ -1,6 +1,6 @@
-from collections import namedtuple
-from typing import *
 import zlib  # for deterministic hash
+from typing import NamedTuple, List, Dict, Iterable
+
 
 class Block(NamedTuple):
   footprint: str
@@ -123,39 +123,5 @@ def net_exp(dict: Dict[str, Iterable[Pin]]) -> str:
 
 """4. Generate Full Netlist"""
 
-def generate_netlist(blocks_dict, nets_dict):
+def generate_netlist(blocks_dict: Dict[str, Block], nets_dict: Dict[str, Iterable[Pin]]) -> str:
     return gen_header() + '\n' + block_exp(blocks_dict) + '\n' + net_exp(nets_dict) + '\n' + ')'
-
-###############################################################################################################################################################################################
-
-"""5. Test Data"""
-
-blocks_dict = {'U1': Block('Package_DIP:DIP-4_W7.62mm', 'LM555', [], []),
-            'R3': Block('OptoDevice:R_LDR_4.9x4.2mm_P2.54mm_Vertical', '1k', [], []),
-            'R1': Block('OptoDevice:R_LDR_4.9x4.2mm_P2.54mm_Vertical', '4k7', [], []),
-            'R2': Block('OptoDevice:R_LDR_4.9x4.2mm_P2.54mm_Vertical', '10k', [], []),
-            'C1': Block('Capacitor_SMD:CP_Elec_3x5.3', '100\u03BCF', [], []),
-            'C2': Block('Capacitor_SMD:C_0201_0603Metric', '10n', [], []),
-            'D1': Block('LED_SMD:LED_0603_1608Metric_Pad1.05x0.95mm_HandSolder', 'LED', [], [])
-              }
-
-block_test1 = blocks_dict
-block_test2: Dict[str, Block] = {}
-block_test3 = {'A': Block('Capacitor', '10k', [], []), 'B': Pin('LED', 'LED')}
-block_test4 = {1: Block('Capacitor', '10k', [], []), 2: Block('LED', 'LED', [], [])}
-# Extra test case: block_test5 = {'A': Block('Capacitor', '10k'), 'B': Block('LED', 'LED')}
-
-
-nets_dict = {'Net-(R3-Pad1)': [Pin('U1', '3'), Pin('R3', '1')],
-            'Net-(C1-Pad1)': [Pin('U1', '2'), Pin('U1', '5'), Pin('U1', '6'), Pin('R2', '2'), Pin('C1', '1'), Pin('C2', '1')],
-            'Net-(R1-Pad2)': [Pin('R2', '1'), Pin('R1', '2'), Pin('U1', '7')],
-            '+9V': [Pin('R1', '1'), Pin('U1', '4'), Pin('U1', '8')],
-            'GND': [Pin('D1', '1'), Pin('C2', '2'), Pin('C1', '2'), Pin('U1', '1')],
-            'Net-(D1-Pad2)': [Pin('D1', '2'), Pin('R3', '2')]
-            }
-
-net_test1 = nets_dict
-net_test2: Dict[str, List[Pin]] = {}
-net_test3 = {'Net1': [Pin('U1', '3'), Pin('U2', '1')], 'Net2': [Block('R2', '4', [], []), Pin('R3', '1')]}
-net_test4 = {1: [Pin('U1', '3'), Pin('U2', '1')], 2: [Block('R2', '4', [], []), Pin('R3', '1')]}
-# Extra test case: net_test5 = {'Net3': [], 'Net4': [Pin('R2', '4'), Pin('R3', '1')]}
