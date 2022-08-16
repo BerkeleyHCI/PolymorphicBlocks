31e55caa6a3eaee5930f670469b434a51b6c9b92
Richard Lin <elpato25@gmail.com>
2022-08-16 15:59:21 -0700

    Add Hirose 0.5mm FPCs

--- electronics_lib/PassiveConnector.py
+++ electronics_lib/PassiveConnector.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Iterable
 from electronics_abstract_parts import *
 
 
@@ -11,7 +11,7 @@ class PassiveConnector(Connector, GeneratorBlock, FootprintBlock):
   than the maximum pin index (but can be smaller, unassigned pins are NC).
   The allocated pin names correlate with the footprint pin, 1-indexed (per electronics convention).
   It is up to the instantiating layer to set the pinmap (or allow the user to set it by refinements)."""
-  allowed_pins: Tuple[int, int] = (0, 0)  # inclusive on both ends, default disallowed anywhere
+  allowed_pins: Iterable[int]
 
   @init_in_parent
   def __init__(self, length: IntLike = 0):
@@ -38,10 +38,8 @@ class PassiveConnector(Connector, GeneratorBlock, FootprintBlock):
     self.assign(self.actual_length, length)
     self.require(max_pin_index <= self.actual_length,
                  f"maximum pin index {max_pin_index} over requested length {length}")
-    self.require(self.actual_length >= self.allowed_pins[0],
-                 f"requested length {length} below allowed range {self.allowed_pins[0]} - {self.allowed_pins[1]}")
-    self.require(self.actual_length <= self.allowed_pins[1],
-                 f"requested length {length} above allowed range {self.allowed_pins[0]} - {self.allowed_pins[1]}")
+    # TODO ideally this is require, but we don't support set ops in the IR
+    assert length in self.allowed_pins, f"requested length {length} outside allowed length {self.allowed_pins}"
 
     (footprint, mfr, part) = self.part_footprint_mfr_name(length)
     self.footprint(
@@ -53,7 +51,7 @@ class PassiveConnector(Connector, GeneratorBlock, FootprintBlock):
 
 class PinHeader254(PassiveConnector, FootprintBlock):
   """Generic 2.54mm pin header in vertical through-hole."""
-  allowed_pins = (2, 16)
+  allowed_pins = range(2, 16+1)
   def part_footprint_mfr_name(self, length: int) -> Tuple[str, str, str]:
     return (f'Connector_PinHeader_2.54mm:PinHeader_1x{length:02d}_P2.54mm_Vertical',
             "Generic", f"PinHeader2.54 1x{length}")
@@ -61,7 +59,7 @@ class PinHeader254(PassiveConnector, FootprintBlock):
 
 class JstPhK(PassiveConnector, FootprintBlock):
   """JST PH-K series connector: 2.00mm shrouded and polarized, in vertical through-hole."""
-  allowed_pins = (2, 16)
+  allowed_pins = range(2, 16+1)
   def part_footprint_mfr_name(self, length: int) -> Tuple[str, str, str]:
     return (f'Connector_JST:JST_PH_B{length}B-PH-K_1x{length:02d}_P2.00mm_Vertical',
             "JST", f"B{length}B-PH-K")
@@ -70,7 +68,7 @@ class JstPhK(PassiveConnector, FootprintBlock):
 class MolexSl(PassiveConnector, FootprintBlock):
   """Molex SL series connector: 2.54mm shrouded and polarized, in vertical through-hole.
   Breadboard wire compatible - especially for debugging in a pinch."""
-  allowed_pins = (2, 25)
+  allowed_pins = range(2, 25+1)
   def part_footprint_mfr_name(self, length: int) -> Tuple[str, str, str]:
     return (f'Connector_Molex:Molex_SL_171971-00{length:02d}_1x{length:02d}_P2.54mm_Vertical',
             "Molex", f"171971-00{length:02d}_1x{length:02d}")
@@ -82,11 +80,15 @@ class Fpc050(PassiveConnector):
 
 
 @abstract_block
-class HiroseFh12sh(Fpc050, FootprintBlock):
+class HiroseFh12(Fpc050, FootprintBlock):
   """Hirose FH12 FFC/FPC connector, 0.50mm pitch horizontal bottom contacts."""
-  allowed_pins = (2, 25)
+  # positions the FH12 exists in, per https://www.hirose.com/product/series/FH12
+  _fh12_positions = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 24, 25, 26, 28, 29,
+                     30, 32, 33, 34, 35, 36, 40, 42, 45, 49, 50, 53, 60}
+  # positions for which there are KiCad footprints
+  _kicad_positions = {6, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 24, 25, 26, 28,
+                      30, 32, 33, 34, 35, 36, 40, 45, 50, 53}
+  allowed_positions = _fh12_positions.intersection(_kicad_positions)
   def part_footprint_mfr_name(self, length: int) -> Tuple[str, str, str]:
-    return (f'Connector_Molex:Molex_SL_171971-00{length:02d}_1x{length:02d}_P2.54mm_Vertical',
-            "Molex", f"171971-00{length:02d}_1x{length:02d}")
-
-    Hirose_FH12-6S-0.5SH_1x06-1MP_P0.50mm_Horizontal
+    return (f'Connector_FFC-FPC:Hirose_FH12-{length}S-0.5SH_1x{length:02d}-1MP_P0.50mm_Horizontal',
+            "Hirose", f"FH12-{length}S-0.5SH")
