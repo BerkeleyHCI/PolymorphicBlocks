b1cc0da7f6f99c86108d57bda982348c34632d48
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2022-09-14 18:41:21 -0700

    motor + controller works in arduino IDE

---
+++ examples/test_robotdriver2/firmware/PS3_Arduino_code/PS3_Arduino_code.ino
@@ -0,0 +1,127 @@
+#include <Ps3Controller.h>
+
+// Motor Pins
+const int motor1A1 = 18;
+const int motor1A2 = 19;
+const int motor1B1 = 22;
+const int motor1B2 = 21;
+const int motor2A1 = 4;
+const int motor2A2 = 16;
+const int motor2B1 = 5;
+const int motor2B2 = 17;
+
+// PWM Properties
+const int freq = 30000;
+const int resolution = 8; // range of [0-255]
+
+int player = 0;
+int battery = 0;
+
+void setMotor1A(int PWM) {    // range [0-255]
+
+    if (PWM > 0) 
+  {
+    digitalWrite(motor1A2, 0);
+    ledcWrite(0, abs(PWM));
+  }
+
+  else if (PWM < 0)
+  {
+    digitalWrite(motor1A1, 0);
+    ledcWrite(1, abs(PWM));
+  }
+  else
+  {
+    digitalWrite(motor1A1, 0);
+    digitalWrite(motor1A2, 0);
+  }
+}
+
+void notify()
+{
+  //---------------------- Battery events ---------------------
+  if ( battery != Ps3.data.status.battery ) {
+    battery = Ps3.data.status.battery;
+    Serial.print("The controller battery is ");
+    if ( battery == ps3_status_battery_charging )      Serial.println("charging");
+    else if ( battery == ps3_status_battery_full )     Serial.println("FULL");
+    else if ( battery == ps3_status_battery_high )     Serial.println("HIGH");
+    else if ( battery == ps3_status_battery_low)       Serial.println("LOW");
+    else if ( battery == ps3_status_battery_dying )    Serial.println("DYING");
+    else if ( battery == ps3_status_battery_shutdown ) Serial.println("SHUTDOWN");
+    else Serial.println("UNDEFINED");
+  }
+}
+
+void onConnect() {
+  Serial.println("Connected.");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(motor1A1, OUTPUT);
+  pinMode(motor1A2, OUTPUT);
+  pinMode(motor1B1, OUTPUT);
+  pinMode(motor1B2, OUTPUT);
+  pinMode(motor2A1, OUTPUT);
+  pinMode(motor2A2, OUTPUT);
+  pinMode(motor2B1, OUTPUT);
+  pinMode(motor2B2, OUTPUT);
+
+  // Assign LED pins to a PWM channel
+  ledcAttachPin(motor1A1, 0);
+  ledcAttachPin(motor1A2, 1);
+  ledcAttachPin(motor1B1, 2);
+  ledcAttachPin(motor1B2, 3);
+  ledcAttachPin(motor1A1, 4);
+  ledcAttachPin(motor2A2, 5);
+  ledcAttachPin(motor2B1, 6);
+  ledcAttachPin(motor2B2, 7);
+
+  // configure LED PWM functionalitites
+  ledcSetup(0, freq, resolution);
+  ledcSetup(1, freq, resolution);
+  ledcSetup(2, freq, resolution);
+  ledcSetup(3, freq, resolution);
+  ledcSetup(4, freq, resolution);
+  ledcSetup(5, freq, resolution);
+  ledcSetup(6, freq, resolution);
+  ledcSetup(7, freq, resolution);
+
+  Ps3.attach(notify);
+  Ps3.attachOnConnect(onConnect);
+  Ps3.begin("b0:fc:36:5b:cf:08"); // Nathan's PS3 Controller
+
+  Serial.println("Ready.");
+}
+
+void loop()
+{
+  if (!Ps3.isConnected())
+    return;
+
+  if (Ps3.data.analog.stick.ly < 0)   // Pulling the stick up makes ly -> negative
+  {
+    digitalWrite(motor1A2, 0);
+    ledcWrite(0, abs(Ps3.data.analog.stick.ly / 2));
+    Serial.println("FORWARD");
+  }
+
+  else if (Ps3.data.analog.stick.ly > 0)
+  {
+    digitalWrite(motor1A1, 0);
+    ledcWrite(1, abs(Ps3.data.analog.stick.ly / 2));
+    Serial.println("BACKWARD");
+  }
+  else
+  {
+    digitalWrite(motor1A1, 0);
+    digitalWrite(motor1A2, 0);
+    Serial.println("BRAKE");
+  }
+
+
+  delay(10);
+}
--- examples/test_robotdriver2/firmware/main.cpp
+++ examples/test_robotdriver2/firmware/main.cpp
@@ -6,7 +6,10 @@
 // based on SSD1306 example:
 // https://github.com/adafruit/Adafruit_SSD1306/blob/master/examples/ssd1306_128x32_spi/ssd1306_128x32_spi.ino
 #include <Adafruit_SSD1306.h>
+#include <Ps3Controller.h>
 
+int player = 0;
+int battery = 0;
 
 const int motor1A1 = 18;
 const int motor1A2 = 19;
@@ -38,12 +41,204 @@ const char *password = "password";
  
 WiFiServer server(80);
 
+
+void notify()
+{
+    //--- Digital cross/square/triangle/circle button events ---
+    if( Ps3.event.button_down.cross )
+        Serial.println("Started pressing the cross button");
+    if( Ps3.event.button_up.cross )
+        Serial.println("Released the cross button");
+
+    if( Ps3.event.button_down.square )
+        Serial.println("Started pressing the square button");
+    if( Ps3.event.button_up.square )
+        Serial.println("Released the square button");
+
+    if( Ps3.event.button_down.triangle )
+        Serial.println("Started pressing the triangle button");
+    if( Ps3.event.button_up.triangle )
+        Serial.println("Released the triangle button");
+
+    if( Ps3.event.button_down.circle )
+        Serial.println("Started pressing the circle button");
+    if( Ps3.event.button_up.circle )
+        Serial.println("Released the circle button");
+
+    //--------------- Digital D-pad button events --------------
+    if( Ps3.event.button_down.up )
+        Serial.println("Started pressing the up button");
+    if( Ps3.event.button_up.up )
+        Serial.println("Released the up button");
+
+    if( Ps3.event.button_down.right )
+        Serial.println("Started pressing the right button");
+    if( Ps3.event.button_up.right )
+        Serial.println("Released the right button");
+
+    if( Ps3.event.button_down.down )
+        Serial.println("Started pressing the down button");
+    if( Ps3.event.button_up.down )
+        Serial.println("Released the down button");
+
+    if( Ps3.event.button_down.left )
+        Serial.println("Started pressing the left button");
+    if( Ps3.event.button_up.left )
+        Serial.println("Released the left button");
+
+    //------------- Digital shoulder button events -------------
+    if( Ps3.event.button_down.l1 )
+        Serial.println("Started pressing the left shoulder button");
+    if( Ps3.event.button_up.l1 )
+        Serial.println("Released the left shoulder button");
+
+    if( Ps3.event.button_down.r1 )
+        Serial.println("Started pressing the right shoulder button");
+    if( Ps3.event.button_up.r1 )
+        Serial.println("Released the right shoulder button");
+
+    //-------------- Digital trigger button events -------------
+    if( Ps3.event.button_down.l2 )
+        Serial.println("Started pressing the left trigger button");
+    if( Ps3.event.button_up.l2 )
+        Serial.println("Released the left trigger button");
+
+    if( Ps3.event.button_down.r2 )
+        Serial.println("Started pressing the right trigger button");
+    if( Ps3.event.button_up.r2 )
+        Serial.println("Released the right trigger button");
+
+    //--------------- Digital stick button events --------------
+    if( Ps3.event.button_down.l3 )
+        Serial.println("Started pressing the left stick button");
+    if( Ps3.event.button_up.l3 )
+        Serial.println("Released the left stick button");
+
+    if( Ps3.event.button_down.r3 )
+        Serial.println("Started pressing the right stick button");
+    if( Ps3.event.button_up.r3 )
+        Serial.println("Released the right stick button");
+
+    //---------- Digital select/start/ps button events ---------
+    if( Ps3.event.button_down.select )
+        Serial.println("Started pressing the select button");
+    if( Ps3.event.button_up.select )
+        Serial.println("Released the select button");
+
+    if( Ps3.event.button_down.start )
+        Serial.println("Started pressing the start button");
+    if( Ps3.event.button_up.start )
+        Serial.println("Released the start button");
+
+    if( Ps3.event.button_down.ps )
+        Serial.println("Started pressing the Playstation button");
+    if( Ps3.event.button_up.ps )
+        Serial.println("Released the Playstation button");
+
+
+    //---------------- Analog stick value events ---------------
+   if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 2 ){
+       Serial.print("Moved the left stick:");
+       Serial.print(" x="); Serial.print(Ps3.data.analog.stick.lx, DEC);
+       Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ly, DEC);
+       Serial.println();
+    }
+
+   if( abs(Ps3.event.analog_changed.stick.rx) + abs(Ps3.event.analog_changed.stick.ry) > 2 ){
+       Serial.print("Moved the right stick:");
+       Serial.print(" x="); Serial.print(Ps3.data.analog.stick.rx, DEC);
+       Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ry, DEC);
+       Serial.println();
+   }
+
+   //--------------- Analog D-pad button events ----------------
+   if( abs(Ps3.event.analog_changed.button.up) ){
+       Serial.print("Pressing the up button: ");
+       Serial.println(Ps3.data.analog.button.up, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.right) ){
+       Serial.print("Pressing the right button: ");
+       Serial.println(Ps3.data.analog.button.right, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.down) ){
+       Serial.print("Pressing the down button: ");
+       Serial.println(Ps3.data.analog.button.down, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.left) ){
+       Serial.print("Pressing the left button: ");
+       Serial.println(Ps3.data.analog.button.left, DEC);
+   }
+
+   //---------- Analog shoulder/trigger button events ----------
+   if( abs(Ps3.event.analog_changed.button.l1)){
+       Serial.print("Pressing the left shoulder button: ");
+       Serial.println(Ps3.data.analog.button.l1, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.r1) ){
+       Serial.print("Pressing the right shoulder button: ");
+       Serial.println(Ps3.data.analog.button.r1, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.l2) ){
+       Serial.print("Pressing the left trigger button: ");
+       Serial.println(Ps3.data.analog.button.l2, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.r2) ){
+       Serial.print("Pressing the right trigger button: ");
+       Serial.println(Ps3.data.analog.button.r2, DEC);
+   }
+
+   //---- Analog cross/square/triangle/circle button events ----
+   if( abs(Ps3.event.analog_changed.button.triangle)){
+       Serial.print("Pressing the triangle button: ");
+       Serial.println(Ps3.data.analog.button.triangle, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.circle) ){
+       Serial.print("Pressing the circle button: ");
+       Serial.println(Ps3.data.analog.button.circle, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.cross) ){
+       Serial.print("Pressing the cross button: ");
+       Serial.println(Ps3.data.analog.button.cross, DEC);
+   }
+
+   if( abs(Ps3.event.analog_changed.button.square) ){
+       Serial.print("Pressing the square button: ");
+       Serial.println(Ps3.data.analog.button.square, DEC);
+   }
+
+   //---------------------- Battery events ---------------------
+    if( battery != Ps3.data.status.battery ){
+        battery = Ps3.data.status.battery;
+        Serial.print("The controller battery is ");
+        if( battery == ps3_status_battery_charging )      Serial.println("charging");
+        else if( battery == ps3_status_battery_full )     Serial.println("FULL");
+        else if( battery == ps3_status_battery_high )     Serial.println("HIGH");
+        else if( battery == ps3_status_battery_low)       Serial.println("LOW");
+        else if( battery == ps3_status_battery_dying )    Serial.println("DYING");
+        else if( battery == ps3_status_battery_shutdown ) Serial.println("SHUTDOWN");
+        else Serial.println("UNDEFINED");
+    }
+
+}
+
+void onConnect(){
+    Serial.println("Connected.");
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Robot Driver " __DATE__ " " __TIME__);
-  WiFi.softAP(ssid);
-  Serial.println(WiFi.softAPIP());
-  server.begin();
+  // WiFi.softAP(ssid);
+  // Serial.println(WiFi.softAPIP());
+  // server.begin();
 
   pinMode(motor1A1, OUTPUT);
   pinMode(motor1A2, OUTPUT);
@@ -65,69 +260,109 @@ void setup() {
   // display.drawPixel(10, 10, SSD1306_WHITE);
   // display.display();
 
+  Ps3.attach(notify);
+  Ps3.attachOnConnect(onConnect);
+  Ps3.begin("b0:fc:36:5b:cf:08"); // Nathan's PS3 Controller
+
   analogWrite(motor1A1, 32);
   digitalWrite(motor1A2, 0);
+
+  
+  Serial.println("setup finished");
 
 }
 
 void loop() {
-  Serial.println("loop");
+  // Serial.println("loop");
 
   // display.invertDisplay(displayInverted);
   // display.display();
 
   // displayInverted = !displayInverted;
 
-  WiFiClient client = server.available();   // listen for incoming clients
-  if (client) {
-    Serial.println("New client");
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
+  // WiFiClient client = server.available();   // listen for incoming clients
+  // if (client) {
+  //   Serial.println("New client");
+  //   String currentLine = "";                // make a String to hold incoming data from the client
+  //   while (client.connected()) {            // loop while the client's connected
+  //     if (client.available()) {             // if there's bytes to read from the client,
+  //       char c = client.read();             // read a byte, then
+  //       if (c == '\n') {                    // if the byte is a newline character
+  //         // if the current line is blank, you got two newline characters in a row.
+  //         // that's the end of the client HTTP request, so send a response:
+  //         if (currentLine.length() == 0) {
+  //           // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+  //           // and a content-type so the client knows what's coming, then a blank line:
+  //           client.println("HTTP/1.1 200 OK");
+  //           client.println("Content-type:text/html");
+  //           client.println();
 
-            // the content of the HTTP response follows the header:
-            // TODO relevant code
-            if (state) {
-              client.print("ON<br/>");
-            } else {
-              client.print("OFF<br/>");
-            }
-            client.print("<a href=\"/tog\">Toggle</a><br>");
+  //           // the content of the HTTP response follows the header:
+  //           // TODO relevant code
+  //           if (state) {
+  //             client.print("ON<br/>");
+  //           } else {
+  //             client.print("OFF<br/>");
+  //           }
+  //           client.print("<a href=\"/tog\">Toggle</a><br>");
 
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
+  //           // The HTTP response ends with another blank line:
+  //           client.println();
+  //           // break out of the while loop:
+  //           break;
+  //         } else {    // if you got a newline, then clear currentLine:
+  //           currentLine = "";
+  //         }
+  //       } else if (c != '\r') {  // if you got anything else but a carriage return character,
+  //         currentLine += c;      // add it to the end of the currentLine
+  //       }
 
-        if (currentLine.endsWith("GET /tog")) {
-          if (!state) {
-            analogWrite(motor1A1, 0);
-            digitalWrite(motor1A2, 0);
-          } else {
-            analogWrite(motor1A1, 0.1);
-            digitalWrite(motor1A2, 0);
-          }
-          state = !state;
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-  }
+  //       if (currentLine.endsWith("GET /tog")) {
+  //         if (!state) {
+  //           analogWrite(motor1A1, 0);
+  //           digitalWrite(motor1A2, 0);
+  //         } else {
+  //           analogWrite(motor1A1, 0.1);
+  //           digitalWrite(motor1A2, 0);
+  //         }
+  //         state = !state;
+  //       }
+  //     }
+  //   }
+  //   // close the connection:
+  //   client.stop();
+  // }
+
+    if(!Ps3.isConnected())
+        return;
+
+    //-------------------- Player LEDs -------------------
+    Serial.print("Setting LEDs to player "); Serial.println(player, DEC);
+    Ps3.setPlayer(player);
+
+    player += 1;
+    if(player > 10) player = 0;
+
+
+    //------ Digital cross/square/triangle/circle buttons ------
+    if( Ps3.data.button.cross && Ps3.data.button.down )
+        Serial.println("Pressing both the down and cross buttons");
+    if( Ps3.data.button.square && Ps3.data.button.left )
+        Serial.println("Pressing both the square and left buttons");
+    if( Ps3.data.button.triangle && Ps3.data.button.up )
+        Serial.println("Pressing both the triangle and up buttons");
+    if( Ps3.data.button.circle && Ps3.data.button.right )
+        Serial.println("Pressing both the circle and right buttons");
+
+    if( Ps3.data.button.l1 && Ps3.data.button.r1 )
+        Serial.println("Pressing both the left and right bumper buttons");
+    if( Ps3.data.button.l2 && Ps3.data.button.r2 )
+        Serial.println("Pressing both the left and right trigger buttons");
+    if( Ps3.data.button.l3 && Ps3.data.button.r3 )
+        Serial.println("Pressing both the left and right stick buttons");
+    if( Ps3.data.button.select && Ps3.data.button.start )
+        Serial.println("Pressing both the select and start buttons");
+
+    delay(2000);
+
 }
