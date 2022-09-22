387ed87f01bbc531b445390c23393de0b9437c0f
TayKaiJun <tehohno@gmail.com>
2022-09-22 12:23:09 -0700

    Update PS3_Arduino_code.ino
    fixed neopixel patterns for left/right/forward/backward + different colors for up/down/neutral

--- examples/test_robotdriver2/firmware/PS3_Arduino_code/PS3_Arduino_code.ino
+++ examples/test_robotdriver2/firmware/PS3_Arduino_code/PS3_Arduino_code.ino
@@ -2,13 +2,16 @@
 #include <Freenove_WS2812_Lib_for_ESP32.h>
 #include <PCF8574.h>
 
-#define IDLE -1
-#define LEFT  1
-#define RIGHT	2
-#define FORWARD 3
-#define BACKWARD 4
+#define DISCONNECTED -1
+#define IDLE 0
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define FORWARD 5
+#define BACKWARD 6
 
-// Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
+int loop_count = 0;
 
 PCF8574 pcf8574(0x38, 12, 13);
 int expander_led0 = 4;
@@ -16,82 +19,121 @@ int expander_led1 = 5;
 int expander_led2 = 6;
 int expander_led3 = 7;
 
-class NeoPixelArray{
-  public:
-    NeoPixelArray(int led_count, int mcu_pin) : LEDS_COUNT(led_count), LEDS_PIN(mcu_pin), CHANNEL(0) {
-      strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
-      
+
+class NeoPixelArray {
+public:
+  NeoPixelArray(int led_count, int mcu_pin)
+    : LEDS_COUNT(led_count), LEDS_PIN(mcu_pin), CHANNEL(0), half_led_count(LEDS_COUNT / 2){
+    strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
+  }
+
+  void setup() {
+    strip.begin();
+    strip.setBrightness(25);
+
+    // start up with all lights white
+    for (int i = 0; i < LEDS_COUNT; i++) {
+      strip.setLedColorData(i, 255, 255, 255);
     }
+    strip.show();
+  }
 
-    void setup(){
-      strip.begin();
-      strip.setBrightness(10);
+  void set_state(int state, int color_state) {
+    int delay_val = 50;
 
-      // start up with all lights red	
+
+    if (state == DISCONNECTED) {
       for (int i = 0; i < LEDS_COUNT; i++) {
         strip.setLedColorData(i, 255, 0, 0);
       }
       strip.show();
-    }
+    } else {
+      if (loop_count % 5 == 0) {
+        switch (state) {
 
-    void set_state(int state){
-      switch(state){
-
-        case LEFT: 
-          for (int j = 0; j < LEDS_COUNT; j++) {
-            for (int i = 0; i < LEDS_COUNT; i++) {
-              if(i == j)
-                strip.setLedColorData(i, 255, 95, 31);
-              else
-                strip.setLedColorData(i, 0, 0, 0);
+          case LEFT:
+            for (int j = 0; j < LEDS_COUNT; j++) {
+              for (int i = 0; i < LEDS_COUNT; i++) {
+                if (i == j)
+                  strip.setLedColorData(i, colors[color_state][0], colors[color_state][1], colors[color_state][2]);
+                else
+                  strip.setLedColorData(i, 0, 0, 0);
+              }
+              strip.show();
+              delay(delay_val);
             }
-            strip.show();
-            delay(2);
-          } 
-          break;
+            break;
 
-        case RIGHT: 
-          for (int j = LEDS_COUNT-1; j >= 0; j--) {
-            for (int i = LEDS_COUNT-1; i >= 0; i--) {
-              if(i == j)
-                strip.setLedColorData(i, 255, 95, 31);
-              else
-                strip.setLedColorData(i, 0, 0, 0);
+          case RIGHT:
+            for (int j = LEDS_COUNT - 1; j >= 0; j--) {
+              for (int i = LEDS_COUNT - 1; i >= 0; i--) {
+                if (i == j)
+                  strip.setLedColorData(i, colors[color_state][0], colors[color_state][1], colors[color_state][2]);
+                else
+                  strip.setLedColorData(i, 0, 0, 0);
+              }
+              strip.show();
+              delay(delay_val);
             }
-            strip.show();
-            delay(2);
-          } 
-          break;
-        
-        case 0: 
-          for (int j = 0; j < 255; j += 2) {
-            for (int i = 0; i < LEDS_COUNT; i++) {
-              strip.setLedColorData(i, strip.Wheel((i * 256 / LEDS_COUNT + j) & 255));
-            }
-            strip.show();
-            delay(2);
-          } 
-          break;
+            break;
 
-        default:
-          for (int i = 0; i < LEDS_COUNT; i++) {
-            strip.setLedColorData(i, 0, 0, 0);
-          }
-          strip.show();
+          case BACKWARD:
+            for (int j = 0; j <= half_led_count; j++) {
+              for (int i = 0; i < LEDS_COUNT; i++) {
+                if (i == j || i == LEDS_COUNT - 1 - j)
+                  strip.setLedColorData(i, colors[color_state][0], colors[color_state][1], colors[color_state][2]);
+                else
+                  strip.setLedColorData(i, 0, 0, 0);
+              }
+              strip.show();
+              delay(delay_val * 2);
+            }
+            break;
+
+          case FORWARD:
+            for (int j = half_led_count; j >= 0; j--) {
+              for (int i = 0; i < LEDS_COUNT; i++) {
+                if (i == j || i == LEDS_COUNT - 1 - j)
+                  strip.setLedColorData(i, colors[color_state][0], colors[color_state][1], colors[color_state][2]);
+                else
+                  strip.setLedColorData(i, 0, 0, 0);
+              }
+              strip.show();
+              delay(delay_val * 2);
+            }
+            break;
+
+          default:
+            for (int j = 0; j < 255; j += 2) {
+              for (int i = 0; i < LEDS_COUNT; i++) {
+                strip.setLedColorData(i, strip.Wheel((i * 256 / LEDS_COUNT + j) & 255));
+              }
+              strip.show();
+              delay(5);
+            }
+            break;
+        }
       }
-    }    
+    }
+  }
 
-  protected:
-    Freenove_ESP32_WS2812 strip;  
-    const int LEDS_COUNT;
-    const int LEDS_PIN;
-    const int CHANNEL;
-
+protected:
+  Freenove_ESP32_WS2812 strip;
+  const int LEDS_COUNT;
+  const int LEDS_PIN;
+  const int CHANNEL;
+  const int half_led_count;
+  byte colors[3][3] = {
+    { 255, 95, 31 },  // amber color
+    { 0, 255, 0 },    // green color
+    { 0, 0, 255 }     // blue color
+  };
 };
 
 class LedcMotor {
 public:
-  LedcMotor(int out1, int out2, int ledc) : out1_(out1), out2_(out2), ledc_(ledc) {
+  LedcMotor(int out1, int out2, int ledc)
+    : out1_(out1), out2_(out2), ledc_(ledc) {
   }
 
   void setup() {
@@ -101,7 +143,7 @@ public:
     digitalWrite(out2_, 0);
     ledcSetup(ledc_, kPwmFreq, kPwmBits);
     ledcWrite(ledc_, 0);
-    ledcAttachPin(out1_, ledc_);  
+    ledcAttachPin(out1_, ledc_);
   }
 
   void setPwm(int pwm) {
@@ -122,7 +164,7 @@ protected:
   const int out1_, out2_, ledc_;  // pin assignments for 1 and 2, and LEDC channel
 
   const int kPwmFreq = 24000;  // fast enough to not brown out the motor drivers but out of audible range
-  const int kPwmBits = 8; // range of [0-255]
+  const int kPwmBits = 8;      // range of [0-255]
 
   bool fwdAttached_ = true;  // is the forward direction was the last attached PWM configuration
 };
@@ -149,7 +191,7 @@ const int kPwmLimit = 191;
 
 NeoPixelArray ledArray(5, 15);
 
-int limitPwm (int pwm) {
+int limitPwm(int pwm) {
   if (abs(pwm) > kPwmLimit) {
     return kPwmLimit * (pwm / abs(pwm));
   } else if (abs(pwm) < 15) {
@@ -159,9 +201,8 @@ int limitPwm (int pwm) {
   }
 }
 
-void applyAccleration (int *PWM, int *prev_PWM) {
-  if (abs(*PWM - *prev_PWM) > acceleration)
-  {
+void applyAccleration(int *PWM, int *prev_PWM) {
+  if (abs(*PWM - *prev_PWM) > acceleration) {
     *PWM = *PWM + acceleration * (*PWM - *prev_PWM) / abs(*PWM - *prev_PWM);
   }
 
@@ -169,29 +210,26 @@ void applyAccleration (int *PWM, int *prev_PWM) {
 }
 
 
-void notify()
-{
+void notify() {
   //---------------------- Battery events ---------------------
-  if ( battery != Ps3.data.status.battery ) {
+  if (battery != Ps3.data.status.battery) {
     battery = Ps3.data.status.battery;
     Serial.print("The controller battery is ");
-    if ( battery == ps3_status_battery_charging )      Serial.println("charging");
-    else if ( battery == ps3_status_battery_full )     Serial.println("FULL");
-    else if ( battery == ps3_status_battery_high )     Serial.println("HIGH");
-    else if ( battery == ps3_status_battery_low)       Serial.println("LOW");
-    else if ( battery == ps3_status_battery_dying )    Serial.println("DYING");
-    else if ( battery == ps3_status_battery_shutdown ) Serial.println("SHUTDOWN");
+    if (battery == ps3_status_battery_charging) Serial.println("charging");
+    else if (battery == ps3_status_battery_full) Serial.println("FULL");
+    else if (battery == ps3_status_battery_high) Serial.println("HIGH");
+    else if (battery == ps3_status_battery_low) Serial.println("LOW");
+    else if (battery == ps3_status_battery_dying) Serial.println("DYING");
+    else if (battery == ps3_status_battery_shutdown) Serial.println("SHUTDOWN");
     else Serial.println("UNDEFINED");
   }
-
 }
 
 void onConnect() {
   Serial.println("Connected.");
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   motor1A.setup();
@@ -203,23 +241,23 @@ void setup()
 
   Ps3.attach(notify);
   Ps3.attachOnConnect(onConnect);
-  Ps3.begin("b0:fc:36:5b:cf:08"); // Nathan's PS3 Controller
+  Ps3.begin("b0:fc:36:5b:cf:08");  // Nathan's PS3 Controller
 
   Serial.println("Ready.");
 
   // Set pinMode to OUTPUT
-	pcf8574.pinMode(expander_led0, OUTPUT);
+  pcf8574.pinMode(expander_led0, OUTPUT);
   pcf8574.pinMode(expander_led1, OUTPUT);
   pcf8574.pinMode(expander_led2, OUTPUT);
   pcf8574.pinMode(expander_led3, OUTPUT);
 
 
-	Serial.print("Init pcf8574...");
-	if (pcf8574.begin()){
-		Serial.println("OK");
-	}else{
-		Serial.println("KO");
-	}
+  Serial.print("Init pcf8574...");
+  if (pcf8574.begin()) {
+    Serial.println("OK");
+  } else {
+    Serial.println("KO");
+  }
 
   pcf8574.digitalWrite(expander_led0, HIGH);
   pcf8574.digitalWrite(expander_led1, HIGH);
@@ -233,15 +271,6 @@ void loop() {
     int forward = Ps3.data.analog.stick.ly;
     int turn = Ps3.data.analog.stick.lx;
 
-    if(turn < 0){
-      ledArray.set_state(LEFT);
-    }
-    else if (turn > 0){
-      ledArray.set_state(RIGHT);
-    }
-    else{
-      ledArray.set_state(IDLE);
-    }
     int left_PWM = forward * 3 / 2 + turn;
     int right_PWM = forward * 3 / 2 - turn;
 
@@ -264,10 +293,35 @@ void loop() {
     motor2B.setPwm(limitPwm(right_up_PWM));
 
     Serial.printf("Motors: % 3i % 3i    % 3i % 3i\n", left_PWM, right_PWM, left_up_PWM, right_up_PWM);
+
+    int joystick_threshold = 10;
+    int color_state = 0;
+
+    if (up < (-1 * joystick_threshold))
+      color_state = DOWN;
+    else if (up > joystick_threshold)
+      color_state = UP;
+    else
+      color_state = IDLE;
+
+    if (turn < (-1 * joystick_threshold) && turn < (-1 * abs(forward))) {
+      ledArray.set_state(LEFT, color_state);
+    } else if (turn > joystick_threshold && turn > abs(forward)) {
+      ledArray.set_state(RIGHT, color_state);
+    } else if (forward > joystick_threshold && forward > abs(turn)) {
+      ledArray.set_state(FORWARD, color_state);
+    } else if (forward < (-1 * joystick_threshold) && forward < (-1 * abs(turn))) {
+      ledArray.set_state(BACKWARD, color_state);
+    } else {
+      ledArray.set_state(IDLE, color_state);
+    }
+
+  } else {
+    ledArray.set_state(DISCONNECTED, DISCONNECTED);
   }
 
-  else{
-    ledArray.set_state(0);
-  }
+  loop_count++;
+  if (loop_count > 1000000)
+    loop_count = 0;
   delay(50);
 }
