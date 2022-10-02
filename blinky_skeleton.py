980834bd0deaa99ec59f8f6ca06dbcf6c086c035
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2022-10-01 17:23:00 -0700

    Reverted blinky_skeleton.py

--- blinky_skeleton.py
+++ blinky_skeleton.py
@@ -1,22 +1,10 @@
 from edg import *
-from electronics_model.KiCadSchematicBlock import KiCadSchematicBlock
 
-
-class KiCadBlock(KiCadSchematicBlock):
-  def __init__(self) -> None:
-    super().__init__()
-
-    self.PORT_A = self.Port(Passive())
-
-  def contents(self) -> None:
-    super().contents()
-
-    self.import_kicad('C:\\Users\\Nathan Nguyendinh\\Documents\\PCB Files\\EDG-IDE Simple Circuit\\EDG-IDE Simple Circuit.net')
 
 class BlinkyExample(BoardTop):
   def contents(self) -> None:
     super().contents()
-    self.test = self.Block(KiCadBlock())
+    # your implementation here
 
 
 if __name__ == "__main__":
