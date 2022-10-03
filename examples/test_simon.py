769fb304ce6c640f3fbdc1e672cb5f2ac1ab5e0b
Richard Lin <elpato25@gmail.com>
2022-10-03 15:48:31 -0700

    Update test_simon.py

--- examples/test_simon.py
+++ examples/test_simon.py
@@ -78,7 +78,7 @@ class TestSimon(BoardTop):
         ImplicitConnect(self.gnd, [Common]),
     ) as imp:
       for i in range(4):
-        driver = self.btn_drv[i] = imp.Block(HighSideSwitch(frequency=(0.1, 10) * kHertz))
+        driver = self.btn_drv[i] = imp.Block(HighSideSwitch(frequency=(0.1, 1) * kHertz))
         self.connect(self.mcu.gpio.allocate(f'btn_drv{i}'), driver.control)
         if i == 0:  # only one draws current, since we assume only one will be lit at any point in time
           self.connect(driver.output, self.btn[i].led_a)
