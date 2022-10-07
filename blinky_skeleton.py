a74e977525e99292c7f6f491da941bf42d5ea5f8
Richard Lin <elpato25@gmail.com>
2022-10-06 20:43:47 -0700

    SimpleBoardTop refactor

--- blinky_skeleton.py
+++ blinky_skeleton.py
@@ -1,15 +1,9 @@
 from edg import *
 
 
-class BlinkyExample(BoardTop):
+class BlinkyExample(SimpleBoardTop):
   def contents(self) -> None:
     super().contents()
-    self.usb = self.Block(UsbCReceptacle())
-    self.mcu = self.Block(Stm32f103_48())
-    self.led = self.Block(IndicatorLed())
-    self.connect(self.usb.pwr, self.mcu.pwr)
-    self.connect(self.usb.gnd, self.mcu.gnd, self.led.gnd)
-    self.connect(self.mcu.gpio.request('led'), self.led.signal)
     # your implementation here
 
 
