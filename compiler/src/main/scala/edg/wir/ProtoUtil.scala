6e9ebe1bfd2235083f005f42ebdffabdc0efc77d
Richard Lin <elpato25@gmail.com>
2022-11-29 21:57:05 -0800

    basic bugfix and basic conversions

--- compiler/src/main/scala/edg/wir/ProtoUtil.scala
+++ compiler/src/main/scala/edg/wir/ProtoUtil.scala
@@ -1,18 +1,38 @@
 package edg.wir
 
-import edgir.common.common
+import edgir.init.init
+import edgir.elem.elem
+import edgir.expr.expr
+
+import scala.collection.SeqMap
 
 object ProtoUtil {
-  def getNameOrder(pb: Option[common.Metadata]): Seq[String] = {
-    pb.getOrElse(common.Metadata()).meta.members match {
-      case Some(members) => members.node.collect {
-        case (name, meta) if meta.meta.namespaceOrder.isDefined => meta.getNamespaceOrder.names
-      }.flatten.toSeq
-      case None => Seq()
+  class BaseSeqMapToProto[ProtoType, ValueType](items: SeqMap[String, ValueType],
+                                                ctor: (String, Option[ValueType]) => ProtoType) {
+    def toPb: Seq[ProtoType] = {
+      items.map { case (name, value) =>
+        ctor(name, Some(value))
+      }.toSeq
     }
   }
 
-  def toNameOrder(nameOrder: Seq[String]): common.Metadata = {
-    common.Metadata(meta=common.Metadata.Meta.NamespaceOrder(common.NamespaceOrder(names=nameOrder)))
+  implicit class ParamSeqMapToProto(items: SeqMap[String, init.ValInit])
+      extends BaseSeqMapToProto[elem.NamedValInit, init.ValInit](items, elem.NamedValInit(_, _)) {
+  }
+
+  implicit class PortSeqMapToProto(items: SeqMap[String, elem.PortLike])
+      extends BaseSeqMapToProto[elem.NamedPortLike, elem.PortLike](items, elem.NamedPortLike(_, _)) {
+  }
+
+  implicit class BlockSeqMapToProto(items: SeqMap[String, elem.BlockLike])
+      extends BaseSeqMapToProto[elem.NamedBlockLike, elem.BlockLike](items, elem.NamedBlockLike(_, _)) {
+  }
+
+  implicit class LinkSeqMapToProto(items: SeqMap[String, elem.LinkLike])
+      extends BaseSeqMapToProto[elem.NamedLinkLike, elem.LinkLike](items, elem.NamedLinkLike(_, _)) {
+  }
+
+  implicit class ConstraintSeqMapToProto(items: SeqMap[String, expr.ValueExpr])
+      extends BaseSeqMapToProto[elem.NamedValueExpr, expr.ValueExpr](items, elem.NamedValueExpr(_, _)) {
   }
 }
