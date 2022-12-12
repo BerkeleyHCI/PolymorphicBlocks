de759bf3278a9826caeb1798d741ed5b47c5b9cd
Richard Lin <elpato25@gmail.com>
2022-12-12 14:48:00 -0800

    Update UsbBitBang.py

--- electronics_abstract_parts/UsbBitBang.py
+++ electronics_abstract_parts/UsbBitBang.py
@@ -11,8 +11,15 @@ class UsbBitBang(DiscreteApplication):
   @staticmethod
   def digital_external_from_link(link_port: DigitalBidir) -> DigitalBidir:
     """Creates a DigitalBidir model that is the external-facing port that exports from
-    an internal-facing (link-side) port. The internal-facing port should be ideal."""
-
+    an internal-facing (link-side) port. The internal-facing port should be ideal.
+    These are basically the semantics of a DigitalBidir bridge.
+    TODO: unify code w/ DigitalBidir bridge?"""
+    return DigitalBidir(
+      voltage_out=link_port.link().voltage, current_draw=link_port.link().current_drawn,
+      voltage_limits=link_port.link().voltage_limits, current_limits=link_port.link().current_limits,
+      output_thresholds=link_port.link().output_thresholds, input_thresholds=link_port.link().input_thresholds,
+      pulldown_capable=link_port.link().pulldown_capable, pullup_capable=link_port.link().pullup_capable
+    )
 
   def __init__(self) -> None:
     super().__init__()
@@ -38,13 +45,19 @@ class UsbBitBang(DiscreteApplication):
     self.dm_res = self.Block(Resistor(68*Ohm(tol=0.05)))
 
     self.connect(self.dm, self.dm_res.a.adapt_to(DigitalBidir()))  # internal ports are ideal
-    self.connect(self.usb.dm, self.dm_res.b.adapt_to(DigitalBidir(
-      voltage_out=self.dm.link().voltage
-    )))
+    self.connect(self.usb.dm, self.dm_res.b.adapt_to(
+      self.digital_external_from_link(self.dm)))
 
     self.connect(self.dp, self.dp_res.a.adapt_to(DigitalBidir()))
     self.connect(self.usb.dp, self.dp_res.b.adapt_to(DigitalBidir(
-      voltage_out=self.dp.link().voltage.hull(self.dp_pull.link().voltage)
+      voltage_out=self.dp.link().voltage.hull(self.dp_pull.link().voltage),
+      current_draw=self.dp.link().current_drawn + self.dp_pull.link().current_drawn,
+      voltage_limits=self.dp.link().voltage_limits.intersect(self.dp_pull.link().voltage_limits),
+      current_limits=self.dp.link().current_limits.intersect(self.dp_pull.link().current_limits),
+      output_thresholds=self.dp.link().output_thresholds.intersect(self.dp_pull.link().output_thresholds),
+      input_thresholds=self.dp.link().input_thresholds.hull(self.dp_pull.link().input_thresholds),
+      pulldown_capable=self.dp.link().pulldown_capable | self.dp_pull.link().pulldown_capable,
+      pullup_capable=self.dp.link().pullup_capable | self.dp_pull.link().pullup_capable
     )))
 
     self.connect(self.dp_pull, self.dp_pull_res.a.adapt_to(DigitalSink()))
