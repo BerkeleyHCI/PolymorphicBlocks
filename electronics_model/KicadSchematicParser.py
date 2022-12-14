86676e46b3d26ccdfb96ac7721035d012bd5a726
Richard Lin <elpato25@gmail.com>
2022-12-13 18:17:55 -0800

    Update KicadSchematicParser.py

--- electronics_model/KicadSchematicParser.py
+++ electronics_model/KicadSchematicParser.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import List, Any, Dict, Tuple, TypeVar, Type
 
 import sexpdata  # type: ignore
@@ -9,6 +10,11 @@ def test_cast(x: Any, type: Type[TestCaseType]) -> TestCaseType:
   assert isinstance(x, type)
   return x
 
+def extract_only(x: List[TestCaseType]) -> TestCaseType:
+  """Asserts the input list only has one element, and returns it."""
+  assert len(x) == 1
+  return x[0]
+
 def group_by_car(elts: List[Any]) -> Dict[Any, List[List[Any]]]:
   """Groups a list of elements by each element's car (its first element).
   Discards elements that have a non-symbol car or elements that are just a symbol."""
@@ -19,16 +25,33 @@ def group_by_car(elts: List[Any]) -> Dict[Any, List[List[Any]]]:
     # otherwise discard
   return out_dict
 
-def parse_xy(sexp: List[Any], expected_car: str) -> Tuple[float, float]:
-  """Given a sexp of the form (expected_car, a, b) (for a, b float), returns (a, b)."""
+def parse_xy(sexp: List[Any], expected_car: str = 'xy') -> Tuple[float, float]:
+  """Given a sexp of the form (xy, x, y) (for x, y float), returns (x, y)."""
   assert len(sexp) == 3
   assert sexp[0] == sexpdata.Symbol(expected_car)
-  return (test_cast(sexp[1], float), test_cast(sexp[2], float))
+  return (float(sexp[1]), float(sexp[2]))
+
+def parse_at(sexp: List[Any], expected_car: str = 'at') -> Tuple[float, float, float]:
+  """Given a sexp of the form (at, x, y, r) (for x, y, r float), returns (x, y, r)."""
+  assert len(sexp) == 4
+  assert sexp[0] == sexpdata.Symbol(expected_car)
+  return (float(sexp[1]), float(sexp[2]), float(sexp[3]))
 
 def parse_symbol(sexp: Any) -> str:
   """Asserts sexp is a Symbol and returns its value."""
   assert isinstance(sexp, sexpdata.Symbol)
   return sexp.value()
+
+
+class KicadLibPin:
+  def __repr__(self):
+    return f"{self.__class__.__name__}({self.number} @ {self.pt})"
+
+  def __init__(self, sexp: List[Any]):
+    assert parse_symbol(sexp[0]) == 'pin'
+    sexp_dict = group_by_car(sexp)
+    self.pt = parse_at(extract_only(sexp_dict['at']))
+    self.number = test_cast(extract_only(sexp_dict['number'])[1], str)
 
 
 class KicadLibSymbol:
@@ -41,6 +64,11 @@ class KicadLibSymbol:
     sexp_dict = group_by_car(sexp)
     self.properties: Dict[str, str] = {test_cast(prop[1], str): test_cast(prop[2], str)
                                        for prop in sexp_dict.get('property', [])}
+    symbol_elts = itertools.chain(*[symbol_sexp[2:]  # discard 'symbol' and the name
+                                    for symbol_sexp in sexp_dict.get('symbol', [])])
+    symbol_elts_dict = group_by_car(list(symbol_elts))
+    pins = [KicadLibPin(pin_sexp) for pin_sexp in symbol_elts_dict.get('pin', [])]
+    print(pins)
 
 
 class KicadWire:
@@ -50,9 +78,10 @@ class KicadWire:
   def __init__(self, sexp: List[Any]):
     assert parse_symbol(sexp[0]) == 'wire'
     sexp_dict = group_by_car(sexp)
-    assert len(sexp_dict['pts']) == 1 and len(sexp_dict['pts'][0]) == 3  # including pts itself
-    self.pt1 = parse_xy(sexp_dict['pts'][0][1], 'xy')
-    self.pt2 = parse_xy(sexp_dict['pts'][0][2], 'xy')
+    pts = extract_only(sexp_dict['pts'])
+    assert len(pts) == 3  # pts, pt0, pt1
+    self.pt1 = parse_xy(pts[1], 'xy')
+    self.pt2 = parse_xy(pts[2], 'xy')
 
 
 class KicadSchematic:
@@ -60,12 +89,8 @@ class KicadSchematic:
     schematic_top = sexpdata.loads(data)
     assert parse_symbol(schematic_top[0]) == 'kicad_sch'
     top_groups = group_by_car(schematic_top)
-    print(top_groups.keys())
-    print(top_groups['wire'])
-
     wires = [KicadWire(elt) for elt in top_groups.get('wire', [])]
-    print(wires)
-    lib_symbols = [KicadLibSymbol(elt) for elt in top_groups.get('lib_symbols', [])[0][1:]]  # slice to discard car
+    lib_symbols = [KicadLibSymbol(elt) for elt in extract_only(top_groups.get('lib_symbols', []))[1:]]  # slice to discard car
     print(lib_symbols)
 
-    sexpdata.Symbol('kicad_sch').value()
+    # TODO support hierarchy with sheet_instances and symbol_instances
