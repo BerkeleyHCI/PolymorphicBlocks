f477aa096337e4fddfb1372e6d1787a0efaf8e56
Richard Lin <elpato25@gmail.com>
2022-12-13 16:04:40 -0800

    infra
    infra

--- electronics_model/KicadSchematicParser.py
+++ electronics_model/KicadSchematicParser.py
@@ -1,1 +1,8 @@
+import sexpdata
 
+
+class KicadSchematic:
+  def __init__(self, data: str):
+    schematic_top = sexpdata.loads(data)
+    print(schematic_top)
+    assert schematic_top[0] == sexpdata.Symbol('kicad_sch')
---
+++ electronics_model/test_kicad_schematic_parser.py
@@ -0,0 +1,11 @@
+import unittest
+
+import os.path
+from KicadSchematicParser import KicadSchematic
+
+
+class KicalSchematicParserTest(unittest.TestCase):
+  def test_kicad(self):
+    with open(os.path.join(os.path.dirname(__file__), "resources", "test_kicad_import.kicad_sch"), "r") as file:
+      file_data = file.read()
+    sch = KicadSchematic(file_data)
