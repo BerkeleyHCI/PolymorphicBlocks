a43a34db10a6d69bacae1836babe08f92781bb1b
Richard Lin <elpato25@gmail.com>
2022-12-19 18:30:38 -0800

    also check component values

--- electronics_model/test_kicad_import.py
+++ electronics_model/test_kicad_import.py
@@ -2,14 +2,18 @@ import os
 import unittest
 
 import edgir
+from edg_core import Range
 from electronics_model import Passive
 from electronics_model.KiCadSchematicBlock import KiCadSchematicBlock
-from electronics_abstract_parts import Resistor, Capacitor, Volt, Ohm, Farad
+from electronics_abstract_parts import Resistor, Capacitor, Volt, Ohm, uFarad
 
 
 def example_filepath(filename: str) -> str:
     return os.path.join(os.path.dirname(__file__), "resources", filename)
 
+
+# Note that all the below blocks are the same circuit (component values and connectivity)
+# but defined in slightly different ways.
 class KiCadBlock(KiCadSchematicBlock):
     """Block that has its implementation completely defined in KiCad."""
     def __init__(self) -> None:
@@ -43,7 +47,7 @@ class KiCadInlineVarsBlock(KiCadSchematicBlock):
         self.import_kicad(example_filepath("test_kicad_import_inline_vars.kicad_sch"), {
             'r1_res': 51*Ohm(tol=0.05),
             'r2_res': 51*Ohm(tol=0.05),
-            'c1_cap': 47*Farad(tol=0.05),
+            'c1_cap': 47*uFarad(tol=0.2),
             'in_voltage': (0, 6.3)*Volt,
         })
 
@@ -55,7 +59,7 @@ class KiCadCodePartsBock(KiCadSchematicBlock):
         self.PORT_A = self.Port(Passive())
         self.R1 = self.Block(Resistor(51*Ohm(tol=0.05)))
         self.R2 = self.Block(Resistor(51*Ohm(tol=0.05)))
-        self.C1 = self.Block(Capacitor(47*Farad(tol=0.05), (0, 6.3)*Volt))
+        self.C1 = self.Block(Capacitor(47*uFarad(tol=0.2), (0, 6.3)*Volt))
         self.import_kicad(example_filepath("test_kicad_import_codeparts.kicad_sch"))
 
 
@@ -129,3 +133,9 @@ class KiCadImportProtoTestCase(unittest.TestCase):
         expected_conn.exported.internal_block_port.ref.steps.add().name = 'R1'
         expected_conn.exported.internal_block_port.ref.steps.add().name = 'a'
         self.assertIn(expected_conn, constraints)
+
+        self.assertIn(edgir.AssignLit(['R1', 'resistance'], Range.from_tolerance(51, 0.05)), constraints)
+        self.assertIn(edgir.AssignLit(['R2', 'resistance'], Range.from_tolerance(51, 0.05)), constraints)
+
+        self.assertIn(edgir.AssignLit(['C1', 'capacitance'], Range.from_tolerance(47e-6, 0.2)), constraints)
+        self.assertIn(edgir.AssignLit(['C1', 'voltage'], Range(0, 6.3)), constraints)
