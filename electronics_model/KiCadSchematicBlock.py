e2c152ff1fb65562ea6b6d694cd5c7947c9f6d31
Richard Lin <elpato25@gmail.com>
2022-12-19 16:32:01 -0800

    imports

--- electronics_model/KiCadSchematicBlock.py
+++ electronics_model/KiCadSchematicBlock.py
@@ -1,6 +1,5 @@
-from typing import Dict, Type
-
-from kinparse import parse_netlist  # type: ignore
+import inspect
+from typing import Dict, Type, Any
 
 from edg_core import Block
 from electronics_abstract_parts import Resistor, Capacitor, Opamp
@@ -18,7 +17,7 @@ class KiCadSchematicBlock(Block):
         # 'Simulation_SPICE:OPAMP': Opamp,
     }
 
-    def import_kicad(self, filepath: str):
+    def import_kicad(self, filepath: str, locals: Dict[str, Any] = {}):
         with open(filepath, "r") as file:
             file_data = file.read()
         sch = KiCadSchematic(file_data)
@@ -26,12 +25,20 @@ class KiCadSchematicBlock(Block):
         blocks: Dict[str, KiCadImportableBlock] = {}
 
         for symbol in sch.symbols:
-            if hasattr(self, symbol.refdes):  # if block already created, use schematic just for connectivity
+            if hasattr(self, symbol.refdes):  # sub-block defined in the Python Block, schematic only for connections
                 assert not symbol.properties['Value'] or symbol.properties['Value'] == '~',\
                     f"{symbol.refdes} has both code block and non-empty value"
                 block = getattr(self, symbol.refdes)
                 assert isinstance(block, KiCadImportableBlock)
-            elif symbol.lib in self.SYMBOL_MAP:
+            elif symbol.properties['Value'].startswith('#'):  # sub-block with inline Python in the value
+                inline_code = symbol.properties['Value'][1:]
+                # use the caller's globals,
+                block_model = eval(inline_code, inspect.stack()[1][0].f_globals, locals)
+                assert isinstance(block_model, KiCadImportableBlock),\
+                    f"block {block_model} created by {inline_code} not KicadImportableBlock"
+                block = self.Block(block_model)
+                setattr(self, symbol.refdes, block)
+            elif symbol.lib in self.SYMBOL_MAP:  # sub-block with code to parse the value
                 block = self.Block(self.SYMBOL_MAP[symbol.lib].block_from_symbol(symbol.lib, symbol.properties))
                 setattr(self, symbol.refdes, block)
             else:
--- electronics_model/test_kicad_import.py
+++ electronics_model/test_kicad_import.py
@@ -7,12 +7,37 @@ from electronics_model.KiCadSchematicBlock import KiCadSchematicBlock
 from electronics_abstract_parts import Resistor, Capacitor, Volt, Ohm, Farad
 
 
+def example_filepath(filename: str) -> str:
+    return os.path.join(os.path.dirname(__file__), "resources", filename)
+
 class KiCadBlock(KiCadSchematicBlock):
     """Block that has its implementation completely defined in KiCad."""
     def __init__(self) -> None:
         super().__init__()
         self.PORT_A = self.Port(Passive())
-        self.import_kicad(os.path.join(os.path.dirname(__file__), "resources", "test_kicad_import.kicad_sch"))
+        self.import_kicad(example_filepath("test_kicad_import.kicad_sch"))
+
+
+class KiCadInlineBlock(KiCadSchematicBlock):
+    """Block that has its implementation completely defined in KiCad, using inline Python in the symbol value."""
+    def __init__(self) -> None:
+        super().__init__()
+        self.PORT_A = self.Port(Passive())
+        self.import_kicad(example_filepath("test_kicad_import_inline.kicad_sch"))
+
+
+class KiCadInlineVarsBlock(KiCadSchematicBlock):
+    """Block that has its implementation completely defined in KiCad, using inline Python that references
+    local variables defined in the HDL."""
+    def __init__(self) -> None:
+        super().__init__()
+        self.PORT_A = self.Port(Passive())
+        self.import_kicad(example_filepath("test_kicad_import_inline_vars.kicad_sch"), {
+            'r1_res': 51*Ohm(tol=0.05),
+            'r2_res': 51*Ohm(tol=0.05),
+            'c1_cap': 47*Farad(tol=0.05),
+            'in_voltage': (0, 6.3)*Volt,
+        })
 
 
 class KiCadCodePartsBock(KiCadSchematicBlock):
@@ -23,12 +48,20 @@ class KiCadCodePartsBock(KiCadSchematicBlock):
         self.R1 = self.Block(Resistor(51*Ohm(tol=0.05)))
         self.R2 = self.Block(Resistor(51*Ohm(tol=0.05)))
         self.C1 = self.Block(Capacitor(47*Farad(tol=0.05), (0, 6.3)*Volt))
-        self.import_kicad(os.path.join(os.path.dirname(__file__), "resources", "test_kicad_import_codeparts.kicad_sch"))
+        self.import_kicad(example_filepath("test_kicad_import_codeparts.kicad_sch"))
 
 
 class KiCadImportProtoTestCase(unittest.TestCase):
     def test_block(self):
         pb = KiCadBlock()._elaborated_def_to_proto()
+        self.check_connectivity(pb)
+
+    def test_inline_block(self):
+        pb = KiCadInlineBlock()._elaborated_def_to_proto()
+        self.check_connectivity(pb)
+
+    def test_inline_vars_block(self):
+        pb = KiCadInlineVarsBlock()._elaborated_def_to_proto()
         self.check_connectivity(pb)
 
     def test_codeparts_block(self):
