17b0916c094fd1d62d4d76b81c675b25ea55fda9
Richard Lin <richard.lin@berkeley.edu>
2022-12-26 22:27:27 -0800

    Update test_fcml.py

--- examples/test_fcml.py
+++ examples/test_fcml.py
@@ -1,6 +1,7 @@
 import unittest
 
 from edg import *
+from .test_bldc import PowerOutConnector
 
 
 class DiscreteMutlilevelBuckConverter(GeneratorBlock):
@@ -21,7 +22,9 @@ class DiscreteMutlilevelBuckConverter(GeneratorBlock):
   either be high or low (but not both or none).
   Generates a digital isolator for each gate driver that is offset from ground.
   """
-  def __init__(self, levels: IntLike = 2, ratios: RangeLike = (0.25, 0.75)):
+  @init_in_parent
+  def __init__(self, levels: IntLike, ratios: RangeLike, frequency: RangeLike, *,
+               inductor_current_ripple: RangeLike, fet_rds: RangeLike = (0, 0.1)*Ohm):
     super().__init__()
     self.pwr_in = self.Port(VoltageSink.empty())
     self.pwr_out = self.Port(VoltageSource.empty())
@@ -30,10 +33,66 @@ class DiscreteMutlilevelBuckConverter(GeneratorBlock):
     self.pwr_gate = self.Port(VoltageSink.empty())
     self.pwms = self.Port(Vector(DigitalSink.empty()))
 
+    self.frequency = self.ArgParameter(frequency)
+    self.inductor_current_ripple = self.ArgParameter(inductor_current_ripple)
+    self.fet_rds = self.ArgParameter(fet_rds)
+
     self.generator(self.generate, levels, ratios)
 
   def generate(self, levels: int, ratios: Range):
-    pass
+    assert levels >= 2, "levels must be 2 or more"
+    self.power_path = self.Block(BuckConverterPowerPath(
+      self.pwr_in.link().voltage, self.pwr_in.link().voltage * ratios, self.frequency,
+      self.pwr_out.link().current_drawn, (0, 1.5)*Amp,
+      inductor_current_ripple=self.inductor_current_ripple,
+      dutycycle_limit=(0, 1)
+    ))
+    self.connect(self.power_path.pwr_in, self.pwr_in)
+    self.connect(self.power_path.pwr_out, self.pwr_out)
+    self.connect(self.power_path.gnd, self.gnd)
+
+    self.driver = ElementDict[HalfBridgeDriver]()  # [0, levels-1)
+    self.isolator = ElementDict[DigitalIsolator]()  # [1, levels-1), unneeded at 0
+    self.low_fet = ElementDict[Fet]()  # [0, levels-1)
+    self.high_fet = ElementDict[Fet]()
+    self.flying_cap = ElementDict[Capacitor]()  # [1 ... levels-1), excluding main input cap
+    self.boot_diode = ElementDict[Diode]()  # [1, 2*(levels-1)), associated with the powered FET
+    self.boot_cap = ElementDict[Capacitor]()  # similar indexing to boot_diode
+
+    fet_model = Fet.NFet(
+      drain_voltage=self.pwr_in.link().voltage,
+      drain_current=(0, self.power_path.peak_current),
+      gate_voltage=self.pwr_gate.link().voltage,  # TODO account for boot diode drop
+      rds_on=self.fet_rds
+    )
+    flying_cap_model = Capacitor(
+      capacitance=1*uFarad(tol=0),  # TODO size cap
+      voltage=self.pwr_in.link().voltage
+    )
+
+    low_prev = self.gnd
+    high_prev = self.pwr_in
+    low_boot_prev = self.pwr_gate
+    high_boot_prev = None
+
+    for level in range(levels - 1):
+      self.low_fet[level] = low_fet = self.Block(fet_model)
+      low_source = low_fet.source.adapt_to(VoltageSink())
+      self.connect(low_prev, low_source)
+      low_prev = low_fet.drain.adapt_to(VoltageSource())
+
+      self.high_fet[level] = high_fet = self.Block(fet_model)
+      high_drain = high_fet.drain.adapt_to(VoltageSink())
+      self.connect(high_prev, high_drain)
+      high_prev = high_fet.source.adapt_to(VoltageSource())
+
+      if level > 0:  # generate flying cap, if not the main input cap
+        self.flying_cap[level] = flying_cap = self.Block(flying_cap_model)
+        self.connect(low_source, flying_cap.neg.adapt_to(VoltageSink()))
+        self.connect(high_drain, flying_cap.pos.adapt_to(VoltageSink()))
+
+    # TODO connect high_prev, which conflicts b/c it's VoltageSource as well
+    self.connect(low_prev, self.power_path.switch)
 
 
 class FcmlTest(JlcBoardTop):
@@ -62,6 +121,15 @@ class FcmlTest(JlcBoardTop):
       )
       self.v3v3 = self.connect(self.reg_3v3.pwr_out)
 
+      self.conv = imp.Block(DiscreteMutlilevelBuckConverter(
+        4, (0.15, 0.5), 100*kHertz(tol=0),
+        inductor_current_ripple=(0, 1)*Amp
+      ))
+      self.conv_out = imp.Block(PowerOutConnector((0, 0.50)*Amp))
+      self.connect(self.conv.pwr_in, self.vusb)
+      self.connect(self.conv.pwr_out, self.conv_out.pwr)
+    # TODO
+
     # 3V3 DOMAIN
     with self.implicit_connect(
         ImplicitConnect(self.v3v3, [Power]),
@@ -71,6 +139,7 @@ class FcmlTest(JlcBoardTop):
 
       (self.sw1, ), _ = self.chain(imp.Block(DigitalSwitch()), self.mcu.gpio.request('sw1'))
       (self.usb_esd, ), _ = self.chain(self.usb.usb, imp.Block(UsbEsdDiode()), self.mcu.usb.request())
+      self.connect(self.mcu.gpio.request_vector('pwm'), self.conv.pwms)
 
     # Misc board
     self.duck = self.Block(DuckLogo())
