5b4fe0d69d0db34701468f5615e323e55ce02012
ParthPandhare <parthpandhare@gmail.com>
2023-01-04 10:28:22 -0800

    visit_block function updated to account for multiple occurrences of the same component; quantity field added to BOM.csv

--- electronics_model/BomBackend.py
+++ electronics_model/BomBackend.py
@@ -7,30 +7,27 @@ import csv
 
 
 class BomItem:
-    def __init__(self, designator, footprint, value, manufacturer, part_number, datasheet):
+    def __init__(self, designator, quantity, value, footprint, manufacturer, part_number, datasheet):
         self.designator = designator
+        self.quantity = quantity
         self.value = value
         self.footprint = footprint
         self.manufacturer = manufacturer
         self.part_number = part_number
         self.datasheet = datasheet
 
-    def list_values(self) -> List[str]:       # makes it easier to populate .csv file
-        return [str(self.designator), str(self.value), str(self.footprint),
-                str(self.manufacturer), str(self.part_number), str(self.datasheet)]
-
 
 class BomBackend(BaseBackend):      # creates and populates .csv file
     def run(self, design: CompiledDesign) -> List[Tuple[edgir.LocalPath, str]]:
         bom_list = BomTransform(design).run()
         with open('BOM.csv', 'w', newline='') as f:
-            fieldnames = ['Ref Des', 'Value', 'Footprint', 'Manufacturer', 'Part Number', 'Datasheet']
+            fieldnames = ['Ref Des', 'Quantity', 'Value', 'Footprint', 'Manufacturer', 'Part Number', 'Datasheet']
             thewriter = csv.DictWriter(f, fieldnames=fieldnames)
 
             thewriter.writeheader()      # creates the header
 
             for item in bom_list:
-                thewriter.writerow({'Ref Des' : item.designator, 'Value' : item.value,
+                thewriter.writerow({'Ref Des' : item.designator, 'Quantity' : item.quantity, 'Value' : item.value,
                                     'Footprint': item.footprint, 'Manufacturer' : item.manufacturer,
                                     'Part Number' : item.part_number, 'Datasheet' : item.datasheet})
 
@@ -45,14 +42,23 @@ class BomTransform(TransformUtil.Transform):
         self.bom_list: List[BomItem] = []  # populated in traversal order
 
     def visit_block(self, context: TransformUtil.TransformContext, block: edgir.BlockTypes) -> None:
-        bom = BomItem(str(self.design.get_value(context.path.to_tuple() + ('fp_refdes',))),
-                      str(self.design.get_value(context.path.to_tuple() + ('fp_value',))),
-                      str(self.design.get_value(context.path.to_tuple() + ('fp_footprint',))),
-                      str(self.design.get_value(context.path.to_tuple() + ('fp_mfr',))),
-                      str(self.design.get_value(context.path.to_tuple() + ('fp_part',))),
-                      str(self.design.get_value(context.path.to_tuple() + ('fp_datasheet',))))
+        tracker = True
+        for item in self.bom_list:   # checks for multiple occurrences of the same component
+            if str(self.design.get_value(context.path.to_tuple() + ('fp_footprint',))) == item.footprint:
+                item.designator += ' ' + str(self.design.get_value(context.path.to_tuple() + ('fp_refdes',)))
+                item.quantity += 1
+                tracker = False
+                break
 
-        self.bom_list.append(bom)
+        if tracker:
+            bom = BomItem(str(self.design.get_value(context.path.to_tuple() + ('fp_refdes',))), 1,
+                          str(self.design.get_value(context.path.to_tuple() + ('fp_value',))),
+                          str(self.design.get_value(context.path.to_tuple() + ('fp_footprint',))),
+                          str(self.design.get_value(context.path.to_tuple() + ('fp_mfr',))),
+                          str(self.design.get_value(context.path.to_tuple() + ('fp_part',))),
+                          str(self.design.get_value(context.path.to_tuple() + ('fp_datasheet',))))
+
+            self.bom_list.append(bom)
 
     def run(self) -> List[BomItem]:
         self.transform_design(self.design.design)
