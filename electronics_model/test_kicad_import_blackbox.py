a067e666f53c9a366827ecb4275e7e58ede41433
Richard Lin <richard.lin@berkeley.edu>
2023-01-06 02:21:04 -0800

    wip plumbing

--- electronics_model/test_kicad_import_blackbox.py
+++ electronics_model/test_kicad_import_blackbox.py
@@ -1,8 +1,6 @@
 import unittest
-from typing import Type
 
-import edgir
-from edg_core import Range
+from edg_core import Block, Builder
 from electronics_model import KiCadSchematicBlock, VoltageSink, Ground, AnalogSource
 
 
@@ -11,71 +9,19 @@ class KiCadBlackboxBlock(KiCadSchematicBlock):
     map to one of the abstract types."""
     def __init__(self) -> None:
         super().__init__()
-        self.pwr = self.Port(VoltageSink.empty())
-        self.gnd = self.Port(Ground.empty())
-        self.out = self.Port(AnalogSource.empty())
+        self.pwr = self.Port(VoltageSink.empty(), optional=True)
+        self.gnd = self.Port(Ground.empty(), optional=True)
+        self.out = self.Port(AnalogSource.empty(), optional=True)
         self.import_kicad(self.file_path("resources", "test_kicad_import_blackbox.kicad_sch"),
                           conversions={  # ideal ports only here
-                              'pwr': VoltageSink(),
-                              'gnd': Ground(),
-                              'out': AnalogSource(),
+                              'U1.Vdd': VoltageSink(),
+                              'U1.GND': Ground(),
+                              'res.2': AnalogSource(),
                           })
 
 
 class KiCadImportBlackboxTestCase(unittest.TestCase):
     def test_import_blackbox(self):
-        """Checks the connectivity of the generated proto, since the examples have similar structures."""
-        pb = KiCadBlackboxBlock()._elaborated_def_to_proto()
-        constraints = list(map(lambda pair: pair.value, pb.constraints))
-
-        expected_conn = edgir.ValueExpr()
-        expected_conn.connected.link_port.ref.steps.add().name = 'Test_Net_1'
-        expected_conn.connected.link_port.ref.steps.add().name = 'passives'
-        expected_conn.connected.link_port.ref.steps.add().allocate = ''
-        expected_conn.connected.block_port.ref.steps.add().name = 'C1'
-        expected_conn.connected.block_port.ref.steps.add().name = 'neg'
-        self.assertIn(expected_conn, constraints)
-
-        expected_conn = edgir.ValueExpr()
-        expected_conn.connected.link_port.ref.steps.add().name = 'Test_Net_1'
-        expected_conn.connected.link_port.ref.steps.add().name = 'passives'
-        expected_conn.connected.link_port.ref.steps.add().allocate = ''
-        expected_conn.connected.block_port.ref.steps.add().name = 'R2'
-        expected_conn.connected.block_port.ref.steps.add().name = 'b'
-        self.assertIn(expected_conn, constraints)
-
-        expected_conn = edgir.ValueExpr()
-        expected_conn.connected.link_port.ref.steps.add().name = 'node'
-        expected_conn.connected.link_port.ref.steps.add().name = 'passives'
-        expected_conn.connected.link_port.ref.steps.add().allocate = ''
-        expected_conn.connected.block_port.ref.steps.add().name = 'R1'
-        expected_conn.connected.block_port.ref.steps.add().name = 'b'
-        self.assertIn(expected_conn, constraints)
-
-        expected_conn = edgir.ValueExpr()
-        expected_conn.connected.link_port.ref.steps.add().name = 'node'
-        expected_conn.connected.link_port.ref.steps.add().name = 'passives'
-        expected_conn.connected.link_port.ref.steps.add().allocate = ''
-        expected_conn.connected.block_port.ref.steps.add().name = 'C1'
-        expected_conn.connected.block_port.ref.steps.add().name = 'pos'
-        self.assertIn(expected_conn, constraints)
-
-        expected_conn = edgir.ValueExpr()
-        expected_conn.connected.link_port.ref.steps.add().name = 'node'
-        expected_conn.connected.link_port.ref.steps.add().name = 'passives'
-        expected_conn.connected.link_port.ref.steps.add().allocate = ''
-        expected_conn.connected.block_port.ref.steps.add().name = 'R2'
-        expected_conn.connected.block_port.ref.steps.add().name = 'a'
-        self.assertIn(expected_conn, constraints)
-
-        expected_conn = edgir.ValueExpr()
-        expected_conn.exported.exterior_port.ref.steps.add().name = 'PORT_A'
-        expected_conn.exported.internal_block_port.ref.steps.add().name = 'R1'
-        expected_conn.exported.internal_block_port.ref.steps.add().name = 'a'
-        self.assertIn(expected_conn, constraints)
-
-        self.assertIn(edgir.AssignLit(['R1', 'resistance'], Range.from_tolerance(51, 0.05)), constraints)
-        self.assertIn(edgir.AssignLit(['R2', 'resistance'], Range.from_tolerance(51, 0.05)), constraints)
-
-        self.assertIn(edgir.AssignLit(['C1', 'capacitance'], Range.from_tolerance(47e-6, 0.2)), constraints)
-        self.assertIn(edgir.AssignLit(['C1', 'voltage'], Range(0, 6.3)), constraints)
+        # the elaborate_toplevel wrapper is needed since the inner block uses array ports
+        pb = Builder.builder.elaborate_toplevel(KiCadBlackboxBlock())
+        print(pb)
