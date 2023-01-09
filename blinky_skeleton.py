d436841e9d68ab69c133aa0e0942bea948b747e3
ParthPandhare <parthpandhare@gmail.com>
2023-01-08 16:08:55 -0800

    random changes, this doesn't matter

--- blinky_skeleton.py
+++ blinky_skeleton.py
@@ -4,8 +4,34 @@ from edg import *
 class BlinkyExample(SimpleBoardTop):
   def contents(self) -> None:
     super().contents()
-    # your implementation here
+    self.usb = self.Block(UsbCReceptacle())
+    self.buck = self.Block(BuckConverter(3.3*Volt(tol=0.05)))
+    self.connect(self.usb.gnd, self.buck.gnd)
+    self.connect(self.usb.pwr, self.buck.pwr_in)
 
+    with self.implicit_connect(
+            ImplicitConnect(self.buck.pwr_out, [Power]),
+            ImplicitConnect(self.buck.gnd, [Common]),
+    ) as imp:
+      self.mcu = imp.Block(IoController())
 
-if __name__ == "__main__":
-  compile_board_inplace(BlinkyExample)
+      (self.sw, ), _ = self.chain(imp.Block(DigitalSwitch()), self.mcu.gpio.request('sw'))
+
+      self.led = ElementDict[IndicatorLed]()
+      for i in range(4):
+        (self.led[i], ), _ = self.chain(self.mcu.gpio.request(f'led{i}'), imp.Block(IndicatorLed()))
+
+  def refinements(self) -> Refinements:
+    return super().refinements() + Refinements(
+      instance_refinements=[
+        (['buck'], Tps561201),
+        (['mcu'], Esp32_Wroom_32),
+      ],
+      instance_values=[
+        (['mcu', 'pin_assigns'], [
+          'led0=26',
+          'led1=27',
+          'led2=28',
+          'led3=29',
+        ])
+      ])
