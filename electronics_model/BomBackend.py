17755452dfd1e1be6c2c436314cc8c5a29bfeaad
ParthPandhare <parthpandhare@gmail.com>
2023-01-10 23:48:43 -0800

    converted bom_list to be a Dict[BomItem, List[str]] instead of a List[BomItem]
    got rid of quantity & refdes in BomItem, instead using the dictionary values store refdeses & finding quantity during .csv generation
    
    changed BomItem class to take a NamedTuple instead of having an __init__ function

--- electronics_model/BomBackend.py
+++ electronics_model/BomBackend.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, NamedTuple, Dict
 import os
 
 import edgir
@@ -7,31 +7,29 @@ from edg_core import CompiledDesign, TransformUtil, BaseBackend
 import csv
 
 
-class BomItem:
-    def __init__(self, refdes, quantity, value, footprint, manufacturer, part_number, datasheet):
-        self.refdes = refdes
-        self.quantity = quantity
-        self.value = value
-        self.footprint = footprint
-        self.manufacturer = manufacturer
-        self.part_number = part_number
-        self.datasheet = datasheet
+class BomItem(NamedTuple):
+    value: str
+    footprint: str
+    manufacturer: str
+    part_number: str
+    datasheet: str
+    # possibly add a run function to easily populate .csv string?
 
 
 class GenerateBom(BaseBackend):      # creates and populates .csv file
     def run(self, design: CompiledDesign) -> List[Tuple[edgir.LocalPath, str]]:
         bom_list = BomTransform(design).run()
         name = os.path.splitext(os.path.basename(__file__))[0] + '_bom.csv'
+
         with open(name, 'w', newline='') as f:
             fieldnames = ['Ref Des', 'Quantity', 'Value', 'Footprint', 'Manufacturer', 'Part Number', 'Datasheet']
             thewriter = csv.DictWriter(f, fieldnames=fieldnames)
-
             thewriter.writeheader()      # creates the header
 
-            for item in bom_list:
-                thewriter.writerow({'Ref Des' : item.refdes, 'Quantity' : item.quantity, 'Value' : item.value,
-                                    'Footprint' : item.footprint, 'Manufacturer' : item.manufacturer,
-                                    'Part Number' : item.part_number, 'Datasheet' : item.datasheet})
+            for key in bom_list:
+                thewriter.writerow({'Ref Des': ', '.join(bom_list[key]), 'Quantity': len(bom_list[key]),
+                                    'Value': key.value, 'Footprint': key.footprint, 'Manufacturer': key.manufacturer,
+                                    'Part Number': key.part_number, 'Datasheet': key.datasheet})
 
         return [
             (edgir.LocalPath(), name)
@@ -41,26 +39,17 @@ class GenerateBom(BaseBackend):      # creates and populates .csv file
 class BomTransform(TransformUtil.Transform):
     def __init__(self, design: CompiledDesign):
         self.design = design
-        self.bom_list: List[BomItem] = []  # populated in traversal order
+        self.bom_list: Dict[BomItem, List[str]] = {}  # BomItem -> list of refdes
 
     def visit_block(self, context: TransformUtil.TransformContext, block: edgir.BlockTypes) -> None:
-        tracker = True
-        for item in self.bom_list:   # checks for multiple occurrences of the same component
-            if str(self.design.get_value(context.path.to_tuple() + ('fp_footprint',))) == item.footprint:
-                item.refdes += ', ' + str(self.design.get_value(context.path.to_tuple() + ('fp_refdes',)))
-                item.quantity += 1
-                tracker = False
-                break
-
-        if tracker:
-            bom = BomItem(str(self.design.get_value(context.path.to_tuple() + ('fp_refdes',))), 1,
-                          str(self.design.get_value(context.path.to_tuple() + ('fp_value',))),
-                          str(self.design.get_value(context.path.to_tuple() + ('fp_footprint',))),
-                          str(self.design.get_value(context.path.to_tuple() + ('fp_mfr',))),
-                          str(self.design.get_value(context.path.to_tuple() + ('fp_part',))),
-                          str(self.design.get_value(context.path.to_tuple() + ('fp_datasheet',))))
-
-            self.bom_list.append(bom)
+        if self.design.get_value(context.path.to_tuple() + ('fp_footprint',)) is not None:
+            bom_item = BomItem(value=str(self.design.get_value(context.path.to_tuple() + ('fp_value',))),
+                               footprint=str(self.design.get_value(context.path.to_tuple() + ('fp_footprint',))),
+                               manufacturer=str(self.design.get_value(context.path.to_tuple() + ('fp_mfr',))),
+                               part_number=str(self.design.get_value(context.path.to_tuple() + ('fp_part',))),
+                               datasheet=str(self.design.get_value(context.path.to_tuple() + ('fp_datasheet',))))
+            refdes = self.design.get_value(context.path.to_tuple() + ('fp_refdes',))
+            self.bom_list.setdefault(bom_item, []).append(refdes)
 
     def run(self) -> List[BomItem]:
         self.transform_design(self.design.design)
