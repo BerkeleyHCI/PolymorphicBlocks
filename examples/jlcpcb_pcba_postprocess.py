8b4f7313a9b54babb8c28e36281a89eb077980b0
Richard Lin <richard.lin@berkeley.edu>
2023-01-19 15:44:30 -0800

    Update jlcpcb_pcba_postprocess.py

--- examples/jlcpcb_pcba_postprocess.py
+++ examples/jlcpcb_pcba_postprocess.py
@@ -30,8 +30,19 @@ PART_ROTATIONS = {
   'C92482': -90,  # DRV8313 BLDC driver
   'C132291': -90,  # FUSB302B
   'C508453': 180,  # FET
-}
 
+  'C508461': -90,  # SOIC-8 FET
+  'C112032': 180,  # LDO
+  'C413500': -90,  # SOIC-8 gate driver
+  'C476470': -90,  # SOIC-8 digital isolator
+  'C460356': 180,  # SOT-23-5 boost converter
+  'C97521': -90,  # SOIC-8 SPI Flash
+  'C2962219': -90,  # 2x5 1.27mm header shrouded
+  'C126830': 90,  # "SOT-23" USB ESD protector
+  'C6568': -90,  # CP2102 USB UART
+  'C155612': 180,  # SOT-223 1.2v reg
+  'C190271': 180,  # SOT-23-6 93LC56 EEPROM
+}
 
 if __name__ == '__main__':
   def remap_by_dict(elt: str, remap_dict: Dict[str, str]) -> str:
@@ -40,22 +51,26 @@ if __name__ == '__main__':
     else:
       return elt
 
-  # Process the BoM
-  BOM_HEADER_MAP = {
-    'Designation': 'JLCPCB Part #',
-  }
-  with open(f'{args.file_path_prefix}.csv', 'r', newline='') as bom_in, \
-      open(f'{args.file_path_prefix}_jlc.csv', 'w', newline='') as bom_out:
-    csv_in = csv.reader(bom_in, delimiter=';')
-    csv_out = csv.writer(bom_out)
+  refdes_lcsc_map: Dict[str, str] = {}  # refdes -> LCSC part number
+
+  # while we don't need to modify this file, we do need the JLC P/N to refdes map
+  # to apply the rotations, since that data isn't in the placement file
+  with open(f'{args.file_path_prefix}.csv', 'r', newline='') as bom_in:
+    csv_in = csv.reader(bom_in)
 
     rows = list(csv_in)
-    rows[0] = [remap_by_dict(elt, BOM_HEADER_MAP) for elt in rows[0]]
+    refdes_list_index = rows[0].index('Designator')
+    lcsc_index = rows[0].index('JLCPCB Part #')
 
-    for row in rows:
-      csv_out.writerow(row)
+    for i, row in enumerate(rows[1:]):
+      if not row[lcsc_index]:  # ignore rows without part number
+        continue
+      refdes_list = row[refdes_list_index].split(',')
+      for refdes in refdes_list:
+        assert refdes not in refdes_lcsc_map, f"duplicate refdes {refdes} in row {i+1}"
+        refdes_lcsc_map[refdes] = row[lcsc_index]
 
-  print(f"wrote {args.file_path_prefix}_jlc.csv")
+  print(f"read {args.file_path_prefix}.csv")
 
   # Process position CSV
   POS_HEADER_MAP = {
@@ -74,13 +89,17 @@ if __name__ == '__main__':
       rows = list(csv_in)
       rows[0] = [remap_by_dict(elt, POS_HEADER_MAP) for elt in rows[0]]
 
-      lcsc_index = rows[0].index('Val')
+      refdes_index = rows[0].index('Designator')
       rot_index = rows[0].index('Rotation')
 
-      for i, row in enumerate(rows):
-        if row[lcsc_index] in PART_ROTATIONS:
-          row[rot_index] = str((float(row[rot_index]) + PART_ROTATIONS[row[lcsc_index]]) % 360)
-          print(f"correct rotation for row {i}, {row[lcsc_index]}")
+      csv_out.writerow(rows[0])
+
+      for i, row in enumerate(rows[1:]):
+        refdes = row[refdes_index]
+        lcsc_opt = refdes_lcsc_map.get(refdes, None)
+        if lcsc_opt is not None and lcsc_opt in PART_ROTATIONS:
+          row[rot_index] = str((float(row[rot_index]) + PART_ROTATIONS[lcsc_opt]) % 360)
+          print(f"correct rotation for row {i+1} ref {refdes}, {lcsc_opt}")
         csv_out.writerow(row)
 
     print(f"wrote {args.file_path_prefix}-{pos_postfix}-pos_jlc.csv")
