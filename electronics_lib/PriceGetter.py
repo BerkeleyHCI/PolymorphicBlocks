e8ddf9e36b9aefa1abf7f3b792e325e55a4de113
ParthPandhare <parthpandhare@gmail.com>
2023-03-05 16:04:50 -0800

    Update PriceGetter.py

--- electronics_lib/PriceGetter.py
+++ electronics_lib/PriceGetter.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List, Tuple, Dict
 
 import edgir
@@ -25,47 +26,58 @@ class PriceTransform(TransformUtil.Transform):
 
 class GeneratePrice(BaseBackend):
     # part_number -> [ [lower-bound_1, price_1], [lower-bound_2 (and the previous upperbound), price_2], ... ]
-    PRICE_TABLE = Dict[str, List[Tuple[int, float]]] = {}
+    PRICE_TABLE: Dict[str, List[Tuple[int, float]]] = {}
 
     @classmethod
     def get_price_table(cls) -> Dict[str, List[Tuple[int, float]]]:
         if not GeneratePrice.PRICE_TABLE:
-            cur_path = os.path.dirname(__file__)
-            parts_library = os.path.relpath('resources/Pruned_JLCPCB SMT Parts Library(20220419).csv', cur_path)
+            current_path = os.getcwd()
+            parts_library = current_path + "\PolymorphicBlocks\electronics_lib\\resources\Pruned_JLCPCB SMT Parts Library(20220419).csv"
             with open(parts_library, 'r', newline='') as csv_file:
-                csv_reader = csv.DictReader(csv_file, 'r')
+                csv_reader = csv.reader(csv_file, dialect='excel')
+                next(csv_reader)
                 for row in csv_reader:
-                    full_price_list = row['Price']
-                    price_and_quantity_groups = full_price_list.split(',')
-                    value: List[Tuple[int, float]] = []
-                    for price_and_quantity in price_and_quantity_groups:
-                        price_and_quantity_list = price_and_quantity.split(':')
-                        assert len(price_and_quantity_list) == 2
-                        quantity_range = price_and_quantity_list[0].split('-')
-                        assert len(quantity_range) == 1 or len(quantity_range) == 2
-                        value.append((int(quantity_range[0]), float(price_and_quantity_list[1])))
+                    full_price_list = row[10]
+                    if full_price_list == " ":
+                        print("Part " + row[0] + " is missing from the price list.")
+                        value = ([(0, 0.0), (20000, 0.0)])
+                    else:
+                        price_and_quantity_groups = full_price_list.split(',')
+                        value: List[Tuple[int, float]] = []
+                        for price_and_quantity in price_and_quantity_groups:
+                            price_and_quantity_list = price_and_quantity.split(':')
+                            assert len(price_and_quantity_list) == 2
+                            quantity_range = price_and_quantity_list[0].split('-')
+                            assert len(quantity_range) == 1 or len(quantity_range) == 2
+                            value.append((int(quantity_range[0]), float(price_and_quantity_list[1])))
 
                     # sorts each value for PRICE_TABLE by lower-bounds, which is the first int in the Tuple:
                     sorted(value)
-                    GeneratePrice.PRICE_TABLE[row['LCSC Part']] = value
+                    GeneratePrice.PRICE_TABLE[row[0]] = value
         return GeneratePrice.PRICE_TABLE
 
     def generate_price(self, lcsc_part_number: str, quantity: int) -> float:
-        full_price_list = GeneratePrice.PRICE_TABLE[lcsc_part_number]
-        for i in range(len(full_price_list)):
-            if i == len(full_price_list) - 1:   # if you reached the last group
-                return quantity * (full_price_list[i])[1]
-            if quantity < (full_price_list[i + 1])[0]:
-                return (quantity * full_price_list[i])[1]
+        if lcsc_part_number in GeneratePrice.PRICE_TABLE:
+            full_price_list = GeneratePrice.PRICE_TABLE[lcsc_part_number]
+            for i in range(len(full_price_list)):
+                if i == len(full_price_list) - 1:   # if you reached the last group
+                    return quantity * (full_price_list[i])[1]
+                if quantity < (full_price_list[i + 1])[0]:
+                    return (quantity * full_price_list[i])[1]
+        else:
+            print(lcsc_part_number + " is missing from the price list.")
+            return 0
 
     def run(self, design: CompiledDesign, args=None) -> List[Tuple[edgir.LocalPath, str]]:
         assert not args
         price_list = PriceTransform(design).run()
         total_price = 0
         self.get_price_table()
-        for lcsc_part_number, quantity in price_list:
-            total_price += self.generate_price(lcsc_part_number, quantity)
-
+        for lcsc_part_number in price_list:
+            quantity = price_list[lcsc_part_number]
+            print(lcsc_part_number + ": quantity = " + str(quantity))
+            total_price += round(self.generate_price(lcsc_part_number, quantity), 2)
+        print("total price: " + str(total_price))
         return [
             (edgir.LocalPath(), str(total_price))
         ]
