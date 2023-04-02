f75e301d7b6f7440cece33d6b4cc31eb425226d8
Richard Lin <elpato25@gmail.com>
2023-04-01 17:50:54 -0700

    fix switch matrix

--- electronics_lib/SwitchMatrix.py
+++ electronics_lib/SwitchMatrix.py
@@ -5,8 +5,8 @@ from electronics_abstract_parts import *
 
 class SwitchMatrix(HumanInterface, GeneratorBlock):
   """A switch matrix that generates (rows * cols) switches while only using max(rows, cols) IOs, by arranging
-  them in a matrix, having the driver drive one row high at a time and reading which cols are connected
-  (with all cols weakly pulled low).
+  them in a matrix, having the driver drive one col low at a time and reading which rows are low
+  (with all cols weakly pulled high).
   This uses the Switch abstract class, which can be refined into e.g. a tactile switch or mechanical keyswitch.
 
   This generates per-switch diodes which allows multiple keys to be pressed simultaneously.
@@ -16,38 +16,39 @@ class SwitchMatrix(HumanInterface, GeneratorBlock):
   def __init__(self, nrows: IntLike, ncols: IntLike, voltage_drop: RangeLike = (0, 0.7)*Volt):
     super().__init__()
 
-    self.rows = self.Port(Vector(DigitalSink.empty()))
-    self.cols = self.Port(Vector(DigitalSingleSource.empty()))
+    self.rows = self.Port(Vector(DigitalSingleSource.empty()))
+    self.cols = self.Port(Vector(DigitalSink.empty()))
     self.voltage_drop = self.ArgParameter(voltage_drop)
 
     self.generator(self.generate, nrows, ncols)
 
   def generate(self, rows: int, cols: int):
     switch_model = Switch()
-    max_voltage = self.rows.hull(lambda port: port.link().voltage)  # TODO assumes 0 is ground
-    diode_model = Diode(current=(0, 0)*Amp, reverse_voltage=max_voltage, voltage_drop=self.voltage_drop)
+    # col voltage is used as a proxy for voltage, since (properly) using the row voltage causes a circular dependency
+    cols_voltage = self.cols.hull(lambda port: port.link().voltage)
+    diode_model = Diode(current=(0, 0)*Amp, reverse_voltage=cols_voltage, voltage_drop=self.voltage_drop)
 
-    col_ports = {}
-    for col in range(cols):
-      col_ports[col] = self.cols.append_elt(DigitalSingleSource.empty(), str(col))
+    row_ports = {}
+    for row in range(rows):
+      row_ports[row] = self.rows.append_elt(DigitalSingleSource.empty(), str(row))
 
     self.sw = ElementDict[Switch]()
     self.d = ElementDict[Diode]()
-    for row in range(rows):
-      row_port = cast(DigitalSink, self.rows.append_elt(DigitalSink.empty(), str(row)))
-      switch_b_model = DigitalSink()  # ideal, negligible current draw (assumed) and thresholds checked at other side
-      for (col, col_port) in col_ports.items():
+    for col in range(cols):
+      col_port = cast(DigitalSink, self.cols.append_elt(DigitalSink.empty(), str(col)))
+      col_port_model = DigitalSink()  # ideal, negligible current draw (assumed) and thresholds checked at other side
+      for (row, row_port) in row_ports.items():
         sw = self.sw[f"{col},{row}"] = self.Block(switch_model)
         d = self.d[f"{col},{row}"] = self.Block(diode_model)
-        self.connect(d.anode.adapt_to(switch_b_model), row_port)
-        self.connect(d.cathode, sw.a)
-        lowest_output = row_port.link().output_thresholds.upper() - d.actual_voltage_drop.upper()
-        highest_output = row_port.link().voltage.upper() - d.actual_voltage_drop.lower()
-        self.connect(sw.b.adapt_to(DigitalSingleSource(
+        lowest_output = col_port.link().voltage.lower() + d.actual_voltage_drop.lower()
+        highest_output = col_port.link().output_thresholds.lower() + d.actual_voltage_drop.upper()
+        self.connect(d.anode.adapt_to(DigitalSingleSource(
           voltage_out=(lowest_output, highest_output),
-          output_thresholds=(-float('inf'), lowest_output),
-          pulldown_capable=False, high_signal_driver=True
-        )), col_port)
+          output_thresholds=(highest_output, float('inf')),
+          low_signal_driver=True
+        )), row_port)
+        self.connect(d.cathode, sw.a)
+        self.connect(sw.b.adapt_to(col_port_model), col_port)
 
     self.rows.defined()
     self.cols.defined()
