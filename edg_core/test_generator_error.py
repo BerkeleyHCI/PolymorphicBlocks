395dfe559b9505b150cd169b7c84609e3d7acb16
Richard Lin <elpato25@gmail.com>
2023-04-16 14:30:37 -0700

    Update test_generator_error.py

--- edg_core/test_generator_error.py
+++ edg_core/test_generator_error.py
@@ -8,38 +8,22 @@ class BadGeneratorTestCase(unittest.TestCase):
   # These are internal classes to avoid this error case being auto-discovered in a library
 
   class InvalidMissingGeneratorBlock(GeneratorBlock):
-    def __init__(self) -> None:
-      super().__init__()
-      # this is missing a self.generator statement
+    """This doesn't implement generator()"""
 
   def test_missing_generator(self) -> None:
     with self.assertRaises(BlockDefinitionError):
       self.InvalidMissingGeneratorBlock()._elaborated_def_to_proto()
 
 
-  class InvalidMultiGeneratorBlock(GeneratorBlock):
-    def __init__(self) -> None:
-      super().__init__()
-      self.generator(self.dummy_gen)
-      self.generator(self.dummy_gen)
-
-    def dummy_gen(self) -> None:
-      pass
-
-  def test_multi_generator(self) -> None:
-    with self.assertRaises(AssertionError):
-      self.InvalidMultiGeneratorBlock()._elaborated_def_to_proto()
-
-
   class InvalidNonArgGeneratorBlock(GeneratorBlock):
     def __init__(self) -> None:
       super().__init__()
       self.param = self.Parameter(FloatExpr())
-      self.generator(self.dummy_gen, self.param)
+      self.param_value = self.GeneratorParam(self.param, float)
 
-    def dummy_gen(self, x: float) -> None:
-      pass
+    def generate(self) -> None:
+      super().generate()
 
   def test_non_arg_generator(self) -> None:
-    with self.assertRaises(AssertionError):
+    with self.assertRaises(BlockDefinitionError):
       self.InvalidNonArgGeneratorBlock()._elaborated_def_to_proto()
