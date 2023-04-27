816fefdb2c3103f36866e726a11dff7215a9ad14
Richard Lin <richard.lin@berkeley.edu>
2023-04-27 12:19:05 +0200

    Python packaging defs (#247)
    - Add pyproject.toml
    - Refactor paths for packaging:
      - precompiled compiler now in edg_core/resources
    - HDL interface server in its own package, to be runnable with `python
    -m`
    - Update documentation
    - Package uploaded to pip

---
+++ edg_hdl_server/__main__.py
@@ -0,0 +1,160 @@
+import importlib
+import inspect
+import sys
+from types import ModuleType
+from typing import Set, Type, Tuple, TypeVar, cast
+
+import edgir
+import edgrpc
+from edg_core import *
+from edg_core.Core import NonLibraryProperty
+
+
+EDG_PROTO_VERSION = 1
+
+
+class LibraryElementIndexer:
+  """Indexer for libraries, recursively searches modules and their LibraryElements."""
+  def __init__(self):
+    self.seen_modules: Set[ModuleType] = set()
+    self.seen_elements: Set[Type[LibraryElement]] = set()
+
+  def index_module(self, module: ModuleType) -> Set[Type[LibraryElement]]:
+    assert not self.seen_elements and not self.seen_modules
+    self._search_module(module)
+    return self.seen_elements
+
+  def _search_module(self, module: ModuleType) -> None:
+    # avoid repeated work and re-indexing modules
+    if (module.__name__ in sys.builtin_module_names
+            or not hasattr(module, '__file__')  # apparently load six.moves breaks
+            or module in self.seen_modules):
+      return
+    self.seen_modules.add(module)
+
+    for (name, member) in inspect.getmembers(module):
+      if inspect.ismodule(member):  # recurse into visible modules
+        self._search_module(member)
+
+      if inspect.isclass(member) and issubclass(member, LibraryElement) and not issubclass(member, DesignTop) \
+              and member not in self.seen_elements \
+              and (member, NonLibraryProperty) not in member._elt_properties:  # process elements
+        self.seen_elements.add(member)
+
+        for mro in member.mro():
+          self._search_module(importlib.import_module(mro.__module__))
+
+        if issubclass(member, Port):  # TODO for some reason, Links not in __init__ are sometimes not found
+          obj = member()  # TODO can these be class definitions?
+          if hasattr(obj, 'link_type'):
+            self._search_module(importlib.import_module(obj.link_type.__module__))
+
+
+LibraryElementType = TypeVar('LibraryElementType', bound=LibraryElement)
+def elaborate_class(elt_cls: Type[LibraryElementType]) -> Tuple[LibraryElementType, edgir.Library.NS.Val]:
+  obj = elt_cls()
+  if isinstance(obj, Block):
+    block_proto = builder.elaborate_toplevel(obj)
+    return obj, edgir.Library.NS.Val(hierarchy_block=block_proto)  # type: ignore
+  elif isinstance(obj, Link):
+    link_proto = builder.elaborate_toplevel(obj)
+    assert isinstance(link_proto, edgir.Link)  # TODO this needs to be cleaned up
+    return obj, edgir.Library.NS.Val(link=link_proto)  # type: ignore
+  elif isinstance(obj, Bundle):  # TODO: note Bundle extends Port, so this must come first
+    return obj, edgir.Library.NS.Val(bundle=obj._def_to_proto())  # type: ignore
+  elif isinstance(obj, Port):
+    return obj, edgir.Library.NS.Val(port=cast(edgir.Port, obj._def_to_proto()))  # type: ignore
+  else:
+    raise RuntimeError(f"didn't match type of library element {elt_cls}")
+
+
+LibraryClassType = TypeVar('LibraryClassType')
+def class_from_library(elt: edgir.LibraryPath, expected_superclass: Type[LibraryClassType]) -> \
+        Type[LibraryClassType]:
+  elt_split = elt.target.name.split('.')
+  elt_module = importlib.import_module('.'.join(elt_split[:-1]))
+  assert inspect.ismodule(elt_module)
+  cls = getattr(elt_module, elt_split[-1])
+  assert issubclass(cls, expected_superclass)
+  return cls
+
+
+def run_server():
+  stdin_deserializer = BufferDeserializer(edgrpc.HdlRequest, sys.stdin.buffer)
+  stdout_serializer = BufferSerializer[edgrpc.HdlResponse](sys.stdout.buffer)
+
+  while True:
+    request = stdin_deserializer.read()
+    if request is None:  # end of stream
+      sys.exit(0)
+
+    response = edgrpc.HdlResponse()
+    try:
+      if request.HasField('index_module'):
+        module = importlib.import_module(request.index_module.name)
+        library = LibraryElementIndexer()
+        indexed = [edgir.LibraryPath(target=edgir.LocalStep(name=indexed._static_def_name()))
+                   for indexed in library.index_module(module)]
+        response.index_module.indexed.extend(indexed)
+      elif request.HasField('get_library_element'):
+        cls = class_from_library(request.get_library_element.element,
+                                 LibraryElement)  # type: ignore
+        obj, obj_proto = elaborate_class(cls)
+
+        response.get_library_element.element.CopyFrom(obj_proto)
+        if isinstance(obj, DesignTop):
+          obj.refinements().populate_proto(response.get_library_element.refinements)
+      elif request.HasField('elaborate_generator'):
+        generator_type = class_from_library(request.elaborate_generator.element,
+                                            GeneratorBlock)
+        generator_obj = generator_type()
+
+        response.elaborate_generator.generated.CopyFrom(builder.elaborate_toplevel(
+          generator_obj,
+          is_generator=True,
+          generate_values=[(value.path, value.value) for value in request.elaborate_generator.values]))
+      elif request.HasField('run_refinement'):
+        refinement_pass_class = class_from_library(request.run_refinement.refinement_pass,
+                                                   BaseRefinementPass)  # type: ignore
+        refinement_pass = refinement_pass_class()
+
+        refinement_results = refinement_pass.run(
+          CompiledDesign.from_request(request.run_refinement.design, request.run_refinement.solvedValues))
+        response.run_refinement.SetInParent()
+        for path, refinement_result in refinement_results:
+          new_value = response.run_refinement.newValues.add()
+          new_value.path.CopyFrom(path)
+          new_value.value.CopyFrom(refinement_result)
+      elif request.HasField('run_backend'):
+        backend_class = class_from_library(request.run_backend.backend,
+                                           BaseBackend)  # type: ignore
+        backend = backend_class()
+
+        backend_results = backend.run(
+          CompiledDesign.from_request(request.run_backend.design, request.run_backend.solvedValues),
+          dict(request.run_backend.arguments))
+        response.run_backend.SetInParent()
+        for path, backend_result in backend_results:
+          response_result = response.run_backend.results.add()
+          response_result.path.CopyFrom(path)
+          response_result.text = backend_result
+      elif request.HasField('get_proto_version'):
+        response.get_proto_version = EDG_PROTO_VERSION
+      else:
+        raise RuntimeError(f"Unknown request {request}")
+    except BaseException as e:
+      import traceback
+      # exception formatting from https://stackoverflow.com/questions/4564559/get-exception-description-and-stack-trace-which-caused-an-exception-all-as-a-st
+      response.error.error = repr(e)
+      response.error.traceback = "".join(traceback.TracebackException.from_exception(e).format())
+      # also print it, to preserve the usual behavior of errors in Python
+      traceback.print_exc()
+
+    sys.stdout.buffer.write(stdin_deserializer.read_stdout())
+    stdout_serializer.write(response)
+
+
+# In some cases stdout seems to buffer excessively, in which case starting python with -u seems to work
+# https://stackoverflow.com/a/35467658/5875811
+if __name__ == '__main__':
+  run_server()
--- setup.md
+++ setup.md
@@ -1,8 +1,10 @@
 # Setup
-_Setting up and using the IDE is recommended, but HDL-only setup is also documented below._
+
+## HDL Core Setup
+_These are the instructions to set up the HDL core, both for command-line compilation as well as using the IDE (requires additional setup in the next section)._
 _Runs natively on Windows, Linux, and Mac._
 
-1. Download [sbt](https://www.scala-sbt.org/download.html), the Scala build tool.
+1. Make sure you are using Python 3.9 (or later).
 2. If you do not have a Java JDK installed, download and install one.
    An open-source one is [Eclipse Temurin](https://adoptium.net/temurin/releases/?version=17).
    Java 11 (or later) is required.
@@ -23,10 +25,19 @@ _Runs natively on Windows, Linux, and Mac._
    Version reporting formats are not standardized, for example Oracle's Java 8 may report as `Oracle Corporation Java 1.8.0_351`.
    </details>
    <!--Reference: JDK compatibility from https://docs.scala-lang.org/overviews/jdk-compatibility/overview.html-->
-3. Download or clone the IDE plugin sources from https://github.com/BerkeleyHCI/edg-ide.
+3. Install the Python package using the package manager:  
+   `pip install edg`
+
+
+## IDE Setup
+_Using the IDE is recommended since it provides a design browser and block diagram visualizer, but it's optional._
+_Currently, the IDE must be built from source (it's pretty straightforward), but in the future we may make pre-compiled versions available._
+
+1. Download [sbt](https://www.scala-sbt.org/download.html), the Scala build tool.
+2. Download or clone the IDE plugin sources from https://github.com/BerkeleyHCI/edg-ide.
     - If using command line git: make sure to initialize submodules: `git submodule update --init --recursive`.
     - If using GitHub Desktop: it should automatically clone submodules for you.
-4. In the `edg-ide` directory, run `sbt runIDE`.
+3. In the `edg-ide` directory, run `sbt runIDE`.
    sbt will automatically fetch dependencies, compile the plugin, and start the IDE with the plugin enabled.
     - The first run may take a while.
    <details> <summary>Resolving common errors</summary>
@@ -41,33 +52,10 @@ _Runs natively on Windows, Linux, and Mac._
       this is because your Java version is pre-11.
       See the section above for instructions to install a more recent JDK.
    </details>
-5. In the IDE, **open the `PolymorphicBlocks` folder as a project**.
-    - You do not need to clone `PolymorphicBlocks` separately, you can use the submodule in `edg-ide`.
-6. Once the project loads, open `blinky_skeleton.py`.
-7. Set up the Python interpreter once the prompt shows up, "Configure Python interpreter".
-    - Recommended: set up a Virtualenv environment based on a Python 3.7 (or later) interpreter.
-    - Install Python requirements (packages) if prompted, this banner should show up in the `blinky_skeleton.py` editor:  
-      ![Dependencies banner](docs/ide/ide_deps.png)
-    - When requirements finish installing, this popup will show up on the bottom right:  
-      ![Install complete popup](docs/ide/ide_deps_complete.png)
-   <details> <summary>Using other interpreters</summary>
-
-    - If using Pipenv (may need to be installed separately), IntelliJ should also prompt you to install dependencies similarly to the Virtualenv case above.
-    - If using System Interpreter or Conda: you will need to install dependencies manually, `pip install -r requirements.txt`.
-        - On Ubuntu, you may need to select a particular version of Python for pip, using `python3.8 -m pip` instead of `pip` directly.
-   </details>
-9. Open the Block Visualizer tab on the right.
-   It will be empty right now.
-
-
-<details> <summary>Alternatively, HDL-only setup...</summary>
-
-_This isn't necessary if you're using the IDE._
-_Runs natively on Windows, Linux, and Mac._
-
-1. Make sure you are using Python 3.7 (or later).
-2. Make sure you have Java 11 or later.
-3. Install the needed dependencies.
-   If using pip: `pip install -r requirements.txt`
-    - If on Linux and you get an error along the lines of `python: command not found`, you may need to `apt install python-is-python3`.
-</details>
+4. Within the IDE, create a new Python project.
+   - The location can be anywhere.
+   - The default environment type of Virtualenv is fine.
+   - **Make sure to check "inherit global site-packages"**, so that the pip-installed package will be visible.
+   > A prior version of this setup guide had the IDE open the PolymorphicBlocks repository and develop directly on it.
+   > Unless you are developing the core HDL or core libraries, this pip package + new project setup flow is recommended.
+   > However, the IDE does also support working with the PolymorphicBlocks project.
