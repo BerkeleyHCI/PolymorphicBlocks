dcbad77a39253c00faaf7ede37e6e645ec482315
Richard Lin <elpato25@gmail.com>
2023-06-17 21:59:54 -0700

    Update test_swd_debugger.py

--- examples/test_swd_debugger.py
+++ examples/test_swd_debugger.py
@@ -3,36 +3,29 @@ import unittest
 from edg import *
 
 
-class SwdCortexSourceHeaderHorizontal(ProgrammingConnector, FootprintBlock):
+class SwdCortexSourceHeader(ProgrammingConnector, FootprintBlock):
   def __init__(self) -> None:
     super().__init__()
 
-    self.pwr = self.Port(VoltageSink(), [Power])
-    self.gnd = self.Port(Ground(), [Common])  # TODO pin at 0v
-    self.swd = self.Port(SwdTargetPort(), [Input])
-    self.swo = self.Port(DigitalBidir(), optional=True)
-    self.tdi = self.Port(DigitalBidir(), optional=True)
+    self.pwr = self.Port(VoltageSink.empty(), [Power])
+    self.gnd = self.Port(Ground.empty(), [Common])  # TODO pin at 0v
+    self.swd = self.Port(SwdTargetPort.empty(), [Input])
+    self.swo = self.Port(DigitalBidir.empty(), optional=True)
+    self.tdi = self.Port(DigitalBidir.empty(), optional=True)
 
   def contents(self):
     super().contents()
+    self.conn = self.Block(PinHeader127DualShrouded(10))
 
-    self.footprint(
-      'J', 'edg:PinHeader_2x05_P1.27mm_Horizontal_Shrouded',
-      {
-        '1': self.pwr,
-        '2': self.swd.swdio,
-        '3': self.gnd,
-        '4': self.swd.swclk,
-        '5': self.gnd,
-        '6': self.swo,
-        # '7': ,  # key pin technically doesn't exist
-        '8': self.tdi,  # or NC
-        '9': self.gnd,
-        '10': self.swd.reset,
-      },
-      mfr='CNC Tech', part='3220-10-0200-00',
-      value='SWD'
-    )
+    self.connect(self.pwr, self.conn.pins.request('1').adapt_to(VoltageSink()))
+    self.connect(self.gnd, self.conn.pins.request('3').adapt_to(Ground()),
+                 self.conn.pins.request('5').adapt_to(Ground()),
+                 self.conn.pins.request('9').adapt_to(Ground()))
+    self.connect(self.swd.swdio, self.conn.pins.request('2').adapt_to(DigitalBidir()))
+    self.connect(self.swd.swclk, self.conn.pins.request('4').adapt_to(DigitalSink()))
+    self.connect(self.swo, self.conn.pins.request('6').adapt_to(DigitalBidir()))
+    self.connect(self.tdi, self.conn.pins.request('8').adapt_to(DigitalBidir()))
+    self.connect(self.swd.reset, self.conn.pins.request('10').adapt_to(DigitalSink()))
 
 
 class SwdCortexSourceTagConnect(ProgrammingConnector, FootprintBlock):
@@ -134,13 +127,16 @@ class SwdDebugger(JlcBoardTop):
       self.connect(self.mcu.gpio.request('target_swdio_out'), self.target_drv.swdio_out)
       self.connect(self.mcu.gpio.request('target_swdio_in'), self.target_drv.swdio_in)
       self.connect(self.mcu.gpio.request('target_reset'), self.target_drv.reset_in)
+      self.reset_pull = imp.Block(PullupResistor(10*kOhm(tol=0.05))).connected(io=self.target_drv.reset_in)
+      self.reset_sw = imp.Block(DigitalSwitch())
+      self.connect(self.reset_sw.out, self.target_drv.reset_in)
       self.connect(self.mcu.gpio.request('target_swo'), self.target_drv.swo_out)
 
     with self.implicit_connect(
             ImplicitConnect(self.vtarget, [Power]),
             ImplicitConnect(self.gnd, [Common]),
     ) as imp:
-      self.target = imp.Block(SwdCortexSourceTagConnect())
+      self.target = imp.Block(SwdCortexSourceHeader())
       self.connect(self.target_drv.swd, self.target.swd)
       self.connect(self.target_drv.swo_in, self.target.swo)
 
@@ -155,14 +151,10 @@ class SwdDebugger(JlcBoardTop):
     return super().refinements() + Refinements(
       instance_refinements=[
         (['mcu'], Stm32f103_48),
-        (['mcu', 'swd'], SwdCortexTargetTagConnect),
-        (['mcu', 'swd', 'conn'], TagConnectNonLegged),
         (['usb_reg'], Ap2204k),
       ],
       instance_values=[
         (['refdes_prefix'], 'S'),  # unique refdes for panelization
-        (['vusb_protect', 'diode', 'footprint_spec'], 'Diode_SMD:D_SOD-323'),
-
         (['mcu', 'crystal', 'frequency'], Range.from_tolerance(8000000, 0.005)),
         (['mcu', 'pin_assigns'], [
           # these are pinnings on stock st-link
@@ -173,9 +165,8 @@ class SwdDebugger(JlcBoardTop):
           'target_reset=PB0',
           'target_swo=PA10',
           'led_target=PA9',
-          # these are custom additional parts
-          'led_usb=13',
-          'target_reg_en=38',
+          'led_usb=PB6',  # CONNECTED_LED in DAPLing, LED_DAP_BLUE in F103 mbed HDK
+          'target_reg_en=PB15',  # POWER_EN in DAPLink
         ]),
 
         # 2.2uF generates a 1206, but 4.7uF allows a 0805
