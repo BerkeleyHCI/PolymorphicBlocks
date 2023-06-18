80c204cc73ee9a634600fdd57f23ef3daa7700ee
Richard Lin <elpato25@gmail.com>
2023-06-17 21:20:06 -0700

    wip still doesn't work

--- examples/test_fcml/mcu_firmware/main.cpp
+++ examples/test_fcml/mcu_firmware/main.cpp
@@ -28,6 +28,7 @@ void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
+  pinMode(sw, INPUT_PULLUP);
   digitalWrite(led0, 0);
   digitalWrite(led1, 0);
   digitalWrite(led2, 1);
