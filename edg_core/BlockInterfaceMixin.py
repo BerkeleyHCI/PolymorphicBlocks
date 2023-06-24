54d08dd2bc416abc10e57f4b7d5e450d2b4fc70e
Richard Lin <elpato25@gmail.com>
2023-06-24 15:19:01 -0700

    Update BlockInterfaceMixin.py

--- edg_core/BlockInterfaceMixin.py
+++ edg_core/BlockInterfaceMixin.py
@@ -1,11 +1,12 @@
-from abc import ABC
 from typing import TypeVar, Generic
+
+import edgir
 from .HierarchyBlock import BaseBlock, Block
 
 MixinBaseType = TypeVar('MixinBaseType', bound=Block)
 
 
-class BlockInterfaceMixin(BaseBlock, Generic[MixinBaseType], ABC):
+class BlockInterfaceMixin(BaseBlock[edgir.HierarchyBlock], Generic[MixinBaseType]):
     """An interface mixin, allowing additional interface elements (parameters, ports) to be added to
     a block interface - e.g., IoController with HasI2s.
 
@@ -16,6 +17,21 @@ class BlockInterfaceMixin(BaseBlock, Generic[MixinBaseType], ABC):
     class, e.g. IoController.with(HasI2s()). Instances of this class are not really valid on their own.
 
     This may define __init__ parameters as keyword arguments ONLY. Otherwise, this can mess up
-    parameter propagation in super().__init__ calls in implementing classes."""
-    pass
+    parameter propagation in super().__init__ calls in implementing classes.
+
+    This should only be used for defining interface mixins. Implementation mixins should be defined
+    using the Block class hierarchy. This class does not inherit Block to avoid inheriting the
+    ports of the mixin base class which reduces autocomplete clutter.
+    TODO: is this a good decision?
+    TODO: what about cases where it's a bit mixed, e.g. HasI2s also needs to register the self.i2s port?
+    """
+    def __init__(self):
+        super().__init__()
+        # TODO implement me
+        pass
+
+    def contents(self) -> None:
+        # TODO implement me
+        pass
+
 
