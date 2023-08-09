4bd7378f7ac8c5e83a8ca808e802331a50d5f824
Richard Lin <elpato25@gmail.com>
2023-08-09 01:31:47 -0700

    Update Resetable.py

--- electronics_abstract_parts/Resetable.py
+++ electronics_abstract_parts/Resetable.py
@@ -13,7 +13,7 @@ class Resetable(BlockInterfaceMixin[Block]):
     pin high is insufficient.
 
     Microcontrollers may generate internal programming connectors that drive this signal, and system designers must
-    connect microcontroller resets with this in mind.
+    connect microcontroller resets with this in mind - for example, only driving them in open-drain mode.
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
