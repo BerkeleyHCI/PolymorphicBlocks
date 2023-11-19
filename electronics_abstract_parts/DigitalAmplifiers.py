cb7bc7e113fb44c1732ac3e7bd476e8a36f0a66e
Richard Lin <richard.lin@berkeley.edu>
2023-11-18 19:05:59 -0800

    Update DigitalAmplifiers.py

--- electronics_abstract_parts/DigitalAmplifiers.py
+++ electronics_abstract_parts/DigitalAmplifiers.py
@@ -4,7 +4,7 @@ from .AbstractResistor import Resistor
 from .Categories import PowerSwitch
 
 
-class HighSideSwitch(PowerSwitch, Block):
+class HighSideSwitch(PowerSwitch, KiCadSchematicBlock):
   @init_in_parent
   def __init__(self, pull_resistance: RangeLike = 10000*Ohm(tol=0.01), max_rds: FloatLike = 1*Ohm,
                frequency: RangeLike = RangeExpr.ZERO) -> None:
@@ -20,14 +20,15 @@ class HighSideSwitch(PowerSwitch, Block):
     self.max_rds = self.ArgParameter(max_rds)
     self.frequency = self.ArgParameter(frequency)
 
+  def contents(self):
+    super().contents()
+
     pwr_voltage = self.pwr.link().voltage
-    out_current = self.output.link().current_drawn
     pull_resistance = self.pull_resistance
     pull_current_max = pwr_voltage.upper() / pull_resistance.lower()
     pull_power_max = pwr_voltage.upper() * pwr_voltage.upper() / pull_resistance.lower()
 
     gate_voltage = (3.0, 3.0) #(self.get(self.control.link().voltage)[1],  # TODO with better const prop we should use output_threshold[1]
-    # self.get(self.control.link().voltage)[1])
 
     low_amp_rds_max = pull_resistance.lower() / 1000
 
@@ -41,21 +42,13 @@ class HighSideSwitch(PowerSwitch, Block):
       frequency=self.frequency,
       drive_current=self.control.link().current_limits  # TODO this is kind of a max drive current
     ))
-    self.connect(self.pre.source.adapt_to(Ground()), self.gnd)
-    self.connect(self.pre.gate.adapt_to(DigitalSink(
-      current_draw=(0, 0) * Amp  # TODO model pullup resistor current
-      # no voltage limits or threshold, those are constraints to pass into the FETs
-    )), self.control)
-
     self.pull = self.Block(Resistor(
       resistance=pull_resistance,
       power=(0, pull_power_max)
     ))
-    self.connect(self.pull.a.adapt_to(VoltageSink()), self.pwr)
-
     self.drv = self.Block(SwitchFet.PFet(
       drain_voltage=pwr_voltage,
-      drain_current=out_current,
+      drain_current=self.output.link().current_drawn,
       gate_voltage=pwr_voltage,
       rds_on=(0, self.max_rds),
       gate_charge=(0, float('inf')),  # TODO size on turnon time
@@ -64,17 +57,23 @@ class HighSideSwitch(PowerSwitch, Block):
       drive_current=(-1 * pwr_voltage.lower() / pull_resistance.upper(),
                      pwr_voltage.lower() / low_amp_rds_max)  # TODO simultaneously solve both FETs
     ))
-    self.connect(self.drv.source.adapt_to(VoltageSink(
-      current_draw=self.output.link().current_drawn
-    )), self.pwr)
-    self.connect(self.drv.drain.adapt_to(DigitalSource(
-      voltage_out=(0, self.pwr.link().voltage.upper()),
-      current_limits=self.drv.actual_drain_current_rating,
-      output_thresholds=(0, self.pwr.link().voltage.upper()),
-    )), self.output)
-    self.connect(self.pre.drain.adapt_to(DigitalSource()),
-                 self.drv.gate.adapt_to(DigitalSink()),
-                 self.pull.b.adapt_to(DigitalBidir()))
+
+    self.import_kicad(self.file_path("resources", f"{self.__class__.__name__}.kicad_sch"),
+                                     conversions={
+                                       'pwr': VoltageSink(
+                                         current_draw=self.output.link().current_drawn
+                                       ),
+                                       'output': DigitalSource(
+                                         voltage_out=(0, self.pwr.link().voltage.upper()),
+                                         current_limits=self.drv.actual_drain_current_rating,
+                                         output_thresholds=(0, self.pwr.link().voltage.upper()),
+                                       ),
+                                       'control': DigitalSink(
+                                         current_draw=(0, 0)*Amp  # TODO model pullup resistor current
+                                         # no voltage limits or threshold, those are constraints to pass into the FETs
+                                       ),
+                                       'gnd': Ground(),
+                                     })
 
 
 class OpenDrainDriver(PowerSwitch, Block):
