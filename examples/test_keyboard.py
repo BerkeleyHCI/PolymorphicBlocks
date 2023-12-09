c7279622d123307f5f4ef2c503ec79b3b50fa37f
Richard Lin <elpato25@gmail.com>
2023-12-08 19:10:38 -0800

    Xiao RP2040 keyboard example

--- examples/test_keyboard.py
+++ examples/test_keyboard.py
@@ -16,10 +16,25 @@ from edg import *
 class Keyboard(JlcBoardTop):
   def contents(self) -> None:
     super().contents()
+    self.mcu = self.Block(IoController())
+    self.mcu_pwr = self.mcu.with_mixin(IoControllerPowerOut())
+
+    self.pwr_led = self.Block(IndicatorLed())
+    self.connect(self.pwr_led.gnd, self.mcu_pwr.gnd_out)
+    self.connect(self.pwr_led.signal, self.mcu_pwr.pwr_out.as_digital_source())
+
+    self.led = self.Block(IndicatorLed())
+    self.connect(self.led.gnd, self.mcu_pwr.gnd_out)
+    self.connect(self.led.signal, self.mcu.gpio.request())
+
+    self.sw = self.Block(SwitchMatrix(2, 2))
+    self.connect(self.sw.cols, self.mcu.gpio.request_vector('cols'))
+    self.connect(self.sw.rows, self.mcu.gpio.request_vector('rows'))
 
   def refinements(self) -> Refinements:
     return super().refinements() + Refinements(
       instance_refinements=[
+        (['mcu'], Xiao_Rp2040),
       ],
       instance_values=[
       ],
