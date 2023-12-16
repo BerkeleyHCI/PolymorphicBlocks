15195578af9ccc4b643d3da0a85ced646e41fd8c
Richard Lin <elpato25@gmail.com>
2023-12-15 22:02:01 -0800

    library dump code

--- library_to_json.py
+++ library_to_json.py
@@ -1,7 +1,8 @@
 # Simple tool that scans for libraries and dumps the whole thing to a proto file
 import sys
-from typing import TypedDict, Tuple, Optional, Union
+from typing import TypedDict, Tuple, Optional
 import json
+import inspect
 
 from edg_hdl_server.__main__ import LibraryElementIndexer
 import edg_core
@@ -20,10 +21,13 @@ class PortJsonDict(TypedDict):
     is_array: bool
     hint_position: str  # left | right | up | down | '' (empty)
 
-def port_to_dir(name: str, target: edgir.ref_pb2.LibraryPath) -> str:
+def port_to_dir(name: str, target: edgir.ref_pb2.LibraryPath) -> Optional[str]:
     simpleTarget = simpleName(target)
 
-    if simpleTarget == 'VoltageSource':
+    if simpleTarget == 'Passive':
+        return None
+
+    elif simpleTarget == 'VoltageSource':
         return 'right'
     elif simpleTarget == 'VoltageSink':
         if name == 'gnd':
@@ -38,14 +42,14 @@ def port_to_dir(name: str, target: edgir.ref_pb2.LibraryPath) -> str:
     elif simpleTarget == 'DigitalSink':
         return 'left'
     elif simpleTarget == 'DigitalBidir':
-        return ''
+        return None
 
     elif simpleTarget == 'AnalogSource':
         return 'right'
     elif simpleTarget == 'AnalogSink':
         return 'left'
     elif simpleTarget == 'AnalogBidir':
-        return ''
+        return None
 
     elif simpleTarget == 'I2cController':
         return 'right'
@@ -68,7 +72,7 @@ def port_to_dir(name: str, target: edgir.ref_pb2.LibraryPath) -> str:
 
     else:
         print(f"unknown direction {simpleTarget}")
-        return ''
+        return None
 
 def pb_to_port(pair: edgir.elem_pb2.NamedPortLike):
     if pair.value.HasField('lib_elem'):
@@ -91,8 +95,8 @@ def pb_to_port(pair: edgir.elem_pb2.NamedPortLike):
 
 class ParamJsonDict(TypedDict):
     name: str
-    type: str  # int | float | range | string
-    defaultValue: Optional[str]  # in Python HDL
+    type: str  # int | float | bool | range | string | array
+    default_value: Optional[str]  # in Python HDL
 
 
 class BlockJsonDict(TypedDict):
@@ -101,6 +105,7 @@ class BlockJsonDict(TypedDict):
     superClasses: list[str]  # superclasses of self
     ports: list[PortJsonDict]
     argParams: list[ParamJsonDict]
+    is_abstract: bool
 
 
 class NetJsonDict(TypedDict):
@@ -112,15 +117,20 @@ class NetlistJsonDict(TypedDict):
     nets: list[NetJsonDict]
 
 
-OUTPUT_FILE = "library.edg"
+OUTPUT_FILE = "library.json"
 
 if __name__ == '__main__':
     library = LibraryElementIndexer()
 
     pb = edgir.Library()
 
+    all_blocks = []
+
     count = 0
     for cls in library.index_module(edg):
+        # from edg import IndicatorLed
+        # cls = IndicatorLed
+
         obj = cls()
         name = cls.__name__
         if isinstance(obj, edg_core.Block):
@@ -132,19 +142,82 @@ if __name__ == '__main__':
 
             # inspect into the args to get ArgParams
             argParams = []
+            sig = inspect.signature(cls.__init__)
+            for param_name, param in list(sig.parameters.items())[1:]:  # drop 'self'
+                # TODO NEED TO RECURSE FOR *ARGS / **KWARGS
+                if param.kind in [inspect.Parameter.VAR_POSITIONAL, inspect.Parameter.VAR_KEYWORD,
+                                  inspect.Parameter.POSITIONAL_OR_KEYWORD, inspect.Parameter.POSITIONAL_ONLY,
+                                  inspect.Parameter.KEYWORD_ONLY]:
+                    param_pb = edgir.pair_get_opt(block_proto.params, param_name)
+                    if param_pb is not None:
+                        if param.default is inspect.Parameter.empty:
+                            default_value = None
+                        elif isinstance(param.default, (int, float, str, bool)):
+                            default_value = param.default
+                        elif isinstance(param.default, tuple) and len(param.default) == 2 and \
+                                isinstance(param.default[0], (int, float)) and isinstance(param.default[1], (int, float)):
+                            default_value = param.default
+                        elif isinstance(param.default, edg_core.Range):
+                            range = param.default
+                            default_value = (range.lower, range.upper)
+                        elif isinstance(param.default, edg_core.RangeExpr):
+                            if isinstance(param.default.binding, edg_core.ConstraintExpr.RangeLiteralBinding):
+                                range = param.default.binding.value
+                                default_value = (range.lower, range.upper)
+                            else:
+                                print(f"{name}.{param_name}: bad binding {param.default.binding}")
+                        elif isinstance(param.default, edg_core.FloatExpr):
+                            if isinstance(param.default.binding, edg_core.ConstraintExpr.FloatLiteralBinding):
+                                default_value = param.default.binding.value
+                            else:
+                                print(f"{name}.{param_name}: bad binding {param.default.binding}")
+                        elif isinstance(param.default, edg_core.BoolExpr):
+                            if isinstance(param.default.binding, edg_core.ConstraintExpr.BoolLiteralBinding):
+                                default_value = param.default.binding.value
+                            else:
+                                print(f"{name}.{param_name}: bad binding {param.default.binding}")
+                        elif isinstance(param.default, edg_core.StringExpr):
+                            if isinstance(param.default.binding, edg_core.ConstraintExpr.StringLiteralBinding):
+                                default_value = param.default.binding.value
+                            else:
+                                print(f"{name}.{param_name}: bad binding {param.default.binding}")
+                        else:
+                            print(f"{name}.{param_name}: failed to parse default {param.default}")
+                            default_value = None
+
+                        if param_pb.HasField('floating'):
+                            param_type = 'float'
+                        elif param_pb.HasField('integer'):
+                            param_type = 'int'
+                        elif param_pb.HasField('boolean'):
+                            param_type = 'bool'
+                        elif param_pb.HasField('text'):
+                            param_type = 'str'
+                        elif param_pb.HasField('range'):
+                            param_type = 'range'
+                        elif param_pb.HasField('array'):
+                            param_type = 'array'
+                        else:
+                            raise ValueError(f"{name}.{param_name} unknown param type {param_pb}")
+
+                        argParams.append(ParamJsonDict(
+                            name=param_name,
+                            type=param_type,
+                            defaultValue=default_value
+                        ))
+                    else:
+                        print(f"missing param {param_name} in {name}")
 
             block_dict = BlockJsonDict(
                 name="",  # empty for libraries
                 type=simpleName(block_proto.self_class),
                 superClasses=[simpleName(superclass) for superclass in block_proto.superclasses],
                 ports=ports,
-                argParams=argParams
+                argParams=argParams,
+                is_abstract=block_proto.is_abstract
             )
 
-            text = json.dumps(block_dict)
-            print(text)
-
-            sys.exit(0)  # just do one for now
+            all_blocks.append(block_dict)
         # elif isinstance(obj, edg_core.Link):
         #     print(f"Elaborating link {name}")
         #     link_proto = builder.elaborate_toplevel(obj)
@@ -160,7 +233,7 @@ if __name__ == '__main__':
 
         count += 1
 
-    with open(OUTPUT_FILE, 'wb') as file:
-        file.write(pb.SerializeToString())
+    print(f"Writing {count} classes to {OUTPUT_FILE}")
 
-    print(f"Wrote {count} classes to {OUTPUT_FILE}")
+    with open(OUTPUT_FILE, 'w') as file:
+        file.write(json.dumps(all_blocks, indent=2))
