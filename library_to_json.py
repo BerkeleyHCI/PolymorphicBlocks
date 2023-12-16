447be677c3c9ae476e3706753f1d7d0c2690f635
Richard Lin <elpato25@gmail.com>
2023-12-15 16:48:35 -0800

    Update library_to_json.py

--- library_to_json.py
+++ library_to_json.py
@@ -1,9 +1,42 @@
 # Simple tool that scans for libraries and dumps the whole thing to a proto file
+from typing import TypedDict, Tuple, Optional, Union
+
 from edg_hdl_server.__main__ import LibraryElementIndexer
 import edg_core
 import edgir
 import edg
 from edg_core.Builder import builder
+
+
+class PortJsonDict(TypedDict):
+    name: str  # name in parent
+    type: str  # type of self; if array refers to the array element type
+    is_array: str
+    hint_position: str  # left | right | up | down
+
+
+class ParamJsonDict(TypedDict):
+    name: str
+    type: str  # int | float | range | string
+    defaultValue: Optional[str]  # in Python HDL
+
+
+class BlockJsonDict(TypedDict):
+    name: str  # name in superblock - empty for libraries
+    type: str  # type of self
+    superClasses: list[str]  # superclasses of self
+    ports: list[PortJsonDict]
+    argParams: list[ParamJsonDict]
+
+
+class NetJsonDict(TypedDict):
+    name: Optional[str]  # optional net name / label
+    ports: list[Tuple[str, str]]  # block name, port name - note, interior connects only
+
+
+class NetlistJsonDict(TypedDict):
+    nets: list[NetJsonDict]
+
 
 
 OUTPUT_FILE = "library.edg"
@@ -21,16 +54,16 @@ if __name__ == '__main__':
             print(f"Elaborating block {name}")
             block_proto = builder.elaborate_toplevel(obj)
             pb.root.members[name].hierarchy_block.CopyFrom(block_proto)
-        elif isinstance(obj, edg_core.Link):
-            print(f"Elaborating link {name}")
-            link_proto = builder.elaborate_toplevel(obj)
-            pb.root.members[name].link.CopyFrom(link_proto)
-        elif isinstance(obj, edg_core.Bundle):  # TODO: note Bundle extends Port, so this must come first
-            print(f"Elaborating bundle {name}")
-            pb.root.members[name].bundle.CopyFrom(obj._def_to_proto())
-        elif isinstance(obj, edg_core.Port):
-            print(f"Elaborating port {name}")
-            pb.root.members[name].port.CopyFrom(obj._def_to_proto())
+        # elif isinstance(obj, edg_core.Link):
+        #     print(f"Elaborating link {name}")
+        #     link_proto = builder.elaborate_toplevel(obj)
+        #     pb.root.members[name].link.CopyFrom(link_proto)
+        # elif isinstance(obj, edg_core.Bundle):  # TODO: note Bundle extends Port, so this must come first
+        #     print(f"Elaborating bundle {name}")
+        #     pb.root.members[name].bundle.CopyFrom(obj._def_to_proto())
+        # elif isinstance(obj, edg_core.Port):
+        #     print(f"Elaborating port {name}")
+        #     pb.root.members[name].port.CopyFrom(obj._def_to_proto())
         else:
             print(f"Unknown category for class {cls}")
 
