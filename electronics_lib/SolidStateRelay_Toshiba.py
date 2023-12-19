9bea47f84d23e79e982c1ef6fb66d3ab064bb2f0
Richard Lin <elpato25@gmail.com>
2023-12-19 11:01:20 -0800

    Update SolidStateRelay_Toshiba.py

--- electronics_lib/SolidStateRelay_Toshiba.py
+++ electronics_lib/SolidStateRelay_Toshiba.py
@@ -9,15 +9,17 @@ class Tlp3545a(SolidStateRelay, FootprintBlock):
     self.assign(self.led_current_recommendation, (5, 19.5)*mAmp)  # typ=10mA
     self.assign(self.load_voltage_limit, (-48, 48)*Volt)
     self.assign(self.load_current_limit, (0, 2.6)*Amp)
-    self.assign(self.load_resistance, (9, 60)*mOhm)
+    self.assign(self.load_resistance, (35, 60)*mOhm)  # 'A' connection, supports AC but higher resistance
 
     self.footprint(
       'U', 'Package_DIP:SMDIP-6_W7.62mm',
       {
         '1': self.leda,
         '2': self.ledk,
-        '3': self.feta,
-        '4': self.fetb,
+        # '3': nc,
+        '4': self.feta,  # 'A' connection
+        # '5': source - common
+        '6': self.fetb,
       },
       mfr='Toshiba', part='TLP3545A(TP1,F',
       datasheet='https://toshiba.semicon-storage.com/info/docget.jsp?did=60318&prodName=TLP3545A'
