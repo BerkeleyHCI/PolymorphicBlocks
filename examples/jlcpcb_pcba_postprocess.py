f5043113af4750431d9b00c2995ba94f41be9e9b
Richard Lin <richard.lin@berkeley.edu>
2024-01-03 14:54:57 -0800

    wiiiiip

--- examples/jlcpcb_pcba_postprocess.py
+++ examples/jlcpcb_pcba_postprocess.py
@@ -19,7 +19,7 @@ PART_ROTATIONS = {
   'C91199': 180,  # VL53L0X
   'C27396': -90,  # TPA2005D1
   'C12084': -90,  # SN65HVD230DR
-  'C264517': 90,  # 0606 RGB LED
+  'C264517': 180,  # 0606 RGB LED
   'C158099': 90,  # 0404 RGB LED
   'C86832': -90,  # PCF8574 IO expander
   'C500769': -90,  # AP3418 buck converter
@@ -35,6 +35,7 @@ PART_ROTATIONS = {
   'C222571': 0,  # XC6209 5.0 SOT-23-5
   'C86803': -90,  # PCA9554A TSSOP16
   'C106912': -90,  # NLAS4157
+  'C3663690': 180,  # TMP1075N
 
   'C2962219': -90,  # 2x5 1.27mm header shrouded
   'C126830': 90,  # "SOT-23" USB ESD protector
@@ -68,7 +69,6 @@ _FOOTPRINT_ROTATIONS = {
   'Package_TO_SOT_SMD:SOT-89-3': 180,
   'Package_TO_SOT_SMD:SOT-323_SC-70': 180,
   'Package_TO_SOT_SMD:SOT-223-3_TabPin2': 180,
-  'Package_TO_SOT_SMD:TO-252-2': 180,  # aka DPak
   'Package_SO:SOIC-8_3.9x4.9mm_P1.27mm': -90,
   'Package_SO:SOIC-8_5.23x5.23mm_P1.27mm': -90,
   'Package_DFN_QFN:UDFN-4-1EP_1x1mm_P0.65mm_EP0.48x0.48mm': -90,
@@ -95,7 +95,6 @@ PART_OFFSETS: Dict[str, Tuple[float, float]] = {
 }
 _FOOTPRINT_OFFSETS: Dict[str, Tuple[float, float]] = {
   'Package_TO_SOT_SMD:SOT-89-3': (-0.6, 0),
-  'Package_TO_SOT_SMD:TO-252-2': (-1.9, 0),  # aka DPak
 
   'Connector_USB:USB_C_Receptacle_XKB_U262-16XN-4BVC11': (0, -1.25),
   'RF_Module:ESP32-WROOM-32': (0, 0.8),
