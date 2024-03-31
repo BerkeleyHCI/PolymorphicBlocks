a7ad06644be550e86d2fcac475e3a6f94fc9d212
Richard Lin <elpato25@gmail.com>
2024-03-31 16:43:40 -0700

    opax189 and refactor for x197

--- electronics_lib/Opamp_Opax189.py
+++ electronics_lib/Opamp_Opax189.py
@@ -2,23 +2,24 @@ from electronics_abstract_parts import *
 from .JlcPart import JlcPart
 
 
-class Opa197_Base_Device(InternalSubcircuit):
+@non_library
+class Opax189_Base_Device(InternalSubcircuit):
   DEVICES: int
 
   def _analog_in_model(self):
     return AnalogSink.from_supply(
       self.vn, self.vp,
       voltage_limit_tolerance=(-0.5, 0.5)*Volt,  # input common mode absolute maximum ratings
-      signal_limit_tolerance=(-0.1, 0.1)*Volt,
-      impedance=1e13*Ohm(tol=0)  # no tolerance bounds given on datasheet
+      signal_limit_bound=(-0.1, -2.5),
+      impedance=100*MOhm(tol=0),  # differential input impedance; no tolerance bounds specified
     )
 
   def _analog_out_model(self):
     return AnalogSource.from_supply(
       self.vn, self.vp,
-      signal_out_bound=(0.125*Volt, -0.125*Volt),  # output swing from rail, assumed at 10k load
+      signal_out_bound=(0.110*Volt, -0.111*Volt),  # output swing from rail, assumed at 10k load
       current_limits=(-65, 65)*mAmp,  # for +/-18V supply
-      impedance=375*Ohm(tol=0)  # no tolerance bounds given on datasheet; open-loop impedance
+      impedance=380*Ohm(tol=0)  # open-loop impedance; no tolerance bounds specified
     )
 
   def __init__(self):
@@ -30,7 +31,7 @@ class Opa197_Base_Device(InternalSubcircuit):
     ), [Power])
 
 
-class Opa197_Device(Opa197_Base_Device, JlcPart, FootprintBlock):
+class Opa189_Device(Opax189_Base_Device, JlcPart, FootprintBlock):
   DEVICES = 1
 
   def __init__(self):
@@ -54,39 +55,37 @@ class Opa197_Device(Opa197_Base_Device, JlcPart, FootprintBlock):
         '7': self.vp,
         # 8 is NC
       },
-      mfr='Texas Instruments', part='OPA197IDR',
-      datasheet='https://www.ti.com/lit/ds/symlink/opa197.pdf'
+      mfr='Texas Instruments', part='OPA189DR',
+      datasheet='https://www.ti.com/lit/ds/symlink/opa4189.pdf'
     )
-    self.assign(self.lcsc_part, 'C79274')
+    self.assign(self.lcsc_part, 'C781811')
     self.assign(self.actual_basic_part, False)
 
 
-class Opa197(Opamp):
-  """High voltage opamp (4.5-36V) in SOIC-8.
-  (part also available in SOT-23-5)
+class Opa189(Opamp):
+  """High voltage (4.5-36V), low-noise opamp in SOIC-8.
   """
   def contents(self):
     super().contents()
 
-    self.ic = self.Block(Opa197_Device())
+    self.ic = self.Block(Opa189_Device())
     self.connect(self.inn, self.ic.vinn)
     self.connect(self.inp, self.ic.vinp)
     self.connect(self.out, self.ic.vout)
     self.connect(self.pwr, self.ic.vp)
     self.connect(self.gnd, self.ic.vn)
 
-    # Datasheet section 10.1: use a low-ESR 0.1uF capacitor between each supply and ground pin
+    # Datasheet section 10: use a low-ESR 0.1uF capacitor between each supply and ground pin
     self.vdd_cap = self.Block(DecouplingCapacitor(
       capacitance=0.1*uFarad(tol=0.2),
     )).connected(self.gnd, self.pwr)
 
 
-class Opa2197_Device(Opa197_Base_Device, JlcPart, FootprintBlock):
+class Opa2189_Device(Opax189_Base_Device, JlcPart, FootprintBlock):
   DEVICES = 2
 
   def __init__(self):
     super().__init__()
-
     analog_in_model = self._analog_in_model()
     analog_out_model = self._analog_out_model()
     self.inpa = self.Port(analog_in_model)
@@ -111,13 +110,13 @@ class Opa2197_Device(Opa197_Base_Device, JlcPart, FootprintBlock):
         '8': self.vp,
       },
       mfr='Texas Instruments', part='OPA2197IDR',
-      datasheet='https://www.ti.com/lit/ds/symlink/opa197.pdf'
+      datasheet='https://www.ti.com/lit/ds/symlink/opa4189.pdf'
     )
-    self.assign(self.lcsc_part, 'C139363')
+    self.assign(self.lcsc_part, 'C881489')
     self.assign(self.actual_basic_part, False)
 
 
-class Opa2197(MultipackBlock, GeneratorBlock):
+class Opa2189(MultipackBlock, GeneratorBlock):
   """Dual precision RRO opamps.
 
   TODO infrastructure for packed opamps? Packed opamp abstract class? - shared w/ OPAx333
@@ -135,7 +134,7 @@ class Opa2197(MultipackBlock, GeneratorBlock):
 
   def generate(self):
     super().generate()
-    self.ic = self.Block(Opa2197_Device())
+    self.ic = self.Block(Opa2189_Device())
 
     # Datasheet section 9: recommend 0.1uF bypass capacitors close to power supply pins
     self.vdd_cap = self.Block(DecouplingCapacitor(
