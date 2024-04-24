0625825be3b432dcec7baffbabb941aee3c85102
Suke0811 <yusuketanaka@g.ucla.edu>
2024-04-23 17:01:01 -0700

    Add: composition ver sion of SoftPowerSwitch in proogress.

--- electronics_lib/PowerConditioning.py
+++ electronics_lib/PowerConditioning.py
@@ -301,7 +301,7 @@ class SoftPowerGate(PowerSwitch, KiCadSchematicBlock, Block):  # migrate from th
 
   def contents(self):
     super().contents()
-    control_voltage = self.control.link().voltage.hull(RangeExpr.ZERO, )
+    control_voltage = self.btn_in.link().voltage.hull(RangeExpr.ZERO, )
     pwr_voltage = self.pwr_out.link().voltage.hull(RangeExpr.ZERO, )
     pwr_current = self.pwr_out.link().current_drawn.hull(RangeExpr.ZERO, )
 
@@ -368,4 +368,28 @@ class SoftPowerSwitch(SoftPowerGate):
 
   def contents(self):
     self.btn = self.Block(Switch(voltage=0 * Volt(tol=0)))  # TODO - actually model switch voltage
+    super().contents()
+
+
+class SoftPowerBtn(PowerSwitch, Block):
+  @init_in_parent
+  def __init__(self, pull_resistance: RangeLike = 10 * kOhm(tol=0.05), amp_resistance: RangeLike = 10 * kOhm(tol=0.05),
+               diode_drop: RangeLike = (0, 0.4) * Volt):
+    super().__init__()
+
+    with self.implicit_connect(
+            ImplicitConnect(self.gnd, [Common]),
+    ) as imp:
+      pass
+    self.pwr_gate = self.Block(SoftPowerGate(pull_resistance, amp_resistance, diode_drop))
+    self.btn = self.Block(Switch(voltage=0 * Volt(tol=0)))  # TODO - actually model switch voltage
+
+    self.pwr_in = self.Export(self.pwr_gate.pwr_in)
+    self.pwr_out = self.Export(self.pwr_gate.pwr_out)
+    self.gnd = self.Port(Ground.empty(), [Common])
+    self.btn_out = self.Export(self.pwr_gate.btn_out)
+    self.control = self.Export(self.pwr_gate.control)
+
+
+  def contents(self):
     super().contents()
