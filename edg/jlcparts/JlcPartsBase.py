c456421d66831c98d6f09dd96da727b282d17fa2
Richard Lin <elpato25@gmail.com>
2024-08-10 13:37:52 -0700

    Create JlcPartsBase.py

---
+++ edg/jlcparts/JlcPartsBase.py
@@ -0,0 +1,35 @@
+from typing import Any, Tuple
+
+from pydantic import BaseModel, RootModel
+import gzip
+
+kTestingFilename = "../../../../jlcparts/web/public/data/CrystalsakaOscillatorsakaResonatorsCrystals.json.gz"
+kSchemaLcsc = "lcsc"  # JLC part number
+kSchemaPartName = "mfr"  # manufacturer part name
+kSchemaDatasheet = "datasheet"  # URL
+kSchemaAttributes = "attributes"  # attribute table
+
+class JlcPartFile(BaseModel):
+    category: str
+    components: list[list[Any]]  # index-matched with schema
+    schema: list[str]
+
+class JlcPartValue(BaseModel):
+    default: Tuple[str, str]  # value, type / units
+
+class JlcPartAttributeEntry(BaseModel):
+    format: str
+    primary: str
+    values: JlcPartValue
+
+class JlcPartAttributes(RootModel):
+    root: dict[str, JlcPartAttributeEntry]
+
+
+if __name__ == "__main__":
+    with gzip.open(kTestingFilename, 'r') as f:
+        data = JlcPartFile.model_validate_json(f.read())
+        print(len(data.components))
+        print(data.category)
+        print(data.components[0])
+        print(JlcPartAttributes(**data.components[0][-1]))
