132f153d390f80ed4950b25ff6c91d8809d191a5
Richard Lin <elpato25@gmail.com>
2024-08-10 14:26:16 -0700

    Update JlcPartsBase.py

--- edg/jlcparts/JlcPartsBase.py
+++ edg/jlcparts/JlcPartsBase.py
@@ -25,13 +25,10 @@ class JlcPartsFile(BaseModel):
     components: list[list[Any]]  # index-matched with schema
     jlcpart_schema: list[str] = Field(..., alias='schema')
 
-class JlcPartsValue(BaseModel):
-    default: Tuple[Any, str]  # value, type / units
-
 class JlcPartsAttributeEntry(BaseModel):
     format: str
     primary: str
-    values: JlcPartsValue
+    values: dict[str, tuple[Any, str]]
 
 class JlcPartsAttributes(RootModel):
     root: dict[str, JlcPartsAttributeEntry]
@@ -45,9 +42,9 @@ class JlcPartsBase(JlcPart, PartsTableBase):
     def configure_root_dir(root_dir: str):
         """Configures the root dir that contains the data files from jlcparts, eg
         CapacitorsMultilayer_Ceramic_Capacitors_MLCC___SMDakaSMT.json.gz"""
-        assert JlcPartsBase.parts_root_dir is None, \
-            f"attempted to reassign configure_root_dir, was {JlcPartsBase.parts_root_dir}, new {root_dir}"
-        JlcPartsBase.parts_root_dir = root_dir
+        assert JlcPartsBase._parts_root_dir is None, \
+            f"attempted to reassign configure_root_dir, was {JlcPartsBase._parts_root_dir}, new {root_dir}"
+        JlcPartsBase._parts_root_dir = root_dir
 
     _kFileName: str  # set by subclass
     _cached_table: Optional[PartsTable] = None  # set on a per-class basis
@@ -55,7 +52,7 @@ class JlcPartsBase(JlcPart, PartsTableBase):
     @classmethod
     def _jlc_table(cls) -> PartsTable:
         """Return the table, cached if possible"""
-        if cls._cached_table is not None:
+        if cls._cached_table is None:
             cls._cached_table = cls._parse_table()
         return cls._cached_table
 
@@ -69,6 +66,7 @@ class JlcPartsBase(JlcPart, PartsTableBase):
     @classmethod
     def _parse_table(cls) -> PartsTable:
         """Parses the file to a PartsTable"""
+        assert cls._parts_root_dir is not None, "must configure_root_dir with jlcparts data folder"
         with gzip.open(os.path.join(cls._parts_root_dir, cls._kFileName), 'r') as f:
             data = JlcPartsFile.model_validate_json(f.read())
         rows: List[PartsTableRow] = []
@@ -84,8 +82,8 @@ class JlcPartsBase(JlcPart, PartsTableBase):
             row_dict[PartsTableBase.DATASHEET_COL] = component[datasheetIndex]
 
             attributes = JlcPartsAttributes(**component[attributesIndex])
-            row_dict[PartsTableBase.MANUFACTURER_COL] = attributes.root[kAttributeManufacturer].values[0][0]
-            package = attributes.root[kAttributePackage].values[0][0]
+            row_dict[PartsTableBase.MANUFACTURER_COL] = list(attributes.root[kAttributeManufacturer].values)[0][0]
+            package = list(attributes.root[kAttributePackage].values)[0][0]
 
             row_dict_opt = cls._entry_to_table_row(row_dict, package, attributes)
             if row_dict_opt is not None:
