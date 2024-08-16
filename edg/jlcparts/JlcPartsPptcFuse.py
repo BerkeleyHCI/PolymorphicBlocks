d1cd0f9fdff7876a89cffc9483a8dd96834c4bdb
Richard Lin <elpato25@gmail.com>
2024-08-15 20:21:40 -0700

    Update JlcPartsPptcFuse.py

--- edg/jlcparts/JlcPartsPptcFuse.py
+++ edg/jlcparts/JlcPartsPptcFuse.py
@@ -1,17 +1,24 @@
 from typing import Any, Optional, Dict
 from ..abstract_parts import *
-from ..parts import JlcResistor
+from ..parts import JlcPptcFuse
 from .JlcPartsBase import JlcPartsBase, JlcPartsAttributes
 
 
-class JlcPartsPptcFuse(TableResistor, SmdStandardPackageSelector, JlcPartsBase):
-    # _JLC_PARTS_FILE_NAME = "ResistorsChip_Resistor___Surface_Mount"
+class JlcPartsPptcFuse(PptcFuse, TableFuse, SmdStandardPackageSelector, JlcPartsBase):
+    _JLC_PARTS_FILE_NAMES = ["Circuit_ProtectionResettable_Fuses"]
 
     @classmethod
     def _entry_to_table_row(cls, row_dict: Dict[PartsTableColumn, Any], package: str, attributes: JlcPartsAttributes) \
             -> Optional[Dict[PartsTableColumn, Any]]:
         try:
-            # row_dict[cls.KICAD_FOOTPRINT] = JlcResistor.PACKAGE_FOOTPRINT_MAP[package]
+            row_dict[cls.KICAD_FOOTPRINT] = JlcPptcFuse.PACKAGE_FOOTPRINT_MAP[package]
+
+            row_dict[cls.VOLTAGE_RATING] = Range.zero_to_upper(float(PartParserUtil.parse_value(
+                attributes.get("Operating voltage (max)", str), 'V')))
+            row_dict[cls.TRIP_CURRENT] = Range.exact(float(PartParserUtil.parse_value(
+                attributes.get("Trip current", str), 'A')))
+            row_dict[cls.HOLD_CURRENT] = Range.exact(float(PartParserUtil.parse_value(
+                attributes.get("Hold current", str), 'A')))
 
             return row_dict
         except (KeyError, TypeError, PartParserUtil.ParseError):
