e608f9d6fb07baa4c5c1f48f7b804b469dff68ae
Richard Lin <elpato25@gmail.com>
2024-10-27 21:00:05 -0700

    Update blinky_skeleton.py

--- blinky_skeleton.py
+++ blinky_skeleton.py
@@ -4,14 +4,7 @@ from edg import *
 class BlinkyExample(SimpleBoardTop):
   def contents(self) -> None:
     super().contents()
-    self.mcu = self.Block(Xiao_Esp32c3())
-    self.pololu = self.Block(PololuA4988())
-    self.connect(self.pololu.gnd, self.mcu.gnd)
-    self.connect(self.pololu.pwr, self.mcu.vusb_out)
-    self.connect(self.pololu.pwr_logic, self.mcu.pwr_out)
-
     # your implementation here
-
 
 
 if __name__ == "__main__":
