b25faf2b997f5ed5fae164ccfaca8a722849911d
Richard Lin <elpato25@gmail.com>
2025-03-24 00:38:45 -0700

    drv8870

--- edg/parts/__init__.py
+++ edg/parts/__init__.py
@@ -156,6 +156,7 @@ from .Mechanicals import MountingHole, MountingHole_M2_5, MountingHole_M3, Mount
 
 from .MotorDriver_L293dd import L293dd
 from .MotorDriver_Drv8833 import Drv8833
+from .MotorDriver_Drv8870 import Drv8870
 from .Bldc_Drv8313 import Drv8313
 from .StepperDriver_A4988 import A4988, PololuA4988
 
