d070d6a11e378ab4e6d900e3cffc566a9fc60284
Richard Lin <elpato25@gmail.com>
2025-04-30 23:22:02 -0700

    wip bulk placement

--- edg/parts/LedDriver_Tps92200.py
+++ edg/parts/LedDriver_Tps92200.py
@@ -50,7 +50,7 @@ class Tps92200(LedDriverPwm, LedDriver, GeneratorBlock):
     @init_in_parent
     def __init__(self, led_voltage: RangeLike = (1, 4)*Volt, *,
                  input_ripple_limit: FloatLike = 0.2*Volt,  # from 8.2 example application
-                 output_ripple_limit: FloatLike = 0.08*Volt,  # 20mA ripple @ ~4ohm
+                 output_ripple_limit: FloatLike = 0.01*Volt,
                  ripple_current_factor: RangeLike = (0.2, 0.5)) -> None:
         super().__init__()
 
