6f7b22c1c7bafd49caae98687e0178b0a33ddb1d
Richard Lin <elpato25@gmail.com>
2025-06-15 17:32:02 -0700

    fix all the things

--- edg/abstract_parts/test_switching_converters.py
+++ edg/abstract_parts/test_switching_converters.py
@@ -29,7 +29,7 @@ class SwitchingConverterCalculationTest(unittest.TestCase):
         # using the example from https://passive-components.eu/buck-converter-design-and-calculation/
         values = BuckConverterPowerPath._calculate_parameters(
             Range.exact(12 + 0.4), Range.exact(3.3 + 0.4), Range.exact(500e3), Range.exact(1),
-            Range.exact(1), Range.exact(0.35), 1, 0.0165,
+            Range.exact(2), Range.exact(0.35), 1, 0.0165,
             efficiency=Range.exact(1)
         )
         self.assertAlmostEqual(values.dutycycle.upper, 0.298, places=3)
@@ -38,7 +38,7 @@ class SwitchingConverterCalculationTest(unittest.TestCase):
         # the example uses a ripple current of 0.346 for the rest of the calculations
         values = BuckConverterPowerPath._calculate_parameters(
             Range.exact(12 + 0.4), Range.exact(3.3 + 0.4), Range.exact(500e3), Range.exact(1),
-            Range.exact(1), Range.exact(0.346), 1, 0.0165,
+            Range.exact(2), Range.exact(0.346), 1, 0.0165,
             efficiency=Range.exact(1)
         )
         self.assertAlmostEqual(values.inductor_peak_currents.upper, 1.173, places=3)
@@ -47,7 +47,7 @@ class SwitchingConverterCalculationTest(unittest.TestCase):
     def test_boost_converter(self):
         values_ref = BoostConverterPowerPath._calculate_parameters(
             Range.exact(5), Range.exact(10), Range.exact(100e3), Range.exact(0.5),
-            Range.exact(1), Range.exact(0.4), 0.01, 0.001,
+            Range.exact(2), Range.exact(0.4), 0.01, 0.001,
             efficiency=Range.exact(1)
         )
         self.assertEqual(values_ref.dutycycle, Range.exact(0.5))
@@ -58,7 +58,7 @@ class SwitchingConverterCalculationTest(unittest.TestCase):
         # test that component values are calculated for worst-case conversion
         values = BoostConverterPowerPath._calculate_parameters(
             Range(5, 8), Range(7, 10), Range.exact(100e3), Range.exact(0.5),
-            Range.exact(1), Range.exact(0.4), 0.01, 0.001,
+            Range.exact(2), Range.exact(0.4), 0.01, 0.001,
             efficiency=Range.exact(1)
         )
         self.assertEqual(values_ref.inductance, values.inductance)
