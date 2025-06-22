fc0c9427157f660271fbda2ba1bef9778041ea04
Richard Lin <elpato25@gmail.com>
2025-06-21 20:12:03 -0700

    Add refdes + pathname-as-value mode for netlist generation (#412)
    Probably a better default option, with a familiar refdes where that
    belongs, plus the pathname in the value field, which shows up on F.Fab
    in KiCad.

--- edg/electronics_model/NetlistBackend.py
+++ edg/electronics_model/NetlistBackend.py
@@ -10,16 +10,18 @@ class NetlistBackend(BaseBackend):
   def run(self, design: CompiledDesign, args: Dict[str, str] = {}) -> List[Tuple[edgir.LocalPath, str]]:
     if set(args.keys()) - {'RefdesMode'} != set():
       raise ValueError("Invalid argument found in args")
-    refdes_mode = args.get("RefdesMode", "pathName")
-    if refdes_mode == 'pathName':
-      refdes_pathname = True
-    elif refdes_mode == 'refdes':
-      refdes_pathname = False
+    refdes_mode_arg = args.get("RefdesMode", "refdesPathNameValue")
+    if refdes_mode_arg == 'pathName':
+      refdes_mode = kicad.RefdesMode.Pathname
+    elif refdes_mode_arg == 'refdes':
+      refdes_mode = kicad.RefdesMode.Conventional
+    elif refdes_mode_arg == 'refdesPathNameValue':
+      refdes_mode = kicad.RefdesMode.PathnameAsValue
     else:
-      raise ValueError(f"Invalid valueMode value {refdes_mode}")
+      raise ValueError(f"Invalid valueMode value {refdes_mode_arg}")
 
     netlist = NetlistTransform(design).run()
-    netlist_string = kicad.generate_netlist(netlist, refdes_pathname)
+    netlist_string = kicad.generate_netlist(netlist, refdes_mode)
 
     return [
       (edgir.LocalPath(), netlist_string)
