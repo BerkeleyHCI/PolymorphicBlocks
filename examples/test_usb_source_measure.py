fcfb4ccd90c79a116bdd19aa7020c7f5013053a4
Richard Lin <elpato25@gmail.com>
2025-10-25 12:24:29 -0700

    rc2

--- examples/test_usb_source_measure.py
+++ examples/test_usb_source_measure.py
@@ -434,8 +434,8 @@ class SourceMeasureControl(InternalSubcircuit, KiCadSchematicBlock, Block):
       locals={
         'self': self
       }, conversions={
-        'Q1.K': VoltageSink(),  # tvs_p
-        'Q2.K': Ground(),  # tvs_n
+        'tvs_p.K': VoltageSink(),
+        'tvs_n.K': Ground(),
       })
 
 
