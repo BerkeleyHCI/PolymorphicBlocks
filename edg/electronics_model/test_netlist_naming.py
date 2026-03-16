9f30181ed34b48ebdcd7639d8c7cf331f6b93d10
Richard Lin <elpato25@gmail.com>
2026-03-15 22:36:32 -0700

    Update test_netlist_naming.py

--- edg/electronics_model/test_netlist_naming.py
+++ edg/electronics_model/test_netlist_naming.py
@@ -10,10 +10,7 @@ class NetlistNameTestCase(unittest.TestCase):
         path_link = TransformUtil.Path.empty().append_link("link")
         path_porta = TransformUtil.Path.empty().append_block("block").append_port("a")
         path_portb = TransformUtil.Path.empty().append_block("block").append_port("a")
-        order = {path_block: 0,
-                 path_link: 1,
-                 path_porta: 2,
-                 path_portb: 3}
+        order = {path_block: 0, path_link: 1, path_porta: 2, path_portb: 3}
         self.assertEqual(NetlistTransform.name_net([path_block, path_portb, path_porta, path_link], order), path_link)
         self.assertEqual(NetlistTransform.name_net([path_block, path_portb, path_porta], order), path_block)
         self.assertEqual(NetlistTransform.name_net([path_portb, path_porta], order), path_porta)
