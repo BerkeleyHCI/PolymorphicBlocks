0ceb37e60bcd92cf3b3df42600835f892b54fbe9
Richard Lin <elpato25@gmail.com>
2026-03-29 14:03:12 -0700

    wip voltage unit test

--- edg/electronics_model/test_voltage_link.py
+++ edg/electronics_model/test_voltage_link.py
@@ -3,12 +3,84 @@ import unittest
 from typing_extensions import override
 
 from .VoltagePorts import VoltageLink
+from . import *
+
+
+class VoltageTestTop(DesignTop):
+    """Test design with single voltage source and sink with valid ranges"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.src = self.Block(DummyVoltageSource(voltage_out=5 * Volt(tol=0), current_limits=(0, 1) * Amp))
+        self.sink = self.Block(DummyVoltageSink(voltage_limit=5 * Volt(tol=0.1), current_draw=1 * Amp(tol=0)))
+        self.connect(self.src.pwr, self.sink.pwr)
+
+
+class NoSourceVoltageTest(DesignTop):
+    """Test design with missing source"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.sink = self.Block(DummyVoltageSink(voltage_limit=5 * Volt(tol=0.1), current_draw=1 * Amp(tol=0)))
+
+
+class OvervoltageTestTop(DesignTop):
+    """Test design with single source and single restrictive sink"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.src = self.Block(DummyVoltageSource(voltage_out=5 * Volt(tol=0), current_limits=(0, 1) * Amp))
+        self.sink = self.Block(DummyVoltageSink(voltage_limit=3.3 * Volt(tol=0.1), current_draw=1 * Amp(tol=0)))
+        self.connect(self.src.pwr, self.sink.pwr)
+
+
+class OvercurrentTestTop(DesignTop):
+    """Test design with single source and single restrictive sink"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.src = self.Block(DummyVoltageSource(voltage_out=5 * Volt(tol=0), current_limits=(0, 1) * Amp))
+        self.sink1 = self.Block(DummyVoltageSink(voltage_limit=5 * Volt(tol=0.1), current_draw=1 * Amp(tol=0)))
+        self.sink2 = self.Block(DummyVoltageSink(voltage_limit=5 * Volt(tol=0.1), current_draw=1 * Amp(tol=0)))
+        self.connect(self.src.pwr, self.sink1.pwr, self.sink2.pwr)
+
+
+class ReverseVoltageTestTop(DesignTop):
+    """Test design with valid forward and reverse voltage flows"""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class NoReverseSinkVoltageTest(DesignTop):
+    """Test design with reverse voltage source but no sink"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.src = self.Block(DummyVoltageSource(voltage_out=5 * Volt(tol=0), current_limits=(0, 1) * Amp))
+        self.sink = self.Block(DummyVoltageSink(voltage_limit=3.3 * Volt(tol=0.1), current_draw=1 * Amp(tol=0)))
+        self.connect(self.src.pwr, self.sink.pwr)
 
 
 class VoltageLinkTestCase(unittest.TestCase):
-    @override
-    def setUp(self) -> None:
-        self.pb = VoltageLink()._elaborated_def_to_proto()
+    def test_voltage(self) -> None:
+        ScalaCompiler.compile(VoltageTestTop)
 
-    def test_metadata(self) -> None:
-        self.assertIn("nets", self.pb.meta.members.node)
+    def test_no_source(self) -> None:
+        with self.assertRaises(CompilerCheckError):
+            ScalaCompiler.compile(NoSourceVoltageTest)
+
+    def test_overvoltage(self) -> None:
+        with self.assertRaises(CompilerCheckError):
+            ScalaCompiler.compile(OvervoltageTestTop)
+
+    def test_overcurrent(self) -> None:
+        with self.assertRaises(CompilerCheckError):
+            ScalaCompiler.compile(OvercurrentTestTop)
+
+    def test_reverse_voltage(self) -> None:
+        ScalaCompiler.compile(ReverseVoltageTestTop)
+
+    def test_no_reverse_sink(self) -> None:
+        with self.assertRaises(CompilerCheckError):
+            ScalaCompiler.compile(NoReverseSinkVoltageTest)
