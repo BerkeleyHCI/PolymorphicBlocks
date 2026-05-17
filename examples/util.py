e332abc5c98570b92da0ea8c3ee500e5ace4048b
Richard Lin <elpato25@gmail.com>
2026-05-17 16:52:35 -0700

    multi netlsit diff

--- examples/util.py
+++ examples/util.py
@@ -9,15 +9,23 @@ def run_test_board(design: Type[Block]) -> None:
 
     compile_board_inplace(design)
 
-    designfile = inspect.getfile(design)
-    with open(os.path.join(os.path.dirname(designfile), design.__name__, design.__name__ + ".net"), newline=None) as f:
-        generated_netlist = f.read()
+    dirname = os.path.dirname(inspect.getfile(design))
+    ref_net_filenames = [
+        f[:-4] for f in os.listdir(dirname) if f.startswith(design.__name__) and f.endswith(".net.ref")
+    ]
+    net_filenames = [f for f in os.listdir(dirname) if f.startswith(design.__name__) and f.endswith(".net")]
 
-    with open(
-        os.path.join(os.path.dirname(designfile), design.__name__, design.__name__ + ".net.ref"), newline=None
-    ) as f:
-        reference_netlist = f.read()
+    assert set(ref_net_filenames) == set(
+        net_filenames
+    ), f"reference netlist files {ref_net_filenames} do not match generated netlist files {net_filenames}"
 
-    assert (
-        generated_netlist == reference_netlist
-    ), f"netlist differs from reference for {design.__name__}, if this is expected you may need to update the reference"
+    for net_filename in net_filenames:
+        with open(os.path.join(dirname, net_filename), newline=None) as f:
+            generated_netlist = f.read()
+
+        with open(os.path.join(dirname, net_filename + ".ref"), newline=None) as f:
+            reference_netlist = f.read()
+
+        assert (
+            generated_netlist == reference_netlist
+        ), f"netlist differs from reference for {design.__name__}, if this is expected you may need to update the reference"
