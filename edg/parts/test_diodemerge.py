b886a755f9d6581ca6e39fa906f09c8a9b17b812
Richard Lin <elpato25@gmail.com>
2026-05-20 01:09:06 -0700

    arbitrary diode generator

--- edg/parts/test_diodemerge.py
+++ edg/parts/test_diodemerge.py
@@ -8,8 +8,12 @@ class DiodeMergeTestTop(BoardTop):
     def __init__(self) -> None:
         super().__init__()
         self.dut = self.Block(DiodePowerMerge(voltage_drop=(0, 1) * Volt))
-        (self.srca,), _ = self.chain(self.dut.pwr_in1, self.Block(DummyVoltageSource(voltage_out=(12, 14) * Volt)))
-        (self.srcb,), _ = self.chain(self.dut.pwr_in2, self.Block(DummyVoltageSource(voltage_out=(4, 5) * Volt)))
+        (self.srca,), _ = self.chain(
+            self.dut.pwr_ins.request(), self.Block(DummyVoltageSource(voltage_out=(12, 14) * Volt))
+        )
+        (self.srcb,), _ = self.chain(
+            self.dut.pwr_ins.request(), self.Block(DummyVoltageSource(voltage_out=(4, 5) * Volt))
+        )
         (self.sink,), _ = self.chain(self.dut.pwr_out, self.Block(DummyVoltageSink(current_draw=(0.5, 1.5) * Amp)))
 
     @override
@@ -29,5 +33,8 @@ class DiodeMergeTestCase(unittest.TestCase):
         compiled = ScalaCompiler.compile(DiodeMergeTestTop)
 
         self.assertEqual(compiled.get_value(["dut", "pwr_out", "voltage_out"]), Range(3.0, 14.0))
-        self.assertEqual(compiled.get_value(["dut", "pwr_in1", "current_draw"]), Range(0.5, 1.5))
-        self.assertEqual(compiled.get_value(["dut", "pwr_in2", "current_draw"]), Range(0.5, 1.5))
+        self.assertEqual(compiled.get_value(["dut", "pwr_ins", "0", "current_draw"]), Range(0.5, 1.5))
+        self.assertEqual(compiled.get_value(["dut", "pwr_ins", "1", "current_draw"]), Range(0.5, 1.5))
+        self.assertEqual(compiled.get_value(["dut", "diodes[0]", "fp_footprint"]), "Diode_SMD:D_SOD-123")
+        self.assertEqual(compiled.get_value(["dut", "diodes[1]", "fp_footprint"]), "Diode_SMD:D_SOD-123")
+        self.assertEqual(compiled.get_value(["dut", "diodes[2]", "fp_footprint"]), None)  # doesn't exist
