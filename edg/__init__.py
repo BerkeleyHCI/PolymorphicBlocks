ec5f467e732138c26c10c32f32e42d05e3d7776e
Richard Lin <elpato25@gmail.com>
2026-05-21 23:45:09 -0700

    Update __init__.py

--- edg/__init__.py
+++ edg/__init__.py
@@ -3,8 +3,7 @@ from .BoardTop import BoardTop, SimpleBoardTop, JlcBoardTop
 
 from .BoardCompiler import compile_board, compile_board_inplace
 
-# For compatibility, this re-exports all the internal packages and parts libraries and supporting
-# `from edg import *` for convenience and backwards compatibility.
+# For backwards compatibility, this re-exports internal packages and parts libraries to allow `from edg import *`.
 # This may go away in the future.
 from .core import *
 from .electronics_model import *
