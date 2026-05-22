d879fceea90b02c789b031b412dd24994ea03703
Richard Lin <elpato25@gmail.com>
2026-05-21 23:43:18 -0700

    migration notes

--- edg/__init__.py
+++ edg/__init__.py
@@ -1,5 +1,11 @@
-# A metapackage for all the packages needed for electronics design with EDG
+# Some common operations are directly exported
+from .BoardTop import BoardTop, SimpleBoardTop, JlcBoardTop
 
+from .BoardCompiler import compile_board, compile_board_inplace
+
+# For compatibility, this re-exports all the internal packages and parts libraries and supporting
+# `from edg import *` for convenience and backwards compatibility.
+# This may go away in the future.
 from .core import *
 from .electronics_model import *
 from .abstract_parts import *
@@ -7,7 +13,3 @@ from .vendor_parts import *
 from .vendor_parts.jlc import *
 from .circuits import *
 from .parts import *
-
-from .BoardTop import BoardTop, SimpleBoardTop, JlcBoardTop
-
-from .BoardCompiler import compile_board, compile_board_inplace
