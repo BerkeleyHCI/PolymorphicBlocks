57d2c95d6528ad5eaa6542f7540e8ce918ba7e93
Richard Lin <elpato25@gmail.com>
2026-05-21 23:35:38 -0700

    cleaning

--- examples/update_reference_netlists.py
+++ examples/update_reference_netlists.py
@@ -1,7 +1,9 @@
+"""Utility script to update all the reference netlists in the examples directory."""
+
 if __name__ == "__main__":
     from pathlib import Path
 
-    for toplevel_item in Path().iterdir():
+    for toplevel_item in Path(__file__).parent.iterdir():
         if toplevel_item.is_dir():
             for file in toplevel_item.iterdir():
                 if file.is_file() and file.suffix == ".net":
