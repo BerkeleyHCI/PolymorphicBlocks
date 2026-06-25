273f5f2fd9646aebec1677aee42909d5054755b0
Richard Lin <elpato25@gmail.com>
2026-06-24 23:19:38 -0700

    wip

---
+++ edg/util/__init__.py
@@ -0,0 +1,32 @@
+import warnings
+from functools import wraps
+from typing import Tuple, Callable, ParamSpec, TypeVar, Any
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def deprecated_param_remap(*params: Tuple[str, str]) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    """Decorator to remap deprecated parameter names to new names.
+
+    Args:
+        *params: A list of tuples where each tuple contains the old parameter name and the new parameter name.
+    """
+
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
+        def wrapper(self, *args: Any, **kwargs: Any) -> R:
+            for old_param, new_param in params:
+                if old_param in kwargs:
+                    warnings.warn(
+                        f"{old_param} is deprecated and replaced with {new_param}",
+                        DeprecationWarning,
+                        stacklevel=2,
+                    )
+                    assert new_param not in kwargs, f"both old {old_param} and new {new_param} parameters specified"
+                    kwargs[new_param] = kwargs.pop(old_param)
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
