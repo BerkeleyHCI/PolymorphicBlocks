3166b128514dfb5afc5c220d795171acfb1eafd0
Richard Lin <elpato25@gmail.com>
2026-08-29 18:54:38 -0700

    UART baud test

--- edg/electronics_interfaces/test_uart_link.py
+++ edg/electronics_interfaces/test_uart_link.py
@@ -29,6 +29,23 @@ class UartOverconnectTest(DesignTop):
         self.connect(self.device1.port, self.device2.port, self.device3.port)
 
 
+class UartFrequencyTest(DesignTop):
+    def __init__(self) -> None:
+        super().__init__()
+        self.device1 = self.Block(UartBlock(baud_limit=(9600, 115200) * Hertz))
+        self.device2 = self.Block(UartBlock(baud_limit=(38400, 921600) * Hertz))
+        self.connect(self.device1.port, self.device2.port)
+        self.require(self.device1.port.link().baud_limit == (38400, 115200) * Hertz, _unchecked=True)
+
+
+class UartFrequencyInvalidTest(DesignTop):
+    def __init__(self) -> None:
+        super().__init__()
+        self.device1 = self.Block(UartBlock(baud_limit=(9600, 38400) * Hertz))
+        self.device2 = self.Block(UartBlock(baud_limit=(115200, 921600) * Hertz))
+        self.connect(self.device1.port, self.device2.port)
+
+
 class UartTestCase(unittest.TestCase):
     def test_link(self) -> None:
         ScalaCompiler.compile(UartTest)
@@ -36,3 +53,10 @@ class UartTestCase(unittest.TestCase):
     def test_overconnect(self) -> None:
         with self.assertRaises(UnconnectableError):
             ScalaCompiler.compile(UartOverconnectTest)
+
+    def test_frequency(self) -> None:
+        ScalaCompiler.compile(UartFrequencyTest)
+
+    def test_frequency_invalid(self) -> None:
+        with self.assertRaises(CompilerCheckError):
+            ScalaCompiler.compile(UartFrequencyInvalidTest)
